b09a144c8cc5ebaca48fb0b7f0d34c19e6d1d0b6
NikolayKatsai <alaska.rok123@gmail.com>
2019-09-01 23:16:35 +0300

    MyLinkedList class finished

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,48 +3,152 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node first;
+    private Node last;
+    private int size;
+
     @Override
     public void add(T value) {
-
+        Node oldLast = last;
+        last = new Node(value, null, oldLast);
+        if (oldLast != null) {
+            oldLast.next = last;
+        } else {
+            first = last;
+        }
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index == 0) {
+            linkFirst(value);
+            return;
+        }
+        if (index == size) {
+            add(value);
+            return;
+        }
+        checkElementIndex(index);
 
+        Node next = getNode(index);
+        Node prev = next.previous;
+        Node newNode = new Node(value, next, prev);
+        prev.next = newNode;
+        next.previous = newNode;
+        size++;
     }
+
 
     @Override
     public void addAll(List<T> list) {
-
+        for (T item : list) {
+            add(item);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        return getNode(index).item;
     }
 
     @Override
     public void set(T value, int index) {
-
+        getNode(index).item = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        final Node<T> node = getNode(index);
+        final T item = node.item;
+        final Node next = node.next;
+        final Node prev = node.previous;
+
+        if (next == null) {
+            last = prev;
+        } else {
+            next.previous = prev;
+        }
+
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+        }
+
+        size--;
+        return item;
     }
 
     @Override
     public T remove(T t) {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> currentNode = first;
+        for (int i = 0; i < size; i++) {
+            if (currentNode.item.equals(t)) {
+                return remove(i);
+            }
+            currentNode = currentNode.next;
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void linkFirst(T value) {
+        Node oldFirst = first;
+        first = new Node(value, null, oldFirst);
+        if (oldFirst != null) {
+            oldFirst.previous = first;
+        } else {
+            last = first;
+        }
+        size++;
+    }
+
+    private void checkElementIndex(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private Node<T> getNode(int index) {
+        checkElementIndex(index);
+        Node currentNode = null;
+        if (index < size / 2) {
+            currentNode = first;
+            for (int i = 0; i < index; i++) {
+                currentNode = currentNode.next;
+            }
+            return currentNode;
+        } else {
+            currentNode = last;
+            for (int i = size - 1; i > index; i--) {
+                currentNode = currentNode.previous;
+            }
+            return currentNode;
+        }
+    }
+
+    private static class Node<T> {
+        T item;
+        Node next;
+        Node previous;
+
+        public Node(T item, Node next, Node previous) {
+            this.item = item;
+            this.next = next;
+            this.previous = previous;
+        }
     }
 }
