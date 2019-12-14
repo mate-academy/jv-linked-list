a2b76c09dc61fb3c2978704fd47196c4f5f20df3
ivan <Ivan.krivoruchko.1999@gmail.com>
2019-12-14 04:05:19 +0200

    Added hw81 solution

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,48 +3,148 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
-    @Override
-    public void add(T value) {
+    private static class Node<T> {
+        T element;
+        Node<T> next;
+        Node<T> prev;
 
+        Node(T element) {
+            this.element = element;
+        }
+    }
+
+    private int size;
+    private Node<T> head;
+    private Node<T> tail;
+
+    public MyLinkedList() {
+        this.size = 0;
     }
 
     @Override
-    public void add(T value, int index) {
+    public void add(T value) {
+        Node<T> newNode = new Node<>(value);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
+    }
 
+    @Override
+    public void add(T value, int index) throws ArrayIndexOutOfBoundsException {
+        //checkIndex(index);
+        if (index < 0 || index > size) {
+            throw new ArrayIndexOutOfBoundsException("Index is less than 0");
+        }
+        if (size == index) {
+            add(value);
+            return;
+        }
+        Node<T> nodeByIndex = searchNodeByIndex(index);
+        Node<T> newNode = new Node<>(value);
+        newNode.prev = nodeByIndex.prev;
+        newNode.next = nodeByIndex;
+        nodeByIndex.prev = newNode;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (T t : list) {
+            add(t);
+        }
     }
 
     @Override
-    public T get(int index) {
-        return null;
+    public T get(int index) throws ArrayIndexOutOfBoundsException {
+        checkIndex(index);
+        return searchNodeByIndex(index).element;
     }
 
     @Override
-    public void set(T value, int index) {
-
+    public void set(T value, int index) throws ArrayIndexOutOfBoundsException {
+        checkIndex(index);
+        Node<T> nodeByIndex = searchNodeByIndex(index);
+        nodeByIndex.element = value;
     }
 
     @Override
-    public T remove(int index) {
-        return null;
+    public T remove(int index) throws ArrayIndexOutOfBoundsException {
+        checkIndex(index);
+        return removeNode(searchNodeByIndex(index));
     }
 
     @Override
     public T remove(T t) {
+        Node<T> temp = head;
+        for (int i = 0; i < size; i++) {
+            if (t == temp.element || t != null && t.equals(temp.element)) {
+                size--;
+                return temp.element;
+            } else {
+                temp = temp.next;
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void checkIndex(int index) throws ArrayIndexOutOfBoundsException {
+        if (index < 0 || index >= size) {
+            throw new ArrayIndexOutOfBoundsException("Index is less than 0");
+        }
+    }
+
+    private Node<T> searchNodeByIndex(int index) {
+        if ((size >> 1) >= index) {
+            Node<T> temp = head;
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+            return temp;
+        } else {
+            Node<T> temp = tail;
+            for (int i = 0; i < size - index - 1; i++) {
+                temp = temp.prev;
+            }
+            return temp;
+        }
+    }
+
+    private T removeNode(Node<T> node) {
+        if (node == head) {
+            if (size == 1) {
+                T headElement = head.element;
+                size--;
+                return headElement;
+            }
+            node.next.prev = null;
+            head = node.next;
+        } else if (node == tail) {
+            node.prev.next = null;
+            tail = node.prev;
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+        size--;
+        return node.element;
     }
 }
+
+
+
