5e543968bc1431ed2abf28585e023ce680e8cf18
AlinaPodoliaka <57722396+AlinaPodoliaka@users.noreply.github.com>
2019-12-16 15:59:59 +0200

    added hw-8-1-solution

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -1,50 +1,146 @@
 package core.basesyntax;
 
 import java.util.List;
+import java.util.Objects;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+
+    private Node<T> first;
+    private Node<T> last;
+    private int size;
+
     @Override
     public void add(T value) {
-
+        if (size == 0) {
+            first = new Node<>(null, value, null);
+            last = first;
+            size++;
+            return;
+        }
+        Node<T> current = new Node<>(null, value, null);
+        last.next = current;
+        current.previous = last;
+        last = current;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == size) {
+            add(value);
+            return;
+        }
+        Node<T> next = nodeAtIndex(index);
+        Node<T> previous = next.previous;
+        Node<T> current = new Node<>(previous, value, next);
+        if (previous == null) {
+            first = current;
+        } else {
+            previous.next = current;
+        }
+        next.previous = current;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (T element : list) {
+            add(element);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return nodeAtIndex(index).value;
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        nodeAtIndex(index).value = value;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> current = nodeAtIndex(index);
+        Node<T> previous = current.previous;
+        Node<T> next = current.next;
+        if (previous == null) {
+            first = next;
+        } else {
+            previous.next = next;
+        }
+        if (next == null) {
+            last = previous;
+        } else {
+            next.previous = previous;
+        }
+        size--;
+        return current.value;
     }
 
     @Override
     public T remove(T t) {
+        Node<T> current = first;
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(t, current.value)) {
+                return remove(i);
+            }
+            current = current.next;
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
+
+    }
+
+    private Node<T> nodeAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> current;
+        if (index < size / 2) {
+            current = first;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+        } else {
+            current = last;
+            for (int i = size - 1; i > index; i--) {
+                current = current.previous;
+            }
+        }
+        return current;
+    }
+
+    private static class Node<T> {
+        private T value;
+        private Node<T> next;
+        private Node<T> previous;
+
+        private Node(Node<T> prev, T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+            this.previous = prev;
+        }
     }
 }
