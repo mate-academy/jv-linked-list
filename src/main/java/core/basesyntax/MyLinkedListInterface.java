c373fe05a4de444378e821b9f5b8b3c5bcbb38e4
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2020-02-05 09:49:50 +0200

    rewrite linked list task and tests (#60)

--- src/main/java/core/basesyntax/MyLinkedListInterface.java
+++ src/main/java/core/basesyntax/MyLinkedListInterface.java
@@ -4,19 +4,19 @@ import java.util.List;
 
 public interface MyLinkedListInterface<T> {
 
-    void add(T value);
+    boolean add(T value);
 
     void add(T value, int index);
 
-    void addAll(List<T> list);
+    boolean addAll(List<T> list);
 
     T get(int index);
 
-    void set(T value, int index);
+    T set(T value, int index);
 
     T remove(int index);
 
-    T remove(T t);
+    boolean remove(T t);
 
     int size();
 
