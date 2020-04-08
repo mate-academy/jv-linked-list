8ede129e026d30ecde52048f25d194203a800830
ihor <krohmal.igor777@gmail.com>
2020-04-08 17:17:42 +0300

    optimized method add(T value,int index)

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -31,10 +31,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public void add(T value, int index) {
-        if (index < 0 || index > size) {
-            throw new ArrayIndexOutOfBoundsException();
-        }
-        if (size == 0 || index == size) {
+        if (index == size) {
             add(value);
             return;
         }
