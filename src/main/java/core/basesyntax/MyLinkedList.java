3ea672b8d1298df6448b3bb310bac89290b83c27
Oleh Kuzmenko <oleg24453@gmail.com>
2020-04-08 14:11:07 +0300

    added solution

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,48 +3,136 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node head;
+    private int size;
+
     @Override
     public boolean add(T value) {
-        return false;
+        if (isEmpty()) {
+            head = new Node(value, null, null);
+            size++;
+        } else {
+            getLast().next = new Node(value, getLast(), null);
+            size++;
+        }
+        return true;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (isEmpty()) {
+            add(value);
+        } else {
+            Node temp = new Node(value, getByIndex(index - 1), getByIndex(index - 1).next);
+            getByIndex(index - 1).next = temp;
+            if (temp.next != null) {
+                temp.next.prev = temp;
+            }
+            size++;
+        }
     }
 
     @Override
     public boolean addAll(List<T> list) {
-        return false;
+        for (T value : list) {
+            add(value);
+        }
+        return true;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        indexReview(index);
+        return getByIndex(index).item;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        indexReview(index);
+        T oldValue = get(index);
+        getByIndex(index).item = value;
+        return oldValue;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        indexReview(index);
+        Node removed = getByIndex(index);
+        if (removed.prev != null && removed.next != null) {
+            removed.prev.next = removed.next;
+            removed.next.prev = removed.prev;
+        } else if (removed.prev == null && removed.next != null) {
+            head = removed.next;
+            head.prev = null;
+        } else if (removed.prev != null && removed.next == null) {
+            removed.prev.next = null;
+        }
+        size--;
+        return removed.item;
     }
 
     @Override
     public boolean remove(T t) {
-        return false;
+        Node current = head;
+        int index = 1;
+        while (!(current.item == t || (t != null && t.equals(current.item))) && index < size) {
+            current = current.next;
+            index++;
+        }
+        if (current.item == t || (t != null && t.equals(current.item))) {
+            remove(--index);
+        } else {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private class Node {
+        private T item;
+        private Node prev;
+        private Node next;
+
+        public Node(T item, Node prev, Node next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private Node getLast() {
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    private void indexReview(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private Node getByIndex(int index) {
+        Node current = head;
+        int count = 0;
+        while (count < index) {
+            current = current.next;
+            count++;
+        }
+        return current;
     }
 }
