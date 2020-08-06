56ca567949e375d1b33a1ab671dfa495f75331fc
Ilya.Kvitkovskyi <ilya.kvitkovskyi@gmail.com>
2020-08-06 18:42:14 +0300

    added solution

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,48 +3,118 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node<T> header;
+    private int size;
+
+    public MyLinkedList() {
+        header = new Node<>(null, null, null);
+        header.next = header;
+        header.prev = header;
+        size = 0;
+    }
+
     @Override
     public boolean add(T value) {
-        return false;
+        add(value, size);
+        return true;
     }
 
     @Override
     public void add(T value, int index) {
-
+        Node<T> entryPoint = index == size ? header : entry(index);
+        Node<T> element = new Node<>(value, entryPoint, entryPoint.prev);
+        element.prev.next = element;
+        element.next.prev = element;
+        size++;
     }
 
     @Override
     public boolean addAll(List<T> list) {
-        return false;
+        for (T element : list) {
+            add(element);
+        }
+        return true;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        return entry(index).item;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        Node<T> element = entry(index);
+        T prevValue = element.item;
+        element.item = value;
+        return prevValue;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        Node<T> element = entry(index);
+        element.prev.next = element.next;
+        element.next.prev = element.prev;
+        element.next = null;
+        element.prev = null;
+        T value = element.item;
+        element.item = null;
+        size--;
+        return value;
     }
 
     @Override
     public boolean remove(T t) {
+        Node<T> element = header.next;
+        for (int i = 0; i < size; i++) {
+            if (t == element.item || t != null && t.equals(element.item)) {
+                remove(i);
+                return true;
+            }
+            element = element.next;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private Node<T> entry(int index) {
+        checkIndex(index);
+        Node<T> element = header;
+        if (index < (size >> 1)) {
+            for (int i = 0; i <= index; i++) {
+                element = element.next;
+            }
+        } else {
+            for (int i = size; i > index; i--) {
+                element = element.prev;
+            }
+        }
+        return element;
+    }
+
+    private void checkIndex(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayIndexOutOfBoundsException("Wrong index!");
+        }
+    }
+
+    private static class Node<T> {
+        private T item;
+        private Node<T> next;
+        private Node<T> prev;
+
+        private Node(T item, Node<T> next, Node<T> prev) {
+            this.item = item;
+            this.next = next;
+            this.prev = prev;
+        }
     }
 }
