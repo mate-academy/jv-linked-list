3e096a35520640261bc57a035b3f72b19a4e2d5f
Marina <maryna.voloshyna.11@gmail.com>
2020-08-09 18:19:28 +0300

    add solution

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,48 +3,172 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    transient int size;
+    transient Node<T> first;
+    transient Node<T> last;
+
+    private static class Node<T> {
+        T item;
+        Node<T> next;
+        Node<T> prev;
+
+        Node(Node<T> prev, T element, Node<T> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
     @Override
     public boolean add(T value) {
-        return false;
+        Node<T> before = last;
+        if (size == 1) {
+            before = first;
+        }
+        Node<T> node = new Node(before, value, null);
+        if (size == 0) {
+            first = node;
+        }
+        if (size >= 1) {
+            before.next = node;
+        }
+        last = node;
+        size++;
+
+        return true;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (isIndexGood(index) == true) {
+            if (index == size) {
+                add(value);
+            } else {
+                if (index == 0) {
+                    Node<T> node = new Node<>(null, value, first);
+                    first = node;
+                    size++;
+                } else {
+                    Node<T> node = new Node<>(findNodeWithIndex(index).prev,
+                            value, findNodeWithIndex(index));
+                    findNodeWithIndex(index).prev.next = node;
+                    findNodeWithIndex(index).prev = node;
+                    size++;
+                }
+            }
+            return;
+        }
+        throw new IndexOutOfBoundsException("Not right index");
     }
 
     @Override
     public boolean addAll(List<T> list) {
-        return false;
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
+        return true;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (isIndexGood(index) != true) {
+            throw new IndexOutOfBoundsException("Wrong index");
+        }
+        if (index == size) {
+            throw new IndexOutOfBoundsException("Wrong index");
+        }
+        return findNodeWithIndex(index).item;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        if (isIndexGood(index) != true) {
+            throw new IndexOutOfBoundsException("Wring index");
+        }
+        if (index == size) {
+            throw new IndexOutOfBoundsException("Wring index");
+        }
+        T valueBeforeSetting = findNodeWithIndex(index).item;
+        findNodeWithIndex(index).item = value;
+        return valueBeforeSetting;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (isIndexGood(index) != true) {
+            throw new IndexOutOfBoundsException("Wrong index");
+        }
+        if (index == size) {
+            throw new IndexOutOfBoundsException("Wrong index");
+        }
+        Node<T> node = findNodeWithIndex(index);
+        T item = node.item;
+        if (index == size - 1 && index == 0) {
+            first.item = null;
+            last.item = null;
+            size--;
+        } else if (index == 0 && index != size - 1) {
+            node.next.prev = null;
+            first = node.next;
+            node.next = null;
+            node.item = null;
+            size--;
+        } else if (index == size - 1 && index != 0) {
+            node.prev.next = null;
+            last = node.prev;
+            node.prev = null;
+            node.item = null;
+            size--;
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+            unlinkNode(node);
+            size--;
+        }
+        return item;
     }
 
     @Override
     public boolean remove(T t) {
+        Node<T> node = first;
+        for (int i = 0; i < size; i++) {
+            if (node.item == t || (node.item != null && node.item.equals(t))) {
+                remove(i);
+                return true;
+            }
+            node = node.next;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    public boolean isIndexGood(int index) {
+        return (index <= size && index >= 0);
+    }
+
+    public Node<T> findNodeWithIndex(int index) {
+        Node<T> nodeWithIndex = first;
+        for (int i = 0; i < index; i++) {
+            nodeWithIndex = nodeWithIndex.next;
+        }
+        return nodeWithIndex;
+    }
+
+    public Node<T> unlinkNode(Node<T> node) {
+
+        node.prev = null;
+        node.next = null;
+        node.item = null;
+        return node;
     }
 }
+
