151366b830390f1e0ac466d4f3444291f6fb873a
Borisko130 <krimenyukb@gmail.com>
2020-08-09 22:40:56 +0300

    Homework 9 completed

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,48 +3,145 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+
+    private int listSize;
+    private Node<T> headNode;
+    private Node<T> tailNode;
+
+    MyLinkedList() {
+        headNode = new Node<>(null, null, null);
+        tailNode = new Node<>(null, headNode, null);
+        headNode.next = tailNode;
+    }
+
     @Override
     public boolean add(T value) {
-        return false;
+        Node<T> newNode = new Node<>(value, null, null);
+        insertNode(newNode, tailNode);
+        return true;
     }
 
     @Override
     public void add(T value, int index) {
-
+        checkIndexForAdd(index);
+        Node<T> newNode = new Node<>(value, null, null);
+        insertNode(newNode, getNodeByIndex(index));
     }
 
     @Override
     public boolean addAll(List<T> list) {
-        return false;
+        for (T elem : list) {
+            add(elem);
+        }
+        return true;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndexForRestFunctions(index);
+        Node<T> currentNode = getNodeByIndex(index);
+        return currentNode.element;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        checkIndexForRestFunctions(index);
+        T oldValue = getNodeByIndex(index).element;
+        getNodeByIndex(index).element = value;
+        return oldValue;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndexForRestFunctions(index);
+        Node<T> nodeToRemove = getNodeByIndex(index);
+        nodeToRemove.prev.next = nodeToRemove.next;
+        nodeToRemove.next.prev = nodeToRemove.prev;
+        listSize--;
+        return nodeToRemove.element;
     }
 
     @Override
     public boolean remove(T t) {
+        Node<T> nodeToRemove = headNode.next;
+        for (int i = 0; i < listSize; i++) {
+            if (nodeToRemove.element == t || (t != null && nodeToRemove.element.equals(t))) {
+                nodeToRemove.prev.next = nodeToRemove.next;
+                nodeToRemove.next.prev = nodeToRemove.prev;
+                listSize--;
+                return true;
+            }
+            nodeToRemove = nodeToRemove.next;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return listSize;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return listSize == 0;
+    }
+
+    private void insertNode(Node<T> newNode, Node<T> currentNode) {
+        currentNode.prev.next = newNode; //  <prev> -> <new>
+        newNode.prev = currentNode.prev; // <prev> <-> <new>
+        newNode.next = currentNode; // <prev> <-> <new> -> <cur>
+        currentNode.prev = newNode; // <prev> <-> <new> <-> <cur>
+        listSize++;
+    }
+
+    private Node<T> getNodeByIndex(int index) {
+        if (listSize == 0) {
+            return tailNode;
+        }
+        if ((index <= listSize >> 1) || listSize <= 2) {
+            return getDistantNext(index, headNode);
+        } else {
+            return getDistantPrev(listSize - index - 1, tailNode);
+        }
+    }
+
+    private Node<T> getDistantNext(int index, Node<T> node) {
+        if (index <= 0) {
+            return node.next;
+        }
+        index--;
+        return getDistantNext(index, node.next);
+    }
+
+    private Node<T> getDistantPrev(int index, Node<T> node) {
+        if (index <= 0) {
+            return node.prev;
+        }
+        index--;
+        return getDistantPrev(index, node.prev);
+    }
+
+    private void checkIndexForAdd(int index) {
+        if (index > listSize || index < 0) {
+            throw new ArrayIndexOutOfBoundsException("List has no such index");
+        }
+    }
+
+    private void checkIndexForRestFunctions(int index) {
+        if (index >= listSize || index < 0) {
+            throw new ArrayIndexOutOfBoundsException("List has no such index");
+        }
+    }
+
+    private static class Node<T> {
+        public T element;
+        public Node<T> prev;
+        public Node<T> next;
+
+        public Node(T element, Node<T> prev, Node<T> next) {
+            this.element = element;
+            this.prev = prev;
+            this.next = next;
+        }
     }
 }
