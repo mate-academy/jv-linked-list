6d37e754e5b774d2cc987adb32b5966337b7b58c
Roman <matviichuk.office@gmail.com>
2020-08-10 11:21:49 +0300

    maked constructor private too

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -81,7 +81,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         private T element;
         private Node<T> next;
 
-        public Node(Node<T> prev, T element, Node<T> next) {
+        private Node(Node<T> prev, T element, Node<T> next) {
             this.prev = prev;
             this.element = element;
             this.next = next;
