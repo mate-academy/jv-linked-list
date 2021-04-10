a19113eb3fb814087de03a2e8c29e0f5bb2672cb
safordog <safordog@gmail.com>
2021-04-10 11:25:14 +0300

    removed setters and getters, renamed variable and removed <if> statement in getNodeByItem() method

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -16,7 +16,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             last = first;
         } else {
             Node<T> newNode = new Node(last, value, null);
-            last.setNext(newNode);
+            last.next = newNode;
             last = newNode;
         }
         size++;
@@ -33,13 +33,13 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             return;
         }
         Node<T> nodeWithInputIndex = getNodeNodeByIndex(index);
-        Node<T> newNode = new Node(nodeWithInputIndex.getPrev(), value, nodeWithInputIndex);
-        nodeWithInputIndex.setPrev(newNode);
+        Node<T> newNode = new Node(nodeWithInputIndex.prev, value, nodeWithInputIndex);
+        nodeWithInputIndex.prev = newNode;
         if (index == 0) {
             first = newNode;
         }
         if (index != 0) {
-            newNode.getPrev().setNext(newNode);
+            newNode.prev.next = newNode;
         }
         size++;
     }
@@ -58,7 +58,48 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             throw new IndexOutOfBoundsException(MESSAGE_INDEX_OUT_OF_BOUNDS_EXCEPTION);
         }
         Node<T> nodeWithInputIndex = getNodeNodeByIndex(index);
-        return nodeWithInputIndex.getItem();
+        return nodeWithInputIndex.item;
+    }
+
+    @Override
+    public T set(T value, int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException(MESSAGE_INDEX_OUT_OF_BOUNDS_EXCEPTION);
+        }
+        Node<T> actualNode = getNodeNodeByIndex(index);
+        T oldValue = actualNode.item;
+        actualNode.item = value;
+        return oldValue;
+    }
+
+    @Override
+    public T remove(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException(MESSAGE_INDEX_OUT_OF_BOUNDS_EXCEPTION);
+        }
+        Node<T> nodeForDeletion = getNodeNodeByIndex(index);
+        unlinkNode(nodeForDeletion);
+        return nodeForDeletion.item;
+    }
+
+    @Override
+    public boolean remove(T item) {
+        Node<T> nodeForDeletion = getNodeByItem(item);
+        if (nodeForDeletion == null) {
+            return false;
+        }
+        unlinkNode(nodeForDeletion);
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
     }
 
     private Node<T> getNodeNodeByIndex(int index) {
@@ -73,7 +114,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         int indexCounter = 0;
         Node<T> nodeWithInputIndex = first;
         while (indexCounter < index) {
-            nodeWithInputIndex = nodeWithInputIndex.getNext();
+            nodeWithInputIndex = nodeWithInputIndex.next;
             indexCounter++;
         }
         return nodeWithInputIndex;
@@ -83,41 +124,10 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         int indexCounter = size - 1;
         Node<T> nodeWithInputIndex = last;
         while (indexCounter > index) {
-            nodeWithInputIndex = nodeWithInputIndex.getPrev();
+            nodeWithInputIndex = nodeWithInputIndex.prev;
             indexCounter--;
         }
         return nodeWithInputIndex;
-    }
-
-    @Override
-    public T set(T value, int index) {
-        if (index >= size || index < 0) {
-            throw new IndexOutOfBoundsException(MESSAGE_INDEX_OUT_OF_BOUNDS_EXCEPTION);
-        }
-        Node<T> actualNode = getNodeNodeByIndex(index);
-        T oldValue = actualNode.getItem();
-        actualNode.setItem(value);
-        return oldValue;
-    }
-
-    @Override
-    public T remove(int index) {
-        if (index >= size || index < 0) {
-            throw new IndexOutOfBoundsException(MESSAGE_INDEX_OUT_OF_BOUNDS_EXCEPTION);
-        }
-        Node<T> nodeForDeletion = getNodeNodeByIndex(index);
-        unlinkNode(nodeForDeletion);
-        return nodeForDeletion.getItem();
-    }
-
-    @Override
-    public boolean remove(T item) {
-        Node<T> nodeForDeletion = getNodeByItem(item);
-        if (nodeForDeletion == null) {
-            return false;
-        }
-        unlinkNode(nodeForDeletion);
-        return true;
     }
 
     private void unlinkNode(Node<T> nodeForDeletion) {
@@ -126,43 +136,30 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         } else if (nodeForDeletion == last) {
             deleteLastNode(nodeForDeletion);
         } else {
-            nodeForDeletion.getNext().setPrev(nodeForDeletion.getPrev());
-            nodeForDeletion.getPrev().setNext(nodeForDeletion.getNext());
+            nodeForDeletion.next.prev = nodeForDeletion.prev;
+            nodeForDeletion.prev.next = nodeForDeletion.next;
         }
         size--;
     }
 
     private void deleteLastNode(Node<T> nodeForDeletion) {
-        nodeForDeletion.getPrev().setNext(null);
-        last = nodeForDeletion.getPrev();
+        nodeForDeletion.prev.next = null;
+        last = nodeForDeletion.prev;
     }
 
     private void deleteFirstNode(Node<T> nodeForDeletion) {
-        first = nodeForDeletion.getNext();
+        first = nodeForDeletion.next;
         if (first != null) {
-            nodeForDeletion.getNext().setPrev(null);
+            nodeForDeletion.next.prev = null;
         }
     }
 
     private Node<T> getNodeByItem(T item) {
-        Node<T> nodeForDeletion = first;
-        while (nodeForDeletion != null) {
-            if (nodeForDeletion.equalsItems(item)) {
-                break;
-            }
-            nodeForDeletion = nodeForDeletion.getNext();
+        Node<T> node = first;
+        while (node != null && !node.equalsItems(item)) {
+            node = node.next;
         }
-        return nodeForDeletion;
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
+        return node;
     }
 
     private static class Node<T> {
@@ -180,32 +177,8 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             this.next = next;
         }
 
-        public T getItem() {
-            return item;
-        }
-
-        public void setItem(T item) {
-            this.item = item;
-        }
-
-        public Node<T> getNext() {
-            return next;
-        }
-
-        public void setNext(Node<T> next) {
-            this.next = next;
-        }
-
-        public Node<T> getPrev() {
-            return prev;
-        }
-
-        public void setPrev(Node<T> prev) {
-            this.prev = prev;
-        }
-
         private boolean equalsItems(T newItem) {
-            return (newItem == this.getItem() || newItem != null && newItem.equals(this.getItem()));
+            return (newItem == this.item || newItem != null && newItem.equals(this.item));
         }
     }
 }
