cf6c461d2b490f6f8bdf966497fc78d098af30d7
Veronika <koraf9712@gmail.com>
2021-04-13 15:36:51 +0300

    Fixed mistakes

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,37 +3,37 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
-    private Node<T> headElement;
-    private Node<T> tailElement;
+    private Node<T> head;
+    private Node<T> tail;
     private int size;
-
-    private static class Node<T> {
-        private T value;
-        private Node<T> prev;
-        private Node<T> next;
-
-        public Node(Node<T> prev, T value, Node<T> next) {
-            this.prev = prev;
-            this.value = value;
-            this.next = next;
-
-        }
-    }
 
     @Override
     public boolean add(T value) {
-        setTailElement(value);
+        if (head == null) {
+            setHead(value);
+        } else {
+            setTail(value);
+        }
+        size++;
         return true;
     }
 
     @Override
     public void add(T value, int index) {
-        chekIndex(index, size);
         if (index == size) {
-            setTailElement(value);
-        } else {
-            getElementBefore(value, getNode(index));
+            add(value);
+            return;
         }
+        chekIndex(index);
+        if (index == 0) {
+            setHead(value);
+        } else {
+            Node<T> currentNode = getNode(index);
+            Node<T> newNode = new Node<>(currentNode.prev, value, currentNode);
+            currentNode.prev.next = newNode;
+            currentNode.prev = newNode;
+        }
+        size++;
     }
 
     @Override
@@ -46,13 +46,13 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public T get(int index) {
-        chekIndex(index, size - 1);
+        chekIndex(index);
         return getNode(index).value;
     }
 
     @Override
     public T set(T value, int index) {
-        chekIndex(index, size - 1);
+        chekIndex(index);
         Node<T> node = getNode(index);
         T oldNodeValue = node.value;
         node.value = value;
@@ -61,13 +61,13 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public T remove(int index) {
-        chekIndex(index, size - 1);
+        chekIndex(index);
         return unlink(getNode(index));
     }
 
     @Override
     public boolean remove(T object) {
-        Node<T> node = headElement;
+        Node<T> node = head;
         for (int i = 0; i < size; i++) {
             if (node.value != null && node.value.equals(object) || node.value == object) {
                 unlink(node);
@@ -88,33 +88,37 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return size == 0;
     }
 
-    private void chekIndex(int index, int size) {
-        if (index < 0 || index > size) {
+    private void chekIndex(int index) {
+        if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Index out of bounds");
         }
     }
 
-    private void setTailElement(T element) {
-        final Node<T> lastNode = tailElement;
-        final Node<T> newNode = new Node<>(lastNode, element, null);
-        tailElement = newNode;
-        if (lastNode == null) {
-            headElement = newNode;
+    private void setHead(T element) {
+        if (head == null) {
+            Node<T> newNode = new Node<>(null, element, null);
+            head = newNode;
+            tail = newNode;
         } else {
-            lastNode.next = newNode;
+            head = new Node<>(null, element, head);
         }
-        size++;
+    }
+
+    private void setTail(T element) {
+        Node<T> newNode = new Node<>(tail, element, null);
+        tail.next = newNode;
+        tail = newNode;
     }
 
     private Node<T> getNode(int index) {
         Node<T> neededNode;
-        if (index < size / 2) {
-            neededNode = headElement;
+        if (index <= size / 2) {
+            neededNode = head;
             for (int i = 0; i < index; i++) {
                 neededNode = neededNode.next;
             }
         } else {
-            neededNode = tailElement;
+            neededNode = tail;
             for (int i = size - 1; i > index; i--) {
                 neededNode = neededNode.prev;
             }
@@ -122,39 +126,31 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return neededNode;
     }
 
-    private void getElementBefore(T value, Node<T> element) {
-        Node<T> prevElement = element.prev;
-        Node<T> newElement = new Node<>(prevElement, value, element);
-        element.prev = newElement;
-        if (prevElement == null) {
-            headElement = newElement;
-        } else {
-            prevElement.next = newElement;
+    private T unlink(Node<T> unlinkNode) {
+        if (head.equals(unlinkNode)) {
+            if (size == 1) {
+                head = null;
+                tail = null;
+            }
+            head = unlinkNode.next;
+        } else if (unlinkNode.next != null) {
+            unlinkNode.next.prev = unlinkNode.prev;
+            unlinkNode.prev.next = unlinkNode.next;
         }
-        size++;
+        size--;
+        return unlinkNode.value;
     }
 
-    private T unlink(Node<T> unlinkNode) {
-        Node<T> prev = unlinkNode.prev;
-        Node<T> next = unlinkNode.next;
+    private static class Node<T> {
+        private T value;
+        private Node<T> prev;
+        private Node<T> next;
 
-        if (prev == null) {
-            headElement = next;
-        } else {
-            prev.next = next;
-            unlinkNode.prev = null;
+        public Node(Node<T> prev, T value, Node<T> next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+
         }
-
-        if (next == null) {
-            tailElement = prev;
-        } else {
-            next.prev = prev;
-            unlinkNode.next = null;
-        }
-
-        T value = unlinkNode.value;
-        unlinkNode.value = null;
-        size--;
-        return value;
     }
 }
