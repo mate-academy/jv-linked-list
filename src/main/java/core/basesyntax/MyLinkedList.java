4ef129536e8f973114572476fa05f0f750c6f525
apyavchik <81320602+apyavchik@users.noreply.github.com>
2021-05-06 16:59:08 +0300

    Add get and add methods

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,21 +3,56 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
     @Override
     public void add(T value) {
+        if (size == 0) {
+            head = new Node<>(null, value, null);
+            tail = head;
+            ++size;
+        }
+        Node<T> newNode = new Node<>(tail, value, null);
+        tail.next = newNode;
+        tail = newNode;
+        ++size;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index == size) {
+            add(value);
+            return;
+        }
+        if (index == 0) {
+            Node<T> newNode = new Node<>(null, value, head);
+            head.prev = newNode;
+            head = newNode;
+            size++;
+            return;
+        }
+        checkIndex(index);
+        Node<T> newNode = findNode(index);
+        Node<T> add = new Node<>(newNode.prev, value, newNode);
+        newNode.prev.next = add;
+        newNode.prev = add;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T element: list) {
+            add(element);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        Node<T> newNode = findNode(index);
+        return newNode.value;
     }
 
     @Override
@@ -44,4 +79,28 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     public boolean isEmpty() {
         return false;
     }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Not correct index " + index);
+        }
+    }
+
+    private Node<T> findNode(int index) {
+        Node<T> newNode;
+        if (index < size) {
+            newNode = head;
+            for (int i = 0; i < index; i++) {
+                newNode = newNode.next;
+            }
+            return newNode;
+        } else {
+            newNode = tail;
+            for (int i = size - 1; i > index; i--) {
+                newNode = newNode.prev;
+            }
+            return newNode;
+        }
+    }
+
 }
---
+++ src/main/java/core/basesyntax/Node.java
@@ -0,0 +1,37 @@
+package core.basesyntax;
+
+class Node<T> {
+    public Node<T> prev;
+    public T value;
+    public Node<T> next;
+
+    public Node(Node<T> prev, T value, Node<T> next) {
+        this.prev = prev;
+        this.value = value;
+        this.next = next;
+    }
+
+    public Node<T> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<T> prev) {
+        this.prev = prev;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
