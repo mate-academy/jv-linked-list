06cd92671cd92bb384180b35ce143b7c296187a3
Benito <zara.benito17@gmail.com>
2021-06-14 12:39:59 +0300

    Added solution

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,45 +3,148 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node<T> firstNode;
+    private Node<T> lastNode;
+    private int size;
+
+    public static class Node<E> {
+        private E item;
+        private Node<E> prev;
+        private Node<E> next;
+
+        public Node(Node<E> prev, E item, Node<E> next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
     @Override
     public void add(T value) {
+        lastNode = new Node<>(lastNode, value, null);
+        if (size == 0) {
+            firstNode = lastNode;
+        }
+        if (size > 0) {
+            lastNode.prev.next = lastNode;
+        }
+        if (size == 1) {
+            firstNode.next = lastNode;
+        }
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Index is invalid, current size of list: " + size);
+        }
+        Node<T> newNode;
+        if (index == size) {
+            add(value);
+        } else if (index == 0) {
+            newNode = new Node<>(null, value, firstNode);
+            firstNode.prev = newNode;
+            firstNode = newNode;
+            size++;
+        } else {
+            Node<T> node = checkPosition(index);
+            newNode = new Node<>(node.prev, value, node);
+            node.prev.next = newNode;
+            node.prev = newNode;
+            size++;
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T value : list) {
+            add(value);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        Node<T> node = checkPosition(index);
+        return node.item;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        checkIndex(index);
+        Node<T> node = checkPosition(index);
+        T oldItem = node.item;
+        node.item = value;
+        return oldItem;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        Node<T> oldNode = checkPosition(index);
+        System.out.println(oldNode);
+        unlink(oldNode);
+        return oldNode.item;
     }
 
     @Override
     public boolean remove(T object) {
+        Node<T> node = firstNode;
+        for (int i = 0; i < size; i++) {
+            if (node.item == object || node.item != null && node.item.equals(object)) {
+                unlink(node);
+                return true;
+            } else {
+                node = node.next;
+            }
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void unlink(Node<T> node) {
+        if (firstNode == node) {
+            firstNode = firstNode.next;
+        } else if (lastNode == node) {
+            lastNode = lastNode.prev;
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+        size--;
+    }
+
+    private void checkIndex(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index is invalid, current size of list: " + size);
+        }
+    }
+
+    private Node<T> checkPosition(int index) {
+        Node<T> node = firstNode;
+        if (index == 0) {
+            return node;
+        }
+        if (index < size / 2) {
+            for (int i = 0; i < index; i++) {
+                node = node.next;
+            }
+        } else {
+            node = lastNode;
+            for (int i = size - 1; i > index; i--) {
+                node = node.prev;
+            }
+        }
+        return node;
     }
 }
