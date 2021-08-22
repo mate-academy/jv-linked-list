e93d80077b9ac27ebfc2f740b49df657bfe076b3
mishamolnar <michaellmuller423@gmail.com>
2021-08-22 17:45:48 +0300

    add function  works except test Add by Index to the Bottom

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -1,14 +1,42 @@
 package core.basesyntax;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private int size = 0;
+    private Node<T> first;
+    private Node<T> last;
+
     @Override
     public void add(T value) {
+        if (size == 0) {
+            first = new Node(null, value, null);
+            last = first;
+            size++;
+        } else if (size > 0) {
+            last = new Node(getNodeByIndex(size - 1), value, null);
+            getNodeByIndex(size - 1).next = last;
+            size++;
+        }
     }
 
     @Override
     public void add(T value, int index) {
+        indexCheck(index);
+        if (index == size) {
+            add(value);
+        }
+        if (index == 0) {
+            first = new Node(null, value, first);
+            getNodeByIndex(1).prev = first;
+            size++;
+        }
+        Node<T> newNode = new Node(getNodeByIndex(index).prev, value, getNodeByIndex(index - 1).next);
+        // try after test change getNodeByIndex(index - 1).next -->  (Node) getNodeByIndex(index).item
+        getNodeByIndex(index - 1).next = newNode;
+        getNodeByIndex(index + 1).prev = newNode;
+        size++;
     }
 
     @Override
@@ -17,7 +45,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public T get(int index) {
-        return null;
+        return getNodeByIndex(index).item;
     }
 
     @Override
@@ -37,11 +65,37 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
         return false;
+    }
+
+    private Node<T> getNodeByIndex(int index) {
+        Node<T> bufferNode = first;
+        for (int i = 0; i < index; i++) {
+            bufferNode = bufferNode.next;
+        }
+        return bufferNode;
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    private void indexCheck(int index) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("This index does not exist");
+        }
     }
 }
