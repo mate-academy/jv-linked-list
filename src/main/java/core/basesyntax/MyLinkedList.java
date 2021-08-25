0f966775e9fea8f3a53e5f98ee043874b62fb9dd
Viktor Yukhno <pingvinomania@gmail.com>
2021-08-26 01:38:10 +0300

    added fix after Java coach review

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -21,19 +21,14 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public void add(T value) {
-        if (isEmpty()) {
-            addIfFirst(value);
-        } else {
-            addIfLast(value);
-        }
-        size++;
+        add(value, size);
     }
 
     @Override
     public void add(T value, int index) {
-        checkIndex(index);
+        checkIndexForAdd(index);
         if (index == size) {
-            addIfLast(value);
+            addLast(value);
         } else {
             Node<T> standStill = searchNode(index).prev;
             Node<T> newNode = new Node<>(standStill, value, searchNode(index));
@@ -49,34 +44,20 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public void addAll(List<T> list) {
-        T[] inputArray = (T[]) list.toArray();
-        int lengthInputArray = inputArray.length;
-        if (lengthInputArray == 0) {
-            return;
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
         }
-        Node<T> standStill = last;
-        for (T element : inputArray) {
-            Node<T> newNode = new Node<>(standStill, element, null);
-            if (standStill == null) {
-                first = newNode;
-            } else {
-                standStill.next = newNode;
-            }
-            standStill = newNode;
-        }
-        last = standStill;
-        size += lengthInputArray;
     }
 
     @Override
     public T get(int index) {
-        checkIndexForGetAndSet(index);
+        checkIndex(index);
         return searchNode(index).item;
     }
 
     @Override
     public T set(T value, int index) {
-        checkIndexForGetAndSet(index);
+        checkIndex(index);
         Node<T> foundNode = searchNode(index);
         T oldValue = foundNode.item;
         foundNode.item = value;
@@ -85,25 +66,19 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public T remove(int index) {
-        checkIndexForGetAndSet(index);
+        checkIndex(index);
         return unlink(searchNode(index));
     }
 
     @Override
     public boolean remove(T value) {
-        if (value == null) {
-            for (Node<T> node = first; node != null; node = node.next) {
-                if (node.item == null) {
-                    unlink(node);
-                    return true;
-                }
-            }
-        } else {
-            for (Node<T> node = first; node != null; node = node.next) {
-                if (value.equals(node.item)) {
-                    unlink(node);
-                    return true;
-                }
+        for (Node<T> node = first; node != null; node = node.next) {
+            if (node.item == null && value == null) {
+                unlink(node);
+                return true;
+            } else if (value != null && value.equals(node.item)) {
+                unlink(node);
+                return true;
             }
         }
         return false;
@@ -119,15 +94,9 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return size() == 0;
     }
 
-    private void addIfFirst(T value) {
-        Node<T> newNode = new Node<>(null, value, null);
-        first = newNode;
-        last = newNode;
-    }
-
-    private void addIfLast(T value) {
+    private void addLast(T value) {
         Node<T> previousListElement = last;
-        Node<T> newNode = new Node<>(previousListElement, value, null);
+        Node<T> newNode = new Node<>(last, value, null);
         last = newNode;
         if (previousListElement == null) {
             first = newNode;
@@ -136,7 +105,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         }
     }
 
-    private void checkIndex(int index) {
+    private void checkIndexForAdd(int index) {
         if (!(index >= 0 && index <= size)) {
             throw new IndexOutOfBoundsException("You used an incorrect index");
         }
@@ -144,21 +113,21 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     private Node<T> searchNode(int index) {
         if (index > (size / 2)) {
-            Node<T> searchIndex = last;
+            Node<T> soughtNode = last;
             for (int i = size - 1; i > index; i--) {
-                searchIndex = searchIndex.prev;
+                soughtNode = soughtNode.prev;
             }
-            return searchIndex;
+            return soughtNode;
         } else {
-            Node<T> searchIndex = first;
+            Node<T> soughtNode = first;
             for (int i = 0; i < index; i++) {
-                searchIndex = searchIndex.next;
+                soughtNode = soughtNode.next;
             }
-            return searchIndex;
+            return soughtNode;
         }
     }
 
-    private void checkIndexForGetAndSet(int index) {
+    private void checkIndex(int index) {
         if (!(index >= 0 && index < size)) {
             throw new IndexOutOfBoundsException("You used an incorrect index. Element not found");
         }
