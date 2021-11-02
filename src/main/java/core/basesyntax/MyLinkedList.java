be93fca92396e56642a16ab2396d3f6fc615e243
Volodimir <vova.stepanenko2020@gmail.com>
2021-11-02 13:55:12 +0200

    Add base fields MyLinkedList and Node inner class

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,6 +3,30 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private int size;
+    private Node<T> first;
+    private Node<T> last;
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+        private Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        public E getItem() {
+            return item;
+        }
+
+        public void setItem(E value) {
+            item = value;
+        }
+    }
+
     @Override
     public void add(T value) {
     }
@@ -27,7 +51,10 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        Node<T> nodeByIndex = getNodeByIndex(index);
+        removeNode(nodeByIndex);
+        return nodeByIndex.getItem();
     }
 
     @Override
