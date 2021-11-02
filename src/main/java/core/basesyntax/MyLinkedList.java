44fe184caa87b0ad3556139911ef85db96ebc489
Vladyslav Chyver <lione2009lione@gmail.com>
2021-11-02 22:08:23 +0200

    First programm

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,13 +3,48 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private static class Node<T> {
+        T item;
+        Node<T> prev;
+        Node<T> next;
+
+        Node(Node<T> prev,T item, Node<T> next){
+            this.prev = prev;
+            this.item = item;
+            this.next = next;
+        }
+    }
+    private Node<T> tail;
+    private Node<T> head;
+    private int size;
+
+    public MyLinkedList(){
+    }
+
     @Override
     public void add(T value) {
+        Node<T> node = new Node<T>(tail, value, null);
+        if (tail != null) {
+            tail.next = node;
+        }
+        tail = node;
+        if(head == null) {
+            head = node;
+        }
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-    }
+
+        Node<T> node = new Node<T>(tail, value, null);
+        chekForTrueIndex(index);
+        if(index == size - 1) {
+            createNewNode(value);
+        } else {
+            getNodefromIndex(index);
+        }
+        }
 
     @Override
     public void addAll(List<T> list) {
@@ -17,12 +52,13 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public T get(int index) {
-        return null;
+        return getItemFromIndex(index);
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        chekForTrueIndex(index);
+        return setItemToNode(index, value);
     }
 
     @Override
@@ -37,11 +73,55 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void createNewNode(T item) {
+        Node<T> node = new Node<T>(tail, item, null);
+        if (tail != null) {
+            tail.next = node;
+        }
+        tail = node;
+        if(head == null) {
+            head = node;
+        }
+        size++;
+    }
+
+    private void chekForTrueIndex(int index) {
+        if (index < 0 && index > size()-1) {
+            throw new RuntimeException("Index not found");
+        }
+    }
+
+    private Node<T> getNodefromIndex(int index) {
+        Node<T> nodeFromIndex;
+        for (int i = 0; i <= index; i++) {
+            nodeFromIndex = head.next;
+            if (i == index) {
+                return nodeFromIndex;
+            }
+        }
+        return null;
+    }
+
+    private T getItemFromIndex(int index) {
+            return getNodefromIndex(index).item;
+    }
+
+    private T setItemToNode(int index, T value) {
+        Node<T> node;
+        for (int i = 0; i <= index; i++) {
+            node = head.next;
+            if (i == index) {
+                node.item = value;
+            }
+        }
+        return null;
     }
 }
