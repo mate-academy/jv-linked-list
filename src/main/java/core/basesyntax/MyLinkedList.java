a8a668575f443925d98376ae6f7b6b0fd332ceeb
Admin Oleksandr <maslo5@meta.ua>
2021-11-03 16:40:21 +0200

    Added solution

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,45 +3,152 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private int size;
+    private Node<T> first;
+    private Node<T> last;
+
+    private static class Node<T> {
+        private T item;
+        private Node<T> next;
+        private Node<T> prev;
+
+        Node(Node<T> prev, T element, Node<T> next) {
+            this.item = element;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private boolean isValidIndex(int index) {
+        return index >= 0 && index <= size;
+    }
+
+    private Node<T> findNodeByIndex(int index) {
+        if (index == size || !isValidIndex(index)) {
+            throw new IndexOutOfBoundsException(" The index " + index + " is't valid");
+        }
+        if (index < (size >> 1)) {
+            Node<T> currentNode = first;
+            for (int i = 0; i < index; i++) {
+                currentNode = currentNode.next;
+            }
+            return currentNode;
+        } else {
+            Node<T> currentNode = last;
+            for (int i = size - 1; i > index; i--) {
+                currentNode = currentNode.prev;
+            }
+            return currentNode;
+        }
+    }
+
+    private T unlink(Node<T> currentNode) {
+        Node<T> nextNode = currentNode.next;
+        Node<T> prevNode = currentNode.prev;
+        if (prevNode == null) {
+            first = nextNode;
+        } else {
+            prevNode.next = nextNode;
+            currentNode.prev = null;
+        }
+        if (nextNode == null) {
+            last = prevNode;
+        } else {
+            nextNode.prev = prevNode;
+            currentNode.next = null;
+        }
+        T element = currentNode.item;
+        currentNode.item = null;
+        size--;
+        return element;
+    }
+
     @Override
     public void add(T value) {
+        Node<T> lastNode = last;
+        Node<T> newNode = new Node<>(last, value, null);
+        last = newNode;
+        if (lastNode == null) {
+            first = newNode;
+        } else {
+            lastNode.next = newNode;
+        }
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (!isValidIndex(index)) {
+            throw new IndexOutOfBoundsException(" The index " + index + " is't valid");
+        }
+        if (index == size) {
+            add(value);
+        } else if (index == 0) {
+            size++;
+            first = new Node<>(null, value, first);
+            first.next.prev = first;
+        } else {
+            size++;
+            Node<T> newNextNode = findNodeByIndex(index + 1);
+            Node<T> newPrevNode = newNextNode.prev;
+            Node<T> newNode = new Node<>(newPrevNode, value, newNextNode);
+            newNextNode.prev = newNode;
+            newPrevNode.next = newNode;
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T element : list) {
+            add(element);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (!isValidIndex(index)) {
+            throw new IndexOutOfBoundsException(" The index " + index + " is't valid");
+        }
+        return findNodeByIndex(index).item;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        if (!isValidIndex(index)) {
+            throw new IndexOutOfBoundsException(" The index " + index + " is't valid");
+        }
+        Node<T> currentNode = findNodeByIndex(index);
+        T oldValue = currentNode.item;
+        currentNode.item = value;
+        return oldValue;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (!isValidIndex(index)) {
+            throw new IndexOutOfBoundsException(" The index " + index + " is't valid");
+        }
+        return unlink(findNodeByIndex(index));
     }
 
     @Override
     public boolean remove(T object) {
+        for (Node<T> currentNode = first; currentNode != null; currentNode = currentNode.next) {
+            if (currentNode.item == object || object != null && object.equals(currentNode.item)) {
+                unlink(currentNode);
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
