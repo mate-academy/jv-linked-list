438b6c6b4506ba98314bebb9fbb8e0272630e072
farfardesu <savavlad003@gmail.com>
2021-11-03 16:09:08 +0200

    suggested changes were implemented

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -28,6 +28,123 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         }
     }
 
+    @Override
+    public void addAll(List<T> list) {
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
+    }
+
+    @Override
+    public T get(int index) {
+        return findNodeByIndex(index).value;
+    }
+
+    @Override
+    public T set(T value, int index) {
+        checkIndex(index, size - 1);
+        Node<T> tempNode = findNodeByIndex(index);
+        T res = tempNode.value;
+        tempNode.value = value;
+        return res;
+    }
+
+    @Override
+    public T remove(int index) {
+        checkIndex(index, size);
+        Node<T> tempNode = findNodeByIndex(index);
+        T res = tempNode.value;
+        unlink(tempNode);
+        return res;
+    }
+
+    @Override
+    public boolean remove(T object) {
+        Node<T> tempNode = findEqualObj(object);
+        if (tempNode == null) {
+            return false;
+        }
+        unlink(tempNode);
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private static class Node<T> {
+        private T value;
+        private Node<T> next;
+        private Node<T> prev;
+
+        Node(Node<T> prev, T element, Node<T> next) {
+            this.value = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    private void addLast(T value) {
+        Node<T> tempNode = new Node<>(last, value, null);
+        last.next = tempNode;
+        last = tempNode;
+        size++;
+    }
+
+    private void addFirst(T value) {
+        first = new Node(null, value, null);
+        last = first;
+        size++;
+    }
+
+    private Node<T> findNodeByIndex(int index) {
+        checkIndex(index, size - 1);
+        Node<T> result;
+        if (size / 2 > index) {
+            result = first;
+            for (int i = 0; i < index; i++) {
+                result = result.next;
+            }
+        } else {
+            result = last;
+            for (int i = size - 1; i > index; i--) {
+                result = result.prev;
+            }
+        }
+        return result;
+    }
+
+    private Node<T> findEqualObj(T object) {
+        Node<T> tempNode = first;
+        for (int i = 0; i < size; i++) {
+            if (object == tempNode.value || (object != null && object.equals(tempNode.value))) {
+                return tempNode;
+            }
+            tempNode = tempNode.next;
+        }
+        return null;
+    }
+
+    private void unlink(Node<T> node) {
+        if (!node.equals(first)) {
+            node.prev.next = node.next;
+        } else if (node.equals(first)) {
+            first = first.next;
+        }
+        if (!node.equals(last)) {
+            node.next.prev = node.prev;
+        } else if (node.equals(last)) {
+            last = last.prev;
+        }
+        size--;
+    }
+
     private void checkIndex(int index, int limit) {
         if (index > limit || index < 0) {
             throw new IndexOutOfBoundsException("Incorrect index");
@@ -47,102 +164,4 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         tempNode.prev = newNode;
         size++;
     }
-
-    @Override
-    public void addAll(List<T> list) {
-        for (int i = 0; i < list.size(); i++) {
-            add(list.get(i));
-        }
-    }
-
-    private void addFirst(T value) {
-        first = new Node(null, value, null);
-        last = first;
-        size++;
-    }
-
-    private void addLast(T value) {
-        Node<T> tempNode = new Node<T>(last, value, null);
-        last.next = tempNode;
-        last = tempNode;
-        size++;
-    }
-
-    @Override
-    public T get(int index) {
-        return findNodeByIndex(index).value;
-    }
-
-    private Node<T> findNodeByIndex(int index) {
-        checkIndex(index, size - 1);
-        Node<T> result = first;
-        for (int i = 0; i < index; i++) {
-            result = result.next;
-        }
-        return result;
-    }
-
-    @Override
-    public T set(T value, int index) {
-        checkIndex(index, size - 1);
-        Node<T> tempNode = findNodeByIndex(index);
-        T res = tempNode.value;
-        tempNode.value = value;
-        return res;
-    }
-
-    private void removeNode(Node<T> node) {
-        if (!node.equals(first)) {
-            node.prev.next = node.next;
-        } else if (node.equals(first)) {
-            first = first.next;
-        }
-        if (!node.equals(last)) {
-            node.next.prev = node.prev;
-        } else if (node.equals(last)) {
-            last = last.prev;
-        }
-        size--;
-    }
-
-    @Override
-    public T remove(int index) {
-        checkIndex(index, size);
-        Node<T> tempNode = findNodeByIndex(index);
-        T res = tempNode.value;
-        removeNode(tempNode);
-        return res;
-    }
-
-    @Override
-    public boolean remove(T object) {
-        Node<T> tempNode = findEqualObj(object);
-        if (tempNode == null) {
-            return false;
-        }
-        removeNode(tempNode);
-        return true;
-    }
-
-    private Node<T> findEqualObj(T object) {
-        Node<T> tempNode = first;
-        for (int i = 0; i < size; i++) {
-            if (object == tempNode.value || (object != null && object.equals(tempNode.value))) {
-                return tempNode;
-            }
-            tempNode = tempNode.next;
-        }
-        return null;
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
 }
--- src/main/java/core/basesyntax/Node.java
+++ src/main/java/core/basesyntax/Node.java
@@ -1,13 +1,13 @@
-package core.basesyntax;
-
-class Node<T> {
-    protected T value;
-    protected Node<T> prev;
-    protected Node<T> next;
-
-    Node(Node<T> prev, T value, Node<T> next) {
-        this.prev = prev;
-        this.value = value;
-        this.next = next;
-    }
-}
+//package core.basesyntax;
+//
+//class Node<T> {
+//    protected T value;
+//    protected Node<T> prev;
+//    protected Node<T> next;
+//
+//    Node(Node<T> prev, T value, Node<T> next) {
+//        this.prev = prev;
+//        this.value = value;
+//        this.next = next;
+//    }
+//}
