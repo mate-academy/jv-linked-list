e4884c04bd3569333a407c5a84a3df7e6353a0a8
morozovyrv <72643876+morozovyrv@users.noreply.github.com>
2021-12-03 17:26:19 +0200

    MyLinkedList impl ready

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -22,30 +22,60 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public void add(T value, int index) {
-
+        if (index == size) {
+            add(value);
+            return;
+        }
+        if (index == 0) {
+            addFirst(value);
+            return;
+        }
+        Node<T> node = getNode(index);
+        Node<T> prevNode = node.prev;
+        Node<T> newNode = new Node<>(prevNode,value,node);
+        prevNode.next = newNode;
+        node.prev = newNode;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        return getNode(index).item;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        Node<T> node = getNode(index);
+        T tempItem = node.item;
+        node.item = value;
+        return tempItem;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        Node<T> node = getNode(index);
+        T tempItem = node.item;
+        unlinkNode(node);
+        return tempItem;
     }
 
     @Override
     public boolean remove(T object) {
+        Node node = first;
+        for (int i = 0; i < size; i++) {
+            if ((node.item != null && node.item.equals(object)) || node.item == object) {
+                unlinkNode(node);
+                return true;
+            }
+            node = node.next;
+        }
         return false;
     }
 
@@ -59,10 +89,51 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return size == 0;
     }
 
+    private void addFirst(T element) {
+        first.prev = new Node<>(null,element,first);
+        first = first.prev;
+        size++;
+    }
+
+    private void unlinkNode(Node node) {
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        }
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        }
+        if (node.equals(first)) {
+            first = node.next;
+        } else if (node.equals(last)) {
+            last = node.prev;
+        }
+        node = null;
+        size--;
+    }
+
+    private Node<T> getNode(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(String.format("Index: %d, Size: %d",index,size));
+        }
+        Node<T> tempNode;
+        if (index <= size / 2) {
+            tempNode = first;
+            for (int i = 0; i < index; i++) {
+                tempNode = tempNode.next;
+            }
+        } else {
+            tempNode = last;
+            for (int i = size - 1; i > index; i--) {
+                tempNode = tempNode.prev;
+            }
+        }
+        return tempNode;
+    }
+
     private static class Node<E> {
-        E item;
-        Node<E> next;
-        Node<E> prev;
+        private E item;
+        private Node<E> next;
+        private Node<E> prev;
 
         Node(Node<E> prev, E element, Node<E> next) {
             this.item = element;
