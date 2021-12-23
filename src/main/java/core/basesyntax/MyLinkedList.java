7631dc24f69df213cefd5c46d88cefd2d432f621
Stelmakh Yevhen <stelmakh.m.d@gmail.com>
2021-12-23 22:03:18 +0200

    Created my own LinkedList with overriding and some new methods

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,45 +3,158 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private int size = 0;
+    private Node<T> head;
+    private Node<T> tail;
+
+    private static class Node<T> {
+        private T element;
+        private Node<T> next;
+        private Node<T> prev;
+
+        Node(Node<T> prev, T element, Node<T> next) {
+            this.element = element;
+            this.prev = prev;
+            this.next = next;
+
+        }
+    }
+
     @Override
     public void add(T value) {
+        final Node<T> l = tail;
+        final Node<T> newNode = new Node<>(l, value, null);
+        tail = newNode;
+        if (l == null) {
+            head = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        checkIndexForAdding(index);
+        if (index == size) {
+            add(value);
+        } else {
+            final Node<T> beforeNewNode = findNodeOnIndex(index).prev;
+            final Node<T> newNode = new Node<>(beforeNewNode,value, findNodeOnIndex(index));
+            findNodeOnIndex(index).prev = newNode;
+            if (beforeNewNode == null) {
+                head = newNode;
+            } else {
+                beforeNewNode.next = newNode;
+            }
+            size++;
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
+        Object[] objects = list.toArray();
+        for (Object o: objects) {
+            add((T) o);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndexForSettingAndRemoving(index);
+        Node<T> value = head;
+        for (int i = 0; i < index; i++) {
+            value = value.next;
+        }
+        return (T)value.element;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        checkIndexForSettingAndRemoving(index + 1);
+        Node<T> x = findNodeOnIndex(index);
+        T oldValue = x.element;
+        x.element = value;
+        return oldValue;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndexForSettingAndRemoving(index);
+        return unlink(findNodeOnIndex(index));
     }
 
     @Override
     public boolean remove(T object) {
+        if (object == null) {
+            for (Node<T> x = head; x != null; x = x.next) {
+                if (x.element == null) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        } else {
+            for (Node<T> x = head; x != null; x = x.next) {
+                if (object.equals(x.element)) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void checkIndexForAdding(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index doesn't exist");
+        }
+    }
+
+    private void checkIndexForSettingAndRemoving(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index doesn't exist");
+        }
+    }
+
+    private Node<T> findNodeOnIndex(int index) {
+        Node<T> nodeAfterIndex = head;
+        for (int i = 0; i < index; i++) {
+            nodeAfterIndex = nodeAfterIndex.next;
+        }
+        return nodeAfterIndex;
+    }
+
+    private T unlink(Node<T> x) {
+        final T object = x.element;
+        final Node<T> next = x.next;
+        final Node<T> prev = x.prev;
+
+        if (prev == null) {
+            head = next;
+        } else {
+            prev.next = next;
+            x.prev = null;
+        }
+
+        if (next == null) {
+            tail = prev;
+        } else {
+            next.prev = prev;
+            x.next = null;
+        }
+
+        x.element = null;
+        size--;
+        return object;
     }
 }
