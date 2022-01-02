728031448215fefeb20f1d641dabd527ec4d6b35
Maksym Pokras <mxpokras@gmail.com>
2022-01-02 18:57:50 +0200

    removeHelper method extracted from remove

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -15,6 +15,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             this.value = value;
         }
     }
+
     @Override
     public void add(T value) {
         Node node = new Node(value);
@@ -36,33 +37,53 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             add(value);
             return;
         }
-        Node node = new Node()
+        Node node = new Node(value);
+        Node nextNode = getNode(index);
+        node.prev = nextNode.prev;
+        node.next = nextNode;
         if (index == 0) {
-
+            head = node;
         }
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T value : list) {
+            add(value);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        return getNode(index).value;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        Node node = getNode(index);
+        T retVal = node.value;
+        node.value = value;
+        return retVal;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        Node node = getNode(index);
+        removeHelper(node);
+        return node.value;
     }
 
     @Override
     public boolean remove(T object) {
+        Node node = head;
+        while (node != null) {
+            if (valueEquals(node, object)) {
+                removeHelper(node);
+                return true;
+            }
+            node = node.next;
+        }
         return false;
     }
 
@@ -73,17 +94,50 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     private Node getNode(int index) {
+        checkOutOfBounds(index, false);
+        boolean reverse = index > (size / 2);
+        int currIndex = reverse ? size - 1 : 0;
+        Node node = reverse ? tail : head;
+        if (reverse) {
+            while (currIndex != index) {
+                node = node.prev;
+                currIndex--;
+            }
+        } else {
+            while (currIndex != index) {
+                node = node.next;
+                currIndex++;
+            }
+        }
+        return node;
+    }
 
+    private void removeHelper(Node node) {
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        } else {
+            head = node.next;
+        }
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        } else {
+            tail = node.prev;
+        }
+        size--;
     }
 
     private void checkOutOfBounds(int index, boolean add) {
         int upperBound = size - (add ? 0 : 1);
-        if ((index < 0) || (index >= upperBound) {
+        if ((index < 0) || (index >= upperBound)) {
             throw new IndexOutOfBoundsException();
         }
+    }
+
+    private boolean valueEquals(Node node, T object) {
+        return node.value == null ? object == null : node.value.equals(object);
     }
 }
