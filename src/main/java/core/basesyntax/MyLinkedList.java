5476df06de46fc566a8fab7ff4d91f475b09272f
agash <alexgash8@gmail.com>
2022-04-16 09:58:49 +0300

    started to solve

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -1,14 +1,42 @@
 package core.basesyntax;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node first;
+    private Node last;
+
+    private class Node {
+        int value;
+        Node prev;
+        Node next;
+
+        Node(int x) {
+            value = x;
+        }
+    }
+
     @Override
     public void add(T value) {
+        if (first == null) {
+            first = new Node((int) value);
+            last = first;
+            return;
+        }
+        Node tempNode = new Node((int) value);
+        tempNode.prev = last;
+        tempNode.next = null;
+        last.next = tempNode;
+        last = tempNode;
     }
 
     @Override
     public void add(T value, int index) {
+        (int i = 0; i <= index; i++) {
+            Object o =  list.get(i);
+
+        })
     }
 
     @Override
@@ -42,6 +70,9 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public boolean isEmpty() {
+        if (first == null) {
+            return true;
+        }
         return false;
     }
 }
