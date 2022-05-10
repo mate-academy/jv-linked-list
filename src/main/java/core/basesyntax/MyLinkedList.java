80e89fe59b00509ff6d26d01dd308ac424d64e82
Maksym <Maksym_Personal@ip-192-168-0-72.eu-central-1.compute.internal>
2022-05-10 23:03:24 +0300

    26/26 not refactored

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -17,20 +17,22 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             this.next = next;
             this.prev = prev;
         }
+    }
 
-        private int locateByIndex(int index) {
-            int indexToFind = 0;
-            Node currentNode = this;
-            while (indexToFind != index) {
-                indexToFind++;
-                currentNode = currentNode.next;
-            }
-            return indexToFind;
+    private void checkIndexAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Invalid index");
         }
     }
 
-    private void checkIndex(int index) {
+    private void checkIndexSet(int index) {
         if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+    }
+
+    private void checkIndexRemove(int index) {
+        if (index < 0 || index == size) {
             throw new IndexOutOfBoundsException("Invalid index");
         }
     }
@@ -39,101 +41,137 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public void add(T value) {
         if (size == 0) {
-            Node newNode = new Node(value, null, null);
-            first = newNode;
-            last = newNode;
-        } else {
-            Node newNode = new Node(value, null, null);
+            Node node = new Node<>(value, null, null);
+            first = node;
+            last = node;
+        }
+        if (size > 0) {
+            Node node = new Node<>(value, null, null);
             Node currentNode = first;
             while (currentNode.next != null) {
                 currentNode = currentNode.next;
             }
-            currentNode.next = newNode;
-            newNode.prev = currentNode;
+            currentNode.next = node;
+            node.prev = currentNode;
+            last = node;
         }
         size++;
     }
 
     @Override
     public void add(T value, int index) {
-        if (index != size) {
-            checkIndex(index);
+        checkIndexAdd(index);
+        if (size == 0) {
+            Node node = new Node<>(value, null, null);
+            first = node;
+            last = node;
         }
-        if (size == 0 && index == 0) {
-            Node newNode = new Node(value, null, null);
-            first = newNode;
-            last = newNode;
-        } else {
-            Node newNode = new Node(value, null, null);
-            Node currentNode = first;
-            while (currentNode.next != null && currentNode.locateByIndex(index) != index) {
-                currentNode = currentNode.next;
-            }
-            newNode.next = currentNode;
-            newNode.prev = currentNode.prev;
-            currentNode.prev.next = newNode;
-            currentNode.prev = newNode;
-            while (currentNode.next != null) {
-                currentNode = currentNode.next;
-            }
+        if (index == size) {
+            this.add(value);
+            return;
         }
-        size++;
-    }
+        if (index == 0) {
+            Node node = new Node<>(value, null, null);
+            node.next = first;
+            first.prev = node;
+            first = node;
+            size++;
+            return;
+        }
 
+        if (size > 0) {
+            Node node = new Node<>(value, null, null);
+            int count = 0;
+            Node currentNode = first;
+            while (count != index) {
+                currentNode = currentNode.next;
+                count++;
+            }
+            currentNode.prev.next = node;
+            node.prev = currentNode.prev;
+            currentNode.prev = node;
+            node.next = currentNode;
+            size++;
+        }
+    }
     @Override
     public void addAll(List<T> list) {
-        for (T item: list) {
-            add(item);
+        for (T listItem: list) {
+            this.add(listItem);
         }
     }
 
     @Override
     public T get(int index) {
-        checkIndex(index);
+        checkIndexSet(index);
+        int count = 0;
         Node currentNode = first;
-        int counter = 0;
-        while (counter != index) {
-            counter++;
+        while (count != index) {
             currentNode = currentNode.next;
+            count++;
         }
         return (T) currentNode.item;
     }
 
     @Override
     public T set(T value, int index) {
-        checkIndex(index);
-            Node currentNode = first;
-            while (currentNode.locateByIndex(index) != index) {
-                currentNode = currentNode.next;
-            }
-            T returnValue = (T) currentNode.item;
-            currentNode.item = value;
-            return returnValue;
+        checkIndexSet(index);
+        int count = 0;
+        Node currentNode = first;
+        while (count != index) {
+            currentNode = currentNode.next;
+            count++;
+        }
+        Node returnNode = new Node(currentNode.item, currentNode.next, currentNode.prev);
+        currentNode.item = value;
+        return (T) returnNode.item;
     }
 
     @Override
     public T remove(int index) {
-        checkIndex(index);
+        checkIndexRemove(index);
+        int count = 0;
+        if (index == 0 && size == 1) {
+            size--;
+            return (T) first.item;
+        }
+        if (index == 0) {
+            first.next.prev = null;
+            Node returnNode = first;
+            first = first.next;
+            size--;
+            return (T) returnNode.item;
+        }
+        if (index == size - 1) {
+            last.prev.next = null;
+            Node returnNode = last;
+            last = last.prev;
+            size--;
+            return (T) returnNode.item;
+        }
         Node currentNode = first;
-        while (currentNode.locateByIndex(index) != index) {
+        while (count != index) {
             currentNode = currentNode.next;
+            count++;
         }
-        T returnValue = (T) currentNode.item;
-        if (index != 0) {
-            currentNode.prev.next = currentNode.next;
-        }
-         if (index != size - 1){
-             currentNode.next.prev = currentNode.prev;
-        }
-        while (currentNode.next != null) {
-            currentNode = currentNode.next;
-        }
+        currentNode.prev.next = currentNode.next;
+        currentNode.next.prev = currentNode.prev;
         size--;
-        return returnValue;
+        return (T) currentNode.item;
     }
 
     @Override
     public boolean remove(T object) {
+        Node currentNode = first;
+        int index = 0;
+        while (index < size) {
+            if (currentNode.item == object || (currentNode != null && object != null && currentNode.item.equals(object))) {
+                remove(index);
+                return true;
+            }
+            currentNode = currentNode.next;
+            index++;
+        }
         return false;
     }
 
