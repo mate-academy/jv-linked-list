4f83b9ad642ee0248f623606e55c828a95f09d7a
Nikita Semisoshenko <jaderussell197@gmail.com>
2022-06-01 19:48:15 +0300

    All tests have passed. Waiting for refactoring

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -1,17 +1,15 @@
 package core.basesyntax;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     private int size = 0;
     private Node<T> head;
-    private Node<T> tale;
-    public class Node<T> {
-        private T value;
-        private Node<T> prev;
-        private Node<T> next;
+    private Node<T> tail;
+    private class Node<T> {
+        public T value;
+        public Node<T> prev;
+        public Node<T> next;
 
         public Node(T value) {
             this.value = value;
@@ -46,19 +44,19 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     private void insertFirst(T value) {
             Node<T> node = new Node<>(value);
-            node.setPrev(null);
-            node.setNext(null);
+            node.prev = null;
+            node.next = null;
             head = node;
-            tale = node;
+            tail = node;
             size++;
     }
 
     private void insertLast(T value) {
         Node<T> node = new Node<>(value);
-        tale.setNext(node);
-        node.setPrev(tale);
+        tail.setNext(node);
+        node.setPrev(tail);
         node.setNext(null);
-        tale = node;
+        tail = node;
         size++;
     }
     private void insertHead(T value) {
@@ -81,9 +79,12 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         size++;
     }
 
-    private void iterator(int index, T value) {
-        if (index < size - index) insert(headIterator(index), value);
-        else insert(tailIterator(index), value);
+    private Node<T> iterator(int index) {
+        if (index < size - index) {
+            return headIterator(index);
+        } else {
+            return tailIterator(index);
+        }
     }
 
     private Node<T> headIterator(int index) {
@@ -95,13 +96,74 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     }
 
     private Node<T> tailIterator(int index) {
-        Node<T> tNode = tale;
+        Node<T> tNode = tail;
         for (int i = 0; i < size - index - 1; i++) {
             tNode = tNode.getPrev();
         }
         return tNode;
     }
+    public void unlink(Node node) {
+        Node previous = node.getPrev();
+        Node next = node.getNext();
+        if (previous == null && size != 1) {
+            next.prev = null;
+            head = next;
+        } else if (next == null && size != 1) {
+            previous.next = null;
+            tail = previous;
+        } else if (size == 1) {
+            head.prev = null;
+            head.next = null;
+            head.value = null;
+        } else {
+            previous.next = next;
+            next.prev = previous;
+        }
+    }
+    private void indexCheck(int index) {
+        if (index >= size || index < 0) throw new ArrayIndexOutOfBoundsException("Invalid index");
+    }
 
+    private void indexCheckAdd(int index) {
+        if (index > size || index < 0) throw new ArrayIndexOutOfBoundsException("Invalid index");
+    }
+
+    private void addByIndex(int index, T value) {
+        if (index == size && size != 0) insertLast(value);
+        else if (index == 0 && size != 0) insertHead(value);
+        else if (index == 0 && size == 0) insertFirst(value);
+        else insert(iterator(index), value);
+    }
+
+    public boolean removeByValue(T object) {
+        Node<T> tNode = head;
+        for (int i = 0; i < size; i++) {
+            if (tNode.getValue() == object || tNode.getValue() != null && tNode.getValue().equals(object)) {
+                unlink(tNode);
+                size--;
+                return true;
+            } else {
+                tNode = tNode.getNext();
+            }
+        }
+        return false;
+    }
+
+    private T removeByIndex(int index) {
+        Node old = iterator(index);
+        indexCheck(index);
+        unlink(old);
+        size--;
+        return (T)old.getValue();
+    }
+
+    private T setByIndex(int index, T value) {
+        indexCheck(index);
+        Node<T> tNode = iterator(index);
+        T oldValue = tNode.getValue();
+        tNode.setValue(value);
+        return oldValue;
+    }
     @Override
     public void add(T value) {
         if (isEmpty()) insertFirst(value);
@@ -110,33 +172,36 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public void add(T value, int index) {
-        if (index == size) insertLast(value);
-        else if (index == 0) insertHead(value);
-        else iterator(index, value);
+        indexCheckAdd(index);
+        addByIndex(index, value);
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T value : list) {
+            add(value);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        indexCheck(index);
+        return iterator(index).value;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        return setByIndex(index, value);
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        return removeByIndex(index);
     }
 
     @Override
     public boolean remove(T object) {
-        return false;
+        return removeByValue(object);
     }
 
     @Override
@@ -147,16 +212,5 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public boolean isEmpty() {
         return size == 0;
-    }
-
-    @Override
-    public String toString() {
-        List<T> list = new ArrayList<>();
-        Node<T> tNode = head;
-        for (int i = 0; i < size; i++) {
-            list.add(tNode.value);
-            tNode = tNode.getNext();
-        }
-        return list.toString();
     }
 }
