ba4625dbd7a4f731e40443381c3b007518290069
Vitaliy Ozhelivskiy <ozelivskijvitalij@gmail.com>
2022-07-02 20:11:20 +0300

    Implemented all required methods

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,46 +3,146 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private int size;
+    private Node<T> first;
+    private Node<T> last;
+
     @Override
     public void add(T value) {
+        if(size == 0) {
+            first = new Node<>(null, value, null);
+            last = first;
+            size++;
+            return;
+        }
+        last.next = new Node<>(last, value, null);
+        last = last.next;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == size) {
+            add(value);
+            return;
+        }
+        Node<T> newElement;
+        if (index == 0) {
+            newElement = new Node<>(null, value, first);
+            first = newElement;
+            size++;
+            return;
+        }
+        Node<T> element = first;
+        for (int i = 0; i < index; i++) {
+            element = element.next;
+        }
+        newElement = new Node<>(element.prev, value, element);
+        element.prev.next = newElement;
+        element.prev = newElement;
+        size ++;
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T element: list) {
+            add(element);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        indexNotExist(index);
+        Node<T> element = first;
+        for (int i = 0; i < index; i++) {
+            element = element.next;
+        }
+        return element == null ? null : element.value;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        indexNotExist(index);
+        Node<T> element = first;
+        Node<T> newElement;
+        if (index == 0) {
+            newElement = new Node<>(null, value, first.next);
+            first = newElement;
+            first.next.prev = newElement;
+            return element.value;
+        }
+        for (int i = 0; i < index; i++) {
+            element = element.next;
+        }
+        T oldValue = element.value;
+        newElement = new Node<>(element.prev, value, element.next);
+        element.prev.next = newElement;
+        element.next.prev = newElement;
+        return oldValue;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        indexNotExist(index);
+        if (size == 1) {
+            size--;
+            return first.value;
+        }
+        if (index == 0){
+            T removedValue = first.value;
+            first = first.next;
+            first.prev = null;
+            size--;
+            return removedValue;
+        }
+        if (index + 1 == size) {
+            T removedValue = last.value;
+            last = last.prev;
+            last.prev.next = null;
+            size--;
+            return removedValue;
+        }
+        Node<T> element = first;
+        for (int i = 0; i < index; i++) {
+            element = element.next;
+        }
+        element.prev.next = element.next;
+        element.next.prev = element.prev;
+        size--;
+        return element.value;
     }
 
     @Override
     public boolean remove(T object) {
+        Node<T> element = first;
+        for (int i = 0; i < size; i++) {
+            if ((element.value == object ||
+                            (element.value != null && element.value.equals(object)))) {
+                remove(i);
+                return true;
+            }
+            element = element.next;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void indexNotExist(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
     }
 
     private static class Node<E> {
