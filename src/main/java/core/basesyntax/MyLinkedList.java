89cd2c304f00c474a32a2796920e2b0a13be21b0
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-07-05 07:54:33 +0300

    creating methods add addAll

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -1,18 +1,85 @@
 package core.basesyntax;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+
+    private static class Node<T> {
+        T item;
+        Node<T> next;
+        Node<T> prev;
+
+        Node(Node<T> prev, T element, Node<T> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    private Node<T> head;
+    private Node<T> tail;
+    private int size = 0;
+
+    void headElement(T element) {
+        final Node<T> first = head;
+        final Node<T> newNode = new Node<>(null, element, first);
+        head = newNode;
+        if (first == null) {
+            tail = newNode;
+        } else {
+            first.prev = newNode;
+        }
+        size++;
+    }
+
+    void tailElement(T element) {
+        final Node<T> last = tail;
+        final Node<T> newNode = new Node<>(last, element, null);
+        tail = newNode;
+        if (last == null){
+            head = newNode;
+        } else {
+            last.next = newNode;
+        }
+        size++;
+    }
+
+    void insertElementBefore(T element, Node<T> successor) {
+        final Node<T> pred = successor.prev;
+        final Node<T> newNode = new Node<>(pred,element, successor);
+        successor.prev = newNode;
+        if (pred == null) {
+            head = newNode;
+        } else {
+            pred.next = newNode;
+            size++;
+        }
+
+    }
+
     @Override
     public void add(T value) {
+        tailElement(value);
     }
 
     @Override
     public void add(T value, int index) {
+        checkIndex(index);
+        if (index == size)
+            tailElement(value);
+        else
+            insertElementBefore(value, searchElement(index));
     }
 
     @Override
     public void addAll(List<T> list) {
+        Object[] elements = list.toArray();
+        if (elements.length != 0) {
+            Node<T> pred, successor;
+            successor = null;
+            pred = tail;
+        }
     }
 
     @Override
@@ -37,11 +104,30 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
         return false;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 && index > size) {
+            throw new IndexOutOfBoundsException("Index: "+ index  +" of bounds for size : "+ size);
+        }
+    }
+    private Node<T> searchElement(int index) {
+        if (index < (size >> 1)) {
+            Node<T> current = head;
+            for (int i = 0; i < index; i++)
+                current = current.next;
+            return current;
+        } else {
+            Node<T> current = tail;
+            for (int i = size - 1; i > index; i--)
+                current = current.prev;
+            return current;
+        }
     }
 }
