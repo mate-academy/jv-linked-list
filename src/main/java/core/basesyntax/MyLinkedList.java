fd9411c1adbe956ac96d63818ad662057df62b3b
Andrii Malenko <andmalenko@gmail.com>
2022-07-25 23:47:15 +0300

    Please check my solution of jv-linked-list

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,45 +3,143 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private int size;
+    private Node<T> first;
+    private Node<T> last;
+
+    private static class Node<T> {
+        private T item;
+        private Node<T> next;
+        private Node<T> prev;
+
+        Node(Node<T> prev, T element, Node<T> next) {
+            this.prev = prev;
+            this.item = element;
+            this.next = next;
+        }
+    }
+
     @Override
     public void add(T value) {
+        if (isEmpty()) {
+            first = last = new Node<>(null, value, null);
+        } else {
+            last = new Node<>(last, value, null);
+            last.prev.next = last;
+        }
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index == size) {
+            add(value);
+        } else if (index == 0) {
+            isValid(index);
+            first = new Node<>(null, value, first);
+            first.next.prev = first;
+            size++;
+        } else {
+            isValid(index);
+            Node<T> current = findNode(index);
+            current.prev = new Node<>(current.prev, value, current);
+            current.prev.prev.next = current.prev;
+            size++;
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (int i = 0; i < list.size(); i++) {
+            this.add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        isValid(index);
+        return findNode(index).item;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        isValid(index);
+        T temporaryItem = findNode(index).item;
+        findNode(index).item = value;
+        return temporaryItem;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        isValid(index);
+        Node<T> temporaryNode = findNode(index);
+        if (size == 1) {
+            temporaryNode.next = null;
+            temporaryNode.prev = null;
+            first = last = null;
+        } else if (index == 0) {
+            temporaryNode.next.prev = null;
+            first = temporaryNode.next;
+        } else if (index == size - 1) {
+            temporaryNode.prev.next = null;
+            last = temporaryNode;
+        } else {
+            temporaryNode.prev.next = temporaryNode.next;
+            temporaryNode.next.prev = temporaryNode.prev;
+        }
+        size--;
+        return temporaryNode.item;
     }
 
     @Override
     public boolean remove(T object) {
+        for (int i = 0; i < size; i++) {
+            if ((get(i) == object) || (get(i) != null) && (get(i).equals(object))) {
+                remove(i);
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void isValid(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Your index " + index
+                        + " is over the size " + size);
+        }
+    }
+
+    private Node<T> findNode(int index) {
+        if (index <= (size / 2)) {
+            Node<T> currentNode = first;
+            for (int i = 0; i <= index; i++) {
+                if (i == index) {
+                    return currentNode;
+                } else {
+                    currentNode = currentNode.next;
+                }
+            }
+        } else {
+            Node<T> currentNode = last;
+            for (int i = size - 1; i >= index; i--) {
+                if (i == index) {
+                    return currentNode;
+                } else {
+                    currentNode = currentNode.prev;
+                }
+            }
+        }
+        return null;
     }
 }
+
