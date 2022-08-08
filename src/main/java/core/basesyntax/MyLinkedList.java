b51b5456d0fbfca840cc7b83ac9f0a2a1367f810
Alex Alexandrov <alexandrov.alex.al@gmail.com>
2022-08-08 09:17:57 +0300

    An implementation of the double-linked list is inserted.

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,45 +3,166 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    static class Node<T> {
+        private Node<T> prev;
+        private T value;
+        private Node<T> next;
+
+        public Node(Node<T> prev, T value, Node<T> next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
     @Override
     public void add(T value) {
+        Node<T> newNode = new Node<>(null, value, null);
+        if (head == null) {
+            head = newNode;
+            tail = head;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index == size) {
+            add(value);
+        } else {
+            Node<T> currentNode = getNodeByIndex(index);
+            Node<T> newNode = new Node<>(null, value, null);
+            unlinkNode(currentNode.prev, newNode, currentNode, "add");
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T t : list) {
+            add(t);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index >= size) {
+            throw new IndexOutOfBoundsException("The index is out of bounds");
+        }
+        Node<T> currentNode = getNodeByIndex(index);
+        return currentNode.value;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        if (index >= size) {
+            throw new IndexOutOfBoundsException("The index is out of bounds");
+        }
+        Node<T> currentNode = getNodeByIndex(index);
+        T oldNodeValue = currentNode.value;
+        currentNode.value = value;
+        return oldNodeValue;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index >= size) {
+            throw new IndexOutOfBoundsException("The index is out of bounds");
+        }
+        Node<T> currentNode = getNodeByIndex(index);
+        unlinkNode(currentNode.prev, currentNode, currentNode.next, "remove");
+        return currentNode.value;
     }
 
     @Override
     public boolean remove(T object) {
+        Node<T> currentNode = head;
+        if (object == null) {
+            for (int i = 0; i < size; i++) {
+                if (currentNode.value == null) {
+                    unlinkNode(currentNode.prev, currentNode, currentNode.next, "remove");
+                    return true;
+                }
+                currentNode = currentNode.next;
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (object.equals(currentNode.value)) {
+                    unlinkNode(currentNode.prev, currentNode, currentNode.next, "remove");
+                    return true;
+                }
+                currentNode = currentNode.next;
+            }
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private Node<T> getNodeByIndex(int index) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("The index is out of bounds");
+        }
+        Node<T> currentNode;
+        if (index <= size / 2) {
+            currentNode = head;
+            for (int i = 0; i < index; i++) {
+                currentNode = currentNode.next;
+            }
+        } else {
+            currentNode = tail;
+            for (int i = size; i > index + 1; i--) {
+                currentNode = currentNode.prev;
+            }
+        }
+        return currentNode;
+    }
+
+    private void unlinkNode(Node<T> previous, Node<T> current, Node<T> next, String operation) {
+        switch (operation) {
+            case "add":
+                if (next.equals(head)) {
+                    head = current;
+                }
+                if (previous != null) {
+                    previous.next = current;
+                }
+                current.prev = previous;
+                current.next = next;
+                next.prev = current;
+                size++;
+                break;
+            case "remove":
+                if (head.equals(current)) {
+                    head = next;
+                }
+                current.prev = null;
+                current.next = null;
+                if (previous != null) {
+                    previous.next = next;
+                }
+                if (next != null) {
+                    next.prev = previous;
+                }
+                size--;
+                break;
+            default:
+                break;
+        }
     }
 }
