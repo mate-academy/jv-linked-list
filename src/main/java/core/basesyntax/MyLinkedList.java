276dd11980c18d0b98d28c449131a6909d4d1101
Heorhiy Nezbrytskiy <georgiy.nezbritskiy@gmail.com>
2022-08-09 21:02:48 +0300

    creat and implement myLinkedList

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,45 +3,147 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private int size;
+    private Node<T> head;
+    private Node<T> tail;
+
     @Override
     public void add(T value) {
+        if (head == null) {
+            head = new Node<T>(null, value, null);
+            tail = head;
+        } else {
+            tail.next = new Node<>(tail, value, null);
+            tail = tail.next;
+        }
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("The index "
+                    + index + "passed to method is invalid");
+        }
+        if (size == index) {
+            add(value);
+        } else {
+            Node<T> currentNode = findNodeByIndex(index);
+            Node<T> newNode = new Node<>(currentNode.prev, value, currentNode);
+            currentNode.prev = newNode;
+            if (index == 0) {
+                head = newNode;
+            } else {
+                newNode.prev.next = newNode;
+            }
+            size++;
+        }
+
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndexValidation(index);
+        return findNodeByIndex(index).item;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        checkIndexValidation(index);
+        Node<T> currentNode = findNodeByIndex(index);
+        T oldValue = currentNode.item;
+        currentNode.item = value;
+        return oldValue;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndexValidation(index);
+        Node<T> node = findNodeByIndex(index);
+        if (head == tail) {
+            head = null;
+            tail = null;
+            size--;
+        } else if (index == 0) {
+            head.next.prev = null;
+            head = head.next;
+            size--;
+        } else if (index == size - 1) {
+            tail.prev.next = null;
+            tail = tail.prev;
+            size--;
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+            size--;
+        }
+        return node.item;
     }
 
     @Override
     public boolean remove(T object) {
+        int i = 0;
+        if (object == null) {
+            for (Node<T> x = head; x != null; x = x.next) {
+                if (x.item == object) {
+                    remove(i);
+                    return true;
+                }
+                i++;
+            }
+        } else {
+            for (Node<T> x = head; x != null; x = x.next) {
+                if (x.item.equals(object)) {
+                    remove(i);
+                    return true;
+                }
+                i++;
+            }
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void checkIndexValidation(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("The index "
+                    + index + "passed to method is invalid");
+        }
+    }
+
+    private Node<T> findNodeByIndex(int index) {
+        Node<T> currentNode = head;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode;
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+        private Node<E> prev;
+
+        public Node(Node<E> prev, E item, Node<E> next) {
+            this.prev = prev;
+            this.item = item;
+            this.next = next;
+        }
     }
 }
