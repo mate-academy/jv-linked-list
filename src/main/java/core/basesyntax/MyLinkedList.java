5e5748f4f0c5be4b4d644a0dd48e2bdd0c56a03b
Vasyl Pankiv <wasilius2@gmail.com>
2022-08-09 23:07:03 +0300

    add inner class Node

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -1,12 +1,15 @@
 package core.basesyntax;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private int size;
+    private Node<T> head;
+    private Node<T> tail;
     @Override
     public void add(T value) {
     }
-
     @Override
     public void add(T value, int index) {
     }
@@ -43,5 +46,17 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public boolean isEmpty() {
         return false;
+    }
+
+    private class Node<T>{
+        Node<T> next;
+        Node<T> prev;
+        T value;
+
+        public Node(Node<T> next, Node<T> prev, T value) {
+            this.next = next;
+            this.prev = prev;
+            this.value = value;
+        }
     }
 }
