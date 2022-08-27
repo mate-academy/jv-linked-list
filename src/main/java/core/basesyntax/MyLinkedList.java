d2ce6b902b6bd8df54537685e5a9a6bb561bbf26
Zakharchenko <zakharchenko.andrii@gmail.com>
2022-08-27 09:37:35 +0300

    jv-linked-list-hw12-unlink-method-added-v02

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -4,38 +4,48 @@ import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     private int size;
-    private Node<T> tailNode;
-    private Node<T> headNode;
-    private Node<T> currentNode;
-    private int currentNodeIndex;
+    private Node<T> tail;
+    private Node<T> head;
 
-    private Node<T> newNode;
+    private class Node<T> {
+        private T value;
+        private Node<T> prev;
+        private Node<T> next;
+
+        private Node(Node<T> prev, T value, Node<T> next) {
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
 
     @Override
     public void add(T value) {
         if (size == 0) {
             Node newNode = new Node(null, value, null);
-            headNode = newNode;
-            tailNode = newNode;
+            head = newNode;
+            tail = newNode;
         } else {
-            Node newNode = new Node(tailNode, value, null);
-            tailNode.next = newNode;
-            tailNode = newNode;
+            Node newNode = new Node(tail, value, null);
+            tail.next = newNode;
+            tail = newNode;
         }
         size++;
     }
 
     @Override
     public void add(T value, int index) {
+        Node<T> newNode;
+        Node<T> currentNode;
         if (index == size) {
             add(value);
             return;
         }
-        goToNodeByIndex(index);
+        currentNode = goToNodeByIndex(index);
         if (index == 0) {
             newNode = new Node(null, value, currentNode);
             size++;
-            headNode = newNode;
+            head = newNode;
             return;
         }
         newNode = new Node(currentNode.prev, value, currentNode);
@@ -46,22 +56,22 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public void addAll(List<T> myList) {
-        if (!myList.isEmpty()) {
-            for (T itemFromList:myList) {
-                add(itemFromList);
-            }
+        for (T itemFromList:myList) {
+            add(itemFromList);
         }
     }
 
     @Override
     public T get(int index) {
-        goToNodeByIndex(index);
+        Node<T> currentNode;
+        currentNode = goToNodeByIndex(index);
         return currentNode.value;
     }
 
     @Override
     public T set(T value, int index) {
-        goToNodeByIndex(index);
+        Node<T> currentNode;
+        currentNode = goToNodeByIndex(index);
         Node<T> oldNode = new Node(null, currentNode.value,null);
         currentNode.value = value;
         return oldNode.value;
@@ -71,32 +81,26 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     public T remove(int index) {
         checkIndexOutOfSize(index);
         Node<T> oldNode;
+        Node<T> currentNode;
         if (index == 0 && size > 1) {
-            oldNode = new Node(null, headNode.value,null);
-            headNode.next.prev = null;
-            headNode = headNode.next;
-        } else if (index == 0 && size == 1) {
-            oldNode = new Node(null, headNode.value,null);
-            headNode = null;
-            tailNode = null;
+            oldNode = new Node(null, head.value,null);
+            unlink(head);
         } else if (index == size - 1) {
-            oldNode = new Node(null, tailNode.value,null);
-            tailNode.prev.next = null;
-            tailNode = tailNode.prev;
+            oldNode = new Node(null, tail.value,null);
+            unlink(tail);
         } else {
-            goToNodeByIndex(index);
+            currentNode = goToNodeByIndex(index);
             oldNode = new Node(null, currentNode.value, null);
-            currentNode.prev.next = currentNode.next;
-            currentNode.next.prev = currentNode.prev;
+            unlink(currentNode);
         }
-        size--;
         return oldNode.value;
     }
 
     @Override
     public boolean remove(T object) {
+        Node<T> currentNode;
         for (int i = 0; i < size; i++) {
-            goToNodeByIndex(i);
+            currentNode = goToNodeByIndex(i);
             if (object == currentNode.value || object != null && object.equals(currentNode.value)) {
                 remove(i);
                 return true;
@@ -115,19 +119,43 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return size == 0;
     }
 
+    private void unlink(Node node) {
+        if (node.prev == null && node.next == null) {
+            head = null;
+            tail = null;
+        } else if (node.prev == null) {
+            node.next.prev = null;
+            head = node.next;
+        } else if (node.next == null) {
+            node.prev.next = null;
+            tail = node.prev;
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+        size--;
+    }
+
     private void checkIndexOutOfSize(int index) {
         if (index >= size || index < 0) {
-            throw new IndexOutOfBoundsException();
+            throw new IndexOutOfBoundsException("Index out of LinkedList range ");
         }
     }
 
-    private void goToNodeByIndex(int index) {
+    private Node<T> goToNodeByIndex(int index) {
         checkIndexOutOfSize(index);
-        currentNodeIndex = 0;
-        currentNode = headNode;
-        for (int i = 0; i < index; i++) {
-            currentNode = currentNode.next;
-            currentNodeIndex++;
+        Node<T> currentNode;
+        if (index < size >> 1) {
+            currentNode = head;
+            for (int i = 0; i < index; i++) {
+                currentNode = currentNode.next;
+            }
+        } else {
+            currentNode = tail;
+            for (int i = size - 1; i > index; i--) {
+                currentNode = currentNode.prev;
+            }
         }
+        return currentNode;
     }
 }
