d2dd5b67360fd56d5e823c7ee93395b10c6eb365
Valerii Kulibchenko <kulibchenkovalera@gmail.com>
2022-10-03 00:21:10 +0300

    first commit

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,12 +3,56 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    private static class Node<T> {
+        T value;
+        Node<T> prev;
+        Node<T> next;
+
+        public Node(Node<T> prev, T element, Node<T> next) {
+            this.value = element;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
     @Override
     public void add(T value) {
+        if (head == null) {
+            head = new Node<>(null, value,null);
+            tail = head;
+            size++;
+        } else {
+            tail.next = new Node<>(tail, value, null);
+            tail = tail.next;
+            size++;
+
+        }
     }
 
     @Override
     public void add(T value, int index) {
+        if (index < 0 || index > size){
+            throw new IndexOutOfBoundsException();
+        }
+        if (index  == size) {
+            add(value);
+        } else if (index == 0) {
+            head.prev = new Node<>(null, value, head);
+            head.next.prev = head;
+            size++;
+        } else {
+            Node<T> curentNode = head;
+            for (int i = 0; i < index; i++) {
+                curentNode = curentNode.next;
+            }
+            curentNode.next = new Node<>(curentNode, value, curentNode.next);
+            curentNode.next.next.prev = curentNode.next;
+            size++;
+        }
     }
 
     @Override
@@ -17,31 +61,75 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public T get(int index) {
-        return null;
-    }
+        checkIndex(index);
+        Node<T> curentNode = head;
+        for (int i = 0; i < index; i++) {
+            curentNode = curentNode.next;
+        }
+        return curentNode.value;
+        }
 
     @Override
     public T set(T value, int index) {
+        checkIndex(index);
+        Node<T> curentNode = head;
+        for (int i = 0; i < index; i++) {
+            curentNode = curentNode.next;
+        }
+        curentNode.value = value;
         return null;
     }
 
     @Override
     public T remove(int index) {
+        checkIndex(index);
+        if (index == 0) {
+            head.next.prev = null;
+            head = head.next;
+            size--;
+        } else if (index == size - 1){
+            tail.prev.next = null;
+            tail = tail.prev;
+            size--;
+        } else {
+            Node<T> curentNode = head;
+            for (int i = 0; i < size; i++) {
+                curentNode = curentNode.next;
+                if (i == index) {
+                    curentNode.prev.next = curentNode.next;
+                    curentNode.next.prev = curentNode.prev;
+                    size--;
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public boolean remove(T object) {
+        Node<T> curentNode = head;
+        for (int i = 0; i < size; i++) {
+            curentNode = curentNode.next;
+            if (curentNode.value.equals(object)){
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void checkIndex(int index) {
+        if (index >= size || index <0){
+            throw new IndexOutOfBoundsException();
+        }
     }
 }
