ac7130bcf799d9ca490a755b125fa13656d589f5
Rommelua <rommel@ukr.net>
2022-10-07 14:24:21 +0300

    Messages fixed

--- src/test/java/core/basesyntax/MyLinkedListTest.java
+++ src/test/java/core/basesyntax/MyLinkedListTest.java
@@ -347,32 +347,45 @@ public class MyLinkedListTest {
 
         boolean isSecondCatRemove = cats.remove(THE_SAME_SECOND_CAT);
         Assert.assertTrue("Test failed! Result after removing should be true", isSecondCatRemove);
-        Assert.assertEquals("Expected size is incorrect", 6, cats.size());
-        Assert.assertEquals("Unexpected element found in list", FIRST_CAT, cats.get(0));
-        Assert.assertEquals("Expected size is incorrect", THIRD_CAT, cats.get(1));
-        Assert.assertEquals("Expected size is incorrect", THIRD_CAT, cats.get(2));
-        Assert.assertEquals("Expected size is incorrect", null, cats.get(3));
+        Assert.assertEquals("Expected size is incorrect. Expected: 6, but was: " + cats.size(), 6, cats.size());
+        Assert.assertEquals("Unexpected element found in list on 0 position. Expected: " + FIRST_CAT +", but was: " + cats.get(0),
+                FIRST_CAT, cats.get(0));
+        Assert.assertEquals("Unexpected element found in list on 1 position. Expected: " + THIRD_CAT +", but was: " + cats.get(1),
+                THIRD_CAT, cats.get(1));
+        Assert.assertEquals("Unexpected element found in list on 2 position. Expected: " + THIRD_CAT +", but was: " + cats.get(2),
+                THIRD_CAT, cats.get(2));
+        Assert.assertEquals("Unexpected element found in list on 3 position. Expected: null, but was: " + cats.get(3),
+                null, cats.get(3));
 
         boolean isThirdCatRemove = cats.remove(THIRD_CAT);
         Assert.assertTrue("Test failed! Result after removing should be true", isThirdCatRemove);
-        Assert.assertEquals("Expected size is incorrect", 5, cats.size());
-        Assert.assertEquals("Unexpected element found in list", FIRST_CAT, cats.get(0));
-        Assert.assertEquals("Expected size is incorrect", THIRD_CAT, cats.get(1));
-        Assert.assertEquals("Expected size is incorrect", null, cats.get(2));
+        Assert.assertEquals("Expected size is incorrect. Expected: 5, but was: " + cats.size(), 5, cats.size());
+        Assert.assertEquals("Unexpected element found in list on 0 position. Expected: " + FIRST_CAT +", but was: " + cats.get(0),
+                FIRST_CAT, cats.get(0));
+        Assert.assertEquals("Unexpected element found in list on 1 position. Expected: " + THIRD_CAT +", but was: " + cats.get(1),
+                THIRD_CAT, cats.get(1));
+        Assert.assertEquals("Unexpected element found in list on 2 position. Expected: null, but was: " + cats.get(2),
+                null, cats.get(2));
 
         boolean isFourthCatRemove = cats.remove(FOURTH_CAT);
         Assert.assertTrue("Test failed! Result after removing should be true", isFourthCatRemove);
-        Assert.assertEquals("Expected size is incorrect", 4, cats.size());
-        Assert.assertEquals("Unexpected element found in list", FIRST_CAT, cats.get(0));
-        Assert.assertEquals("Expected size is incorrect", THIRD_CAT, cats.get(1));
-        Assert.assertEquals("Expected size is incorrect", null, cats.get(2));
+        Assert.assertEquals("Expected size is incorrect. Expected: 4, but was: " + cats.size(), 4, cats.size());
+        Assert.assertEquals("Unexpected element found in list on 0 position. Expected: " + FIRST_CAT +", but was: " + cats.get(0),
+                FIRST_CAT, cats.get(0));
+        Assert.assertEquals("Unexpected element found in list on 1 position. Expected: " + THIRD_CAT +", but was: " + cats.get(1),
+                THIRD_CAT, cats.get(1));
+        Assert.assertEquals("Unexpected element found in list on 2 position. Expected: null, but was: " + cats.get(2),
+                null, cats.get(2));
 
         boolean isNullRemove = cats.remove(null);
         Assert.assertTrue("Test failed! Result after removing should be true", isNullRemove);
-        Assert.assertEquals("Expected size is incorrect", 3, cats.size());
-        Assert.assertEquals("Unexpected element found in list", FIRST_CAT, cats.get(0));
-        Assert.assertEquals("Expected size is incorrect", THIRD_CAT, cats.get(1));
-        Assert.assertEquals("Expected size is incorrect", null, cats.get(2));
+        Assert.assertEquals("Expected size is incorrect. Expected: 3, but was: " + cats.size(), 3, cats.size());
+        Assert.assertEquals("Unexpected element found in list on 0 position. Expected: " + FIRST_CAT +", but was: " + cats.get(0),
+                FIRST_CAT, cats.get(0));
+        Assert.assertEquals("Unexpected element found in list on 1 position. Expected: " + THIRD_CAT +", but was: " + cats.get(1),
+                THIRD_CAT, cats.get(1));
+        Assert.assertEquals("Unexpected element found in list on 2 position. Expected: null, but was: " + cats.get(2),
+                null, cats.get(2));
     }
 
     @Test
