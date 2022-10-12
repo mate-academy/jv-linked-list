45ce4e72a6e062eb89010913fb9aa4c3c2286af4
Veronika Tretiak <nikatretiak9@gmail.com>
2022-10-12 14:04:12 +0200

    Implemented task

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,45 +3,132 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    class Node<T> {
+        private Node<T> prev;
+        private T value;
+        private Node<T> next;
+
+        public Node(Node<T> prev, T value, Node<T> next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
     @Override
     public void add(T value) {
+        Node<T> newNode = new Node<>(null, value, null);
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.prev = tail;
+            tail.next = newNode;
+        }
+        tail = newNode;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index == size) {
+            add(value);
+        } else if (index == 0) {
+            Node<T> newNode = new Node<>(null, value, head);
+            head.prev = newNode;
+            head = newNode;
+            size++;
+        } else {
+            Node<T> node = getNodeByIndex(index);
+            Node<T> newNode = new Node<>(node.prev, value, node);
+            node.prev.next = newNode;
+            node.prev = newNode;
+            size++;
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T value: list) {
+            add(value);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        Node<T> newNode = getNodeByIndex(index);
+        return newNode.value;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        T valueNode = getNodeByIndex(index).value;
+        getNodeByIndex(index).value = value;
+        return valueNode;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        Node<T> remoteNode = getNodeByIndex(index);
+        unlink(remoteNode);
+        return remoteNode.value;
     }
 
     @Override
     public boolean remove(T object) {
+        Node<T> remoteNode = head;
+        while (remoteNode != null) {
+            if (remoteNode.value == object
+                    || remoteNode.value != null
+                    && remoteNode.value.equals(object)) {
+                unlink(remoteNode);
+                return true;
+            }
+            remoteNode = remoteNode.next;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    public Node<T> getNodeByIndex(int index) {
+        if (index < 0 && index >= size) {
+            throw new IndexOutOfBoundsException("Index is not correct: "
+                            + index + ", for Size: " + size);
+        }
+        if (!(index >= 0 && index < size)) {
+            throw new IndexOutOfBoundsException("Index is not correct: "
+                    + index + ", for Size: " + size);
+        }
+        Node<T> currentNode = head;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode;
+    }
+
+    public void unlink(Node node) {
+        if (node.prev == null) {
+            head = node.next;
+        } else {
+            node.prev.next = node.next;
+        }
+        if (node.next == null) {
+            tail = node.prev;
+        } else {
+            node.next.prev = node.prev;
+        }
+        size--;
     }
 }
