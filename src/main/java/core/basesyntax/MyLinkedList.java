ff6c8a0c78469c4c7d48e8a24953e9bad60fb6f9
AlinaOryshak <alinaoryshak@gmail.com>
2022-10-14 12:46:40 +0300

    class structure was changed, body of addlast() was modified and transferred to add()

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -8,22 +8,85 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     private Node<T> head;
     private Node<T> tail;
 
-    private static class Node<T> {
-        private T value;
-        private Node<T> prev;
-        private Node<T> next;
-
-        public Node(Node<T> prev, T value, Node<T> next) {
-            this.prev = prev;
-            this.value = value;
-            this.next = next;
-        }
-    }
-
     public MyLinkedList() {
         size = 0;
         head = null;
         tail = null;
+    }
+
+    @Override
+    public void add(T value) {
+        Node<T> newNode = new Node<>(tail, value, null);
+        if (size == 0) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+        tail = newNode;
+        size++;
+    }
+
+    @Override
+    public void add(T value, int index) {
+        checkPositionIndex(index);
+        if (index == 0) {
+            addFirst(value);
+        } else if (index == size) {
+            add(value);
+        } else {
+            Node<T> currentAtIndex = getObjByIndex(index);
+            insert(value, currentAtIndex);
+        }
+    }
+
+    @Override
+    public void addAll(List<T> list) {
+        for (T item : list) {
+            add(item);
+        }
+    }
+
+    @Override
+    public T get(int index) {
+        checkElementIndex(index);
+        return getObjByIndex(index).value;
+    }
+
+    @Override
+    public T set(T value, int index) {
+        checkElementIndex(index);
+        Node<T> current = getObjByIndex(index);
+        T oldValue = current.value;
+        current.value = value;
+        return oldValue;
+    }
+
+    @Override
+    public T remove(int index) {
+        checkElementIndex(index);
+        Node<T> current = getObjByIndex(index);
+        size--;
+        return unlink(current);
+    }
+
+    @Override
+    public boolean remove(T object) {
+        int index = getIndex(object);
+        if (isElementIndex(index)) {
+            remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
     }
 
     private void addFirst(T value) {
@@ -34,18 +97,6 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             tail = newNode;
         } else {
             first.prev = newNode;
-        }
-        size++;
-    }
-
-    private void addLast(T value) {
-        Node<T> last = tail;
-        Node<T> newNode = new Node<>(last, value, null);
-        tail = newNode;
-        if (size == 0) {
-            head = newNode;
-        } else {
-            last.next = newNode;
         }
         size++;
     }
@@ -84,46 +135,6 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return VALUE_NOT_FOUND;
     }
 
-    @Override
-    public void add(T value) {
-        addLast(value);
-    }
-
-    @Override
-    public void add(T value, int index) {
-        checkPositionIndex(index);
-        if (index == 0) {
-            addFirst(value);
-        } else if (index == size) {
-            addLast(value);
-        } else {
-            Node<T> currentAtIndex = getObjByIndex(index);
-            insert(value, currentAtIndex);
-        }
-    }
-
-    @Override
-    public void addAll(List<T> list) {
-        for (T item : list) {
-            addLast(item);
-        }
-    }
-
-    @Override
-    public T get(int index) {
-        checkElementIndex(index);
-        return getObjByIndex(index).value;
-    }
-
-    @Override
-    public T set(T value, int index) {
-        checkElementIndex(index);
-        Node<T> current = getObjByIndex(index);
-        T oldValue = current.value;
-        current.value = value;
-        return oldValue;
-    }
-
     private T unlink(Node<T> node) {
         Node<T> beforeNode = node.prev;
         Node<T> afterNode = node.next;
@@ -143,34 +154,6 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return node.value;
     }
 
-    @Override
-    public T remove(int index) {
-        checkElementIndex(index);
-        Node<T> current = getObjByIndex(index);
-        size--;
-        return unlink(current);
-    }
-
-    @Override
-    public boolean remove(T object) {
-        int index = getIndex(object);
-        if (isElementIndex(index)) {
-            remove(index);
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
     private boolean isElementIndex(int index) {
         return (index >= 0 && index < size);
     }
@@ -184,6 +167,18 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     private void checkPositionIndex(int index) {
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private static class Node<T> {
+        private T value;
+        private Node<T> prev;
+        private Node<T> next;
+
+        public Node(Node<T> prev, T value, Node<T> next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
         }
     }
 }
