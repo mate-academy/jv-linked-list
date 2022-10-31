d22f79435ab8be0931893f685561372f3ff01b48
AndriySichniy <sichniy.andriy@gmail.com>
2022-10-31 12:24:51 +0200

    added Node (inner, static)

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,8 +3,25 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private int size;
+    private Node<T> first;
+    private Node<T> last;
+
+    static public class Node<E> {
+        private E item;
+        private Node<E> prev;
+        private Node<E> next;
+
+        public Node(E item, Node<E> prev, Node<E> next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
     @Override
     public void add(T value) {
+
     }
 
     @Override
