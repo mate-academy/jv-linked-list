5bff20b44273459aaa97ff147d17c3fadb3f189a
Oksana Rudenko <0ksaha_rudenko@ukr.net>
2022-11-14 14:10:06 +0200

    jv-linked-list solution fixed

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -6,7 +6,6 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     private int size;
     private Node<T> first;
     private Node<T> last;
-    private Node<T> currentNode;
 
     @Override
     public void add(T value) {
@@ -23,26 +22,22 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public void add(T value, int index) {
-        if (index > size || index < 0) {
-            throw new IndexOutOfBoundsException("Index is not valid. Index is "
-                    + index + ", but it must be in bounds from 0 to " + size);
-        }
-        if (index > 0 && index < size) {
-            getItem(index);
-            currentNode = new Node<>(currentNode.prev, value, currentNode);
-            currentNode.prev.next = currentNode;
-            currentNode.next.prev = currentNode;
-            size++;
+        if (index == size) {
+            add(value);
             return;
         }
+        checkIndex(index);
         if (index == 0 && size > 0) {
             first = new Node<>(null, value, first);
             first.next.prev = first;
             size++;
+            return;
         }
-        if (index == size) {
-            add(value);
-        }
+        Node<T> nodeByIndex = getNodeByIndex(index);
+        Node<T> newNode = new Node<>(nodeByIndex.prev, value, nodeByIndex);
+        newNode.prev.next = newNode;
+        nodeByIndex.prev = newNode;
+        size++;
     }
 
     @Override
@@ -57,30 +52,28 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public T get(int index) {
         checkIndex(index);
-        getItem(index);
-        return currentNode.value;
+        return getNodeByIndex(index).value;
     }
 
     @Override
     public T set(T value, int index) {
         checkIndex(index);
-        getItem(index);
-        T setValue = currentNode.value;
-        currentNode.value = value;
+        T setValue = getNodeByIndex(index).value;
+        getNodeByIndex(index).value = value;
         return setValue;
     }
 
     @Override
     public T remove(int index) {
         checkIndex(index);
-        getItem(index);
-        T removedValue = currentNode.value;
-        unLink(currentNode);
+        T removedValue = getNodeByIndex(index).value;
+        unLink(getNodeByIndex(index));
         return removedValue;
     }
 
     @Override
     public boolean remove(T object) {
+        Node<T> currentNode;
         for (currentNode = first; currentNode != null; currentNode = currentNode.next) {
             if (currentNode.value == object
                     || currentNode.value != null && currentNode.value.equals(object)) {
@@ -108,12 +101,13 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         }
     }
 
-    private void getItem(int index) {
+    private Node<T> getNodeByIndex(int index) {
+        Node<T> currentNode = first;
         if (index >= 0 && index < size / 2) {
             int count = 0;
             for (currentNode = first; count < size / 2; currentNode = currentNode.next) {
                 if (index == count) {
-                    return;
+                    return currentNode;
                 }
                 count++;
             }
@@ -122,11 +116,12 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             int count = size - 1;
             for (currentNode = last; count >= size / 2; currentNode = currentNode.prev) {
                 if (index == count) {
-                    return;
+                    return currentNode;
                 }
                 count--;
             }
         }
+        return currentNode;
     }
 
     private void unLink(Node<T> currentNode) {
@@ -144,7 +139,6 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             next.prev = prev;
             currentNode.next = next;
         }
-        currentNode.value = null;
         size--;
     }
 
