62260664628c50eca1cc43c1161eaed7fe09a237
Denis Kalyakin <denis.kalyakin999@gmail.com>
2022-11-23 01:51:37 +0200

    beginning is set

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,8 +3,22 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private int size;
+    private Node<T> head;
+    private Node<T> tail;
+
     @Override
     public void add(T value) {
+        Node<T> newNode = new Node<>(null, value, null);
+
+        if (isEmpty()){
+            head = tail = newNode;
+        } else {
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
     }
 
     @Override
@@ -17,7 +31,13 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public T get(int index) {
-        return null;
+        Node<T> currentNode = head;
+        T t = null;
+        for (int i = 0; i < index + 1; i++){
+            t = currentNode.value;
+            currentNode = currentNode.next;
+        }
+        return t;
     }
 
     @Override
@@ -37,11 +57,23 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+}
+
+class Node<T> {
+    T value;
+    Node<T> next;
+    Node<T> prev;
+
+    public Node(Node<T> prev, T value, Node<T> next) {
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
     }
 }
