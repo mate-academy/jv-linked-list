1e9ca5408d4eab367a9ae7ee9116c945448ee41c
Sander <alex.kapusniak@gmail.com>
2022-11-28 19:50:54 +0200

    change method add, create method unlink and getNode

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -22,22 +22,21 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public void add(T value, int index) {
         indexAddValidatorForAddElement(index);
-
-        if (index == 0 && size == 0) {
-            addElementToEmptyList(value, index);
-            return;
-        }
-
-        if (index == 0) {
-            addElementToStart(value);
-            return;
-        }
-
-        if (index == size) {
+        if (size > index) {
+            Node<T> currentNode = getNode(index);
+            if (currentNode == first) {
+                first = currentNode.prev = new Node<>(null, value, currentNode);
+                size++;
+            } else {
+                Node<T> prev = currentNode.prev;
+                Node<T> newNode = new Node<>(prev, value, currentNode);
+                prev.next = newNode;
+                currentNode.prev = newNode;
+                size++;
+            }
+        } else {
             add(value);
-            return;
         }
-        addElementToMiddle(value, index);
     }
 
     @Override
@@ -52,24 +51,13 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public T get(int index) {
         indexValidation(index);
-        Node<T> currentNode = first;
-        if (index == 0) {
-            return currentNode.item;
-        }
-
-        for (int i = 0; i < index; i++) {
-            currentNode = currentNode.next;
-        }
-        return currentNode.item;
+        return getNode(index).item;
     }
 
     @Override
     public T set(T value, int index) {
         indexValidation(index);
-        Node<T> currentNode = first;
-        for (int i = 0; i < index; i++) {
-            currentNode = currentNode.next;
-        }
+        Node<T> currentNode = getNode(index);
         T temp = currentNode.item;
         currentNode.item = value;
         return temp;
@@ -78,19 +66,14 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public T remove(int index) {
         indexValidation(index);
-
-        Node<T> currentNode = first;
-        for (int i = 0; i < index; i++) {
-            currentNode = currentNode.next;
-        }
-
+        Node<T> currentNode = getNode(index);
         Node<T> temp = currentNode;
         if (currentNode == first) {
             return deleteFirstElementInList(currentNode, temp);
         }
 
         if (currentNode == last) {
-            return removeElementFromEnd(currentNode);
+            return deleteElementFromEnd(currentNode);
         }
         return deleteElementFromMiddleOfList(currentNode);
     }
@@ -104,11 +87,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         if (findIndex(object) < 0) {
             return false;
         }
-
-        if (remove(findIndex(object)).equals(object)) {
-            return true;
-        }
-        return false;
+        return remove(findIndex(object)).equals(object);
     }
 
     @Override
@@ -119,33 +98,6 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public boolean isEmpty() {
         return size == 0;
-    }
-
-    private void addElementToMiddle(T value, int index) {
-        Node currentNode = first;
-        for (int i = 0; i < index; i++) {
-            currentNode = currentNode.next;
-        }
-
-        Node<T> prev = currentNode.prev;
-        prev.next = new Node<>(prev, value, currentNode);
-        currentNode.prev = prev.next;
-        prev.next.prev = currentNode;
-        size++;
-    }
-
-    private void addElementToStart(T value) {
-        Node<T> newNode = new Node<>(null, value, first);
-        first.prev = newNode;
-        first = newNode;
-        size++;
-        return;
-    }
-
-    private void addElementToEmptyList(T value, int index) {
-        add(value);
-        indexValidation(index);
-        return;
     }
 
     private void indexAddValidatorForAddElement(int index) {
@@ -171,12 +123,34 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return -1;
     }
 
+    private Node<T> getNode(int index) {
+        Node<T> currentNode;
+        if (index > size / 2) {
+            currentNode = last;
+            for (int i = size; i > index + 1; i--) {
+                currentNode = currentNode.prev;
+            }
+        } else {
+            currentNode = first;
+            for (int i = 0; i < index; i++) {
+                currentNode = currentNode.next;
+            }
+        }
+        return currentNode;
+    }
+
+    private void unlink(Node<T> node) {
+        node.prev = null;
+        node.next = null;
+    }
+
     private T deleteElementFromMiddleOfList(Node<T> currentNode) {
         Node<T> nextNode = currentNode.next;
         Node<T> prevNode = currentNode.prev;
         prevNode.next = nextNode;
         nextNode.prev = prevNode;
         size--;
+        unlink(currentNode);
         return currentNode.item;
     }
 
@@ -184,20 +158,23 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         if (currentNode.next == null) {
             first = last = null;
             size--;
+            unlink(currentNode);
             return currentNode.item;
         }
         Node nextNode = currentNode.next;
         nextNode.prev = null;
         first = nextNode;
         size--;
+        unlink(currentNode);
         return temp.item;
     }
 
-    private T removeElementFromEnd(Node<T> currentNode) {
+    private T deleteElementFromEnd(Node<T> currentNode) {
         Node prevNode = currentNode.prev;
         prevNode.next = null;
         last = prevNode;
         size--;
+        unlink(currentNode);
         return currentNode.item;
     }
 
