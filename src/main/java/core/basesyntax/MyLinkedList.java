4d22fa5730b99f17378033936239c1af07d40792
Bogdan Pokhilko <bogdanp1701@gmail.com>
2022-12-19 12:04:28 +0200

    Created constant value: size, head and tail; Created class Node with value, prev and next; Created a method for Node with inputs and inputs executions; Created addHead method, which adds 1st element to the list and checks if array is empty, or already consists of elements; Created addTail method which adds the last element to the list and links with previous element; Created addMidNode method, which adds new element between existing elements and links between them; Created getNodeIndex method, which searches for the correct element by given index. Depending on the number of the element and with help of for loop, moving from head, or tail of the list to find the right element; Created removeNode method which removing the element and unlinks it from the list; Created indexExists method which checks if the given index is not out of boundaries of an array size; In add method, checked if an element is first in the array therefore assign it as head, otherwise assign it as a tail; plus increase size by one; In add(value, index) method, checked if index is not negative and not bigger than size of the array. Checking the same requirement as add method and assigning head or tail, if the index is somewhere between the elements call addMidNode method, increase size by one; In addAll method, with help of for loop, add all the elements to the array; In get method check if the index exists, than call getNodeIndex method; In set method check if index exists, than adding new element to the array and link new element with neighbours; In remove method, check if index exists and call removeNode method; In remove(object) method, with help of while we go through the whole array and search for the asked object; In size method returning size value; In isEmpty method returning true if size equals 0;

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,45 +3,171 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private int size;
+    private Node<T> head;
+    private Node<T> tail;
+
+    class Node<T> {
+        private T value;
+        private Node<T> prev;
+        private Node<T> next;
+
+        public Node(T value, Node<T> prev, Node<T> next) {
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
     @Override
     public void add(T value) {
+        if (size == 0) {
+            addHead(value);
+        } else {
+            addTail(value);
+        }
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index >= 0 && index <= size) {
+            if (index == 0) {
+                addHead(value);
+            } else if (index == size) {
+                addTail(value);
+            } else {
+                addMidNode(getNodeIndex(index), value);
+            }
+            size++;
+        } else {
+            throw new IndexOutOfBoundsException("This index does not exist");
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T values : list) {
+            add(values);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (indexExists(index)) {
+            Node<T> testNode = getNodeIndex(index);
+            return testNode.value;
+        }
+        throw new IndexOutOfBoundsException("Couldn't find requested item by the index");
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        if (index >= 0 && index < size) {
+            Node<T> testNode = getNodeIndex(index);
+            T oldValue = testNode.value;
+            testNode.value = value;
+            return oldValue;
+        }
+        throw new IndexOutOfBoundsException("This index does not exist");
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (indexExists(index)) {
+            Node<T> testNode = getNodeIndex(index);
+            removeNode(testNode);
+            return testNode.value;
+        }
+        throw new IndexOutOfBoundsException("This index does not exist");
     }
 
     @Override
     public boolean remove(T object) {
+        Node<T> testNode = head;
+        while (testNode != null) {
+            if (testNode.value == object || (object != null && object.equals(testNode.value))) {
+                removeNode(testNode);
+                return true;
+            }
+            testNode = testNode.next;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private boolean indexExists(int index) {
+        return index >= 0 && index < size;
+    }
+
+    private T removeNode(Node<T> testNode) {
+        T testValue = null;
+        if (testNode == head) {
+            if (size == 1) {
+                testValue = head.value;
+                head = null;
+            } else {
+                head = head.next;
+                head.prev = null;
+            }
+        } else if (testNode == tail) {
+            testValue = tail.value;
+            tail = tail.prev;
+            tail.next = null;
+        } else {
+            testValue = testNode.value;
+            testNode.prev.next = testNode.next;
+            testNode.next.prev = testNode.prev;
+        }
+        size--;
+        return testValue;
+    }
+
+    private void addHead(T value) {
+        if (size == 0) {
+            head = new Node<>(value, null, tail);
+            tail = head;
+            return;
+        }
+        Node<T> testNode = new Node<>(value, null, head);
+        head.prev = testNode;
+        head = testNode;
+    }
+
+    private void addTail(T value) {
+        Node<T> testNode = new Node<>(value, tail, null);
+        tail.next = testNode;
+        tail = testNode;
+    }
+
+    private void addMidNode(Node<T> prevNode, T value) {
+        Node<T> testNode = new Node<>(value, prevNode.prev, prevNode);
+        prevNode.prev.next = testNode;
+        prevNode.prev = testNode;
+    }
+
+    private Node<T> getNodeIndex(int index) {
+        Node<T> testNode;
+        if (index <= size / 2) {
+            testNode = head;
+            for (int i = 0; i < index; i++) {
+                testNode = testNode.next;
+            }
+        } else {
+            testNode = tail;
+            for (int i = size - 1; i > index; i--) {
+                testNode = testNode.prev;
+            }
+        }
+        return testNode;
     }
 }
