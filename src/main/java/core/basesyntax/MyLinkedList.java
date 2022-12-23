dca69fb59d88ad48618027a0762b38f4d335333d
Oleksandr <116544768+OleksandrKostiukevych@users.noreply.github.com>
2022-12-23 18:31:21 +0200

    Update src/main/java/core/basesyntax/MyLinkedList.java
    Co-authored-by: Mykyta Arkhanhelskyi <nickarchangel97@gmail.com>

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -52,7 +52,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public T set(T value, int index) {
         checkIndex(index);
-        Node<T> replasedNode = getNodeByIndex(index);
+        Node<T> replacedNode = getNodeByIndex(index);
         T removedItem = replasedNode.value;
         replasedNode.value = value;
         return removedItem;
