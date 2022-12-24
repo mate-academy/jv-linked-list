9c52be7424f2aa3eef64533420c4b36e2713fddf
Richard <richarddkochut@gmail.com>
2022-12-24 20:10:15 +0200

    refactor

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -7,7 +7,83 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     private MyLinkedList.Node<T> first;
     private MyLinkedList.Node<T> last;
 
-    MyLinkedList.Node<T> node(int index) {
+    @Override
+    public void add(T value) {
+        final MyLinkedList.Node<T> newNode = new MyLinkedList.Node<>(last, value, null);
+        if (last == null) {
+            first = newNode;
+        } else {
+            last.next = newNode;
+        }
+        last = newNode;
+        size++;
+    }
+
+    @Override
+    public void add(T value, int index) {
+        checkPositionIndex(index);
+        if (index == size) {
+            add(value);
+        } else {
+            linkBefore(value, getNodeByIndex(index));
+        }
+    }
+
+    @Override
+    public void addAll(List<T> list) {
+        for (T element : list) {
+            add(element);
+        }
+    }
+
+    @Override
+    public T get(int index) {
+        checkElementIndex(index);
+        return getNodeByIndex(index).item;
+    }
+
+    @Override
+    public T set(T value, int index) {
+        checkElementIndex(index);
+        MyLinkedList.Node<T> newValue = getNodeByIndex(index);
+        T oldValue = newValue.item;
+        newValue.item = value;
+        return oldValue;
+    }
+
+    @Override
+    public T remove(int index) {
+        checkElementIndex(index);
+        Node<T> currentNode = getNodeByIndex(index);
+        T oldValue = currentNode.item;
+        unlink(currentNode);
+        return oldValue;
+    }
+
+    @Override
+    public boolean remove(T element) {
+        for (MyLinkedList.Node<T> removedNode = first; removedNode
+                != null; removedNode = removedNode.next) {
+            if (removedNode.item == element
+                    || (element != null && element.equals(removedNode.item))) {
+                unlink(removedNode);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private MyLinkedList.Node<T> getNodeByIndex(int index) {
         Node<T> foundedNode;
         if (index < (size / 2)) {
             foundedNode = first;
@@ -23,21 +99,9 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return foundedNode;
     }
 
-    private void checkElementIndex(int index) {
-        if (!(index >= 0 && index < size)) {
-            throw new IndexOutOfBoundsException("Index: " + index + " is more then size " + size);
-        }
-    }
-
-    private void checkPositionIndex(int index) {
-        if (!(index >= 0 && index <= size)) {
-            throw new IndexOutOfBoundsException("Index: " + index + " is more then size " + size);
-        }
-    }
-
-    void unlink(Node<T> removedNode) {
-        final MyLinkedList.Node<T> next = removedNode.next;
-        final MyLinkedList.Node<T> prev = removedNode.prev;
+    private void unlink(Node<T> removedNode) {
+        MyLinkedList.Node<T> next = removedNode.next;
+        MyLinkedList.Node<T> prev = removedNode.prev;
         if (prev == null) {
             first = next;
         } else {
@@ -54,18 +118,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         size--;
     }
 
-    void linkLast(T element) {
-        final MyLinkedList.Node<T> newNode = new MyLinkedList.Node<>(last, element, null);
-        if (last == null) {
-            first = newNode;
-        } else {
-            last.next = newNode;
-        }
-        last = newNode;
-        size++;
-    }
-
-    void linkBefore(T element, MyLinkedList.Node<T> afterNode) {
+    private void linkBefore(T element, MyLinkedList.Node<T> afterNode) {
         final MyLinkedList.Node<T> prev = afterNode.prev;
         final MyLinkedList.Node<T> newNode = new MyLinkedList.Node<>(prev, element, afterNode);
         if (prev == null) {
@@ -77,72 +130,16 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         size++;
     }
 
-    @Override
-    public void add(T value) {
-        linkLast(value);
-    }
-
-    @Override
-    public void add(T value, int index) {
-        checkPositionIndex(index);
-        if (index == size) {
-            linkLast(value);
-        } else {
-            linkBefore(value, node(index));
+    private void checkElementIndex(int index) {
+        if (!(index >= 0 && index < size)) {
+            throw new IndexOutOfBoundsException("Index: " + index + " is more then size " + size);
         }
     }
 
-    @Override
-    public void addAll(List<T> list) {
-        for (T element : list) {
-            add(element);
+    private void checkPositionIndex(int index) {
+        if (!(index >= 0 && index <= size)) {
+            throw new IndexOutOfBoundsException("Index: " + index + " is more then size " + size);
         }
-    }
-
-    @Override
-    public T get(int index) {
-        checkElementIndex(index);
-        return node(index).item;
-    }
-
-    @Override
-    public T set(T value, int index) {
-        checkElementIndex(index);
-        MyLinkedList.Node<T> newValue = node(index);
-        T oldValue = newValue.item;
-        newValue.item = value;
-        return oldValue;
-    }
-
-    @Override
-    public T remove(int index) {
-        checkElementIndex(index);
-        T oldValue = node(index).item;
-        unlink(node(index));
-        return oldValue;
-    }
-
-    @Override
-    public boolean remove(T element) {
-        for (MyLinkedList.Node<T> removedNode = first; removedNode
-                != null; removedNode = removedNode.next) {
-            if ((element != null && element.equals(removedNode.item))
-                    || removedNode.item == null && element == null) {
-                unlink(removedNode);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
     }
 
     private static class Node<T> {
