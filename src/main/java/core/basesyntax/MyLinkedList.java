aacf6fe2d92e9adc2f512541c2a0642387a9fb34
Butovych <dreamextreme29@gmail.com>
2022-12-25 19:21:14 +0200

    Third part

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -8,104 +8,127 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     private Node<T> last;
     int size;
 
-    private static class Node<T> {
-        T value;
-        MyLinkedList.Node<T> next;
-        MyLinkedList.Node<T> prev;
-        public Node(T value){
+    static class Node<T> {
+        private T value;
+        private Node<T> next;
+
+        private Node(T value) {
             this.value = value;
         }
-        Node(MyLinkedList.Node<T> prev, T value, MyLinkedList.Node<T> next) {
-            this.value = value;
-            this.next = next;
-            this.prev = prev;
+        public static <T> Node<T> valueOf(T value) {
+            return new Node<>(value);
         }
     }
 
     @Override
     public void add(T value) {
         Node<T> newNode = new Node<>(value);
-        if (first == null ) {
-            first = this.last = newNode;
+        if (size == 0) {
+            first = last = newNode;
+        } else {
+            last.next = newNode;
+            last = newNode;
         }
-        last.next = new Node<>(value);
-        last = newNode;
         size++;
     }
 
     @Override
     public void add(T value, int index) {
-        Objects.checkIndex(index, size + 1);
-        Node<T> newNode = new Node<>(value);
-        if (first == null){
-            first = last = newNode;
-        } else if (index == 0) {
-             newNode.next = first;
-             first = newNode;
+        Node<T> newNode = Node.valueOf(value);
+        if (index == 0) {
+            addAsHead(newNode);
         } else if (index == size) {
-            last.next = newNode;
-            last = newNode;
+            addAsTail(newNode);
         } else {
-            Node<T> current = getNodeByIndex(index - 1);
-            newNode.next = current.next;
-            current.next = newNode;
+            add(index, newNode);
         }
         size++;
     }
 
-    private Node<T> getNodeByIndex(int index) {
-        Node<T> current = first;
-        for (int i = 0; i < size; i++) {
-            current = current.next;
+    private void addAsHead(Node<T> newNode) {
+        newNode.next = first;
+        first = newNode;
+        if (first.next == null) {
+            last = first;
         }
-        return current;
     }
+
+    private void addAsTail(Node<T> newNode) {
+        last.next = newNode;
+        last = newNode;
+    }
+
+    private void add(int index, Node<T> newNode) {
+        Node<T> node = findNodeByIndex(index - 1);
+        newNode.next = node.next;
+        node.next = newNode;
+    }
+
+    private Node<T> findNodeByIndex(int index) {
+        Objects.checkIndex(index, size);
+        if (index == size - 1) {
+            return last;
+        } else {
+            return nodeAt(index);
+        }
+    }
+
+    private Node<T> nodeAt(int index) {
+        Node<T> currentNode = first;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode;
+    }
+
 
     @Override
     public void addAll(List<T> list) {
-        for (int i = 0; i <= list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             add(list.get(i));
         }
     }
 
     @Override
     public T get(int index) {
-        Objects.checkIndex(index,size);
-        return getNodeByIndex(index).value;
+        Node<T> node = findNodeByIndex(index);
+        return node.value;
     }
 
     @Override
     public T set(T value, int index) {
-        Objects.checkIndex(index, size);
-        Node<T> node = getNodeByIndex(index);
+        Node<T> node = findNodeByIndex(index);
         node.value = value;
-        return value;
+        return node.value;
     }
 
     @Override
     public T remove(int index) {
-        Objects.checkIndex(index, size);
-        T removedValue;
-        if (index == 0) {
-            removedValue = first.value;
-            first = first.next;
-            if (first == null){
-                last = null;
-            }
+        T deletedElement;
+        if (index == 0 && !isEmpty()) {
+            deletedElement = first.value;
+            removeHead();
         } else {
-            Node<T> prev = getNodeByIndex(index - 1);
-            removedValue = prev.next.value;
-            prev.next = prev.next.next;
+            Node<T> previousNode = findNodeByIndex(index - 1);
+            deletedElement = previousNode.next.value;
+            previousNode.next = previousNode.next.next;
             if (index == size - 1) {
-                last = prev;
+                last = previousNode;
             }
         }
-         size--;
-        return removedValue ;
+        size--;
+        return deletedElement;
+    }
+    private void removeHead() {
+        first = first.next;
+        if (first == null) {
+            last = null;
+        }
     }
 
     @Override
     public boolean remove(T object) {
+
         return false;
     }
 
