f93be176aea12a27e4f1f2ac51bfd4e216b93ec0
Serhii Berdychevskyi <sirko120690@mail.com>
2023-01-05 10:55:41 +0200

    Added changes second version hw jv-linked-list

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,27 +3,9 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
-
     private Node<T> head;
     private Node<T> tail;
     private int size;
-
-    static class Node<T> {
-        private T value;
-        private Node<T> prev;
-        private Node<T> next;
-
-        public Node(T val, Node<T> prev) {
-            this.value = val;
-            this.prev = prev;
-        }
-
-        public Node(T value, Node<T> prev, Node<T> next) {
-            this.prev = prev;
-            this.value = value;
-            this.next = next;
-        }
-    }
 
     @Override
     public void add(T value) {
@@ -154,5 +136,22 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             prev.next = next;
         }
         size--;
+    }
+
+    private static class Node<T> {
+        private T value;
+        private Node<T> prev;
+        private Node<T> next;
+
+        public Node(T val, Node<T> prev) {
+            this.value = val;
+            this.prev = prev;
+        }
+
+        public Node(T value, Node<T> prev, Node<T> next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
