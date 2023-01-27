b606ce079e1525c7c235231705b1ea22aeac0ecf
Mik911 <911Sokol@gmail.com>
2023-01-27 15:38:54 +0200

    Thank you for your comment. Code corrected

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -10,60 +10,6 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     public MyLinkedList() {
         head = null;
         tail = null;
-        size = 0;
-    }
-
-    private static class Node<T> {
-        private Node<T> prev;
-        private Node<T> next;
-        private T value;
-
-        public Node(Node<T> prev, Node<T> next, T value) {
-            this.prev = prev;
-            this.next = next;
-            this.value = value;
-        }
-
-        public T getValue() {
-            return value;
-        }
-
-        public void setValue(T value) {
-            this.value = value;
-        }
-    }
-
-    private void unlink(Node<T> node) {
-        // 1 - node - 3
-        if (node.prev == null) {
-            head = node.next;
-        } else {
-            node.prev.next = node.next;
-        }
-        if (node.next == null) {
-            tail = node.prev;
-        } else {
-            node.next.prev = node.prev;
-        }
-    }
-
-    private Node<T> getNode(int index) {
-        int count = 0;
-        Node<T> pointer = head;
-        while (pointer != null) {
-            if (count == index) {
-                return pointer;
-            }
-            pointer = pointer.next;
-            count++;
-        }
-        return null;
-    }
-
-    private void testIndex(int index) {
-        if (index >= size() || index < 0) {
-            throw new IndexOutOfBoundsException("index outside size: " + index);
-        }
     }
 
     @Override
@@ -144,7 +90,6 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         if (pointer != null) {
             T result = pointer.getValue();
             unlink(pointer);
-            size--;
             return result;
         }
         return toReturn;
@@ -157,7 +102,6 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             if (pointer.getValue() == object || pointer.getValue() != null
                     && (pointer.getValue().equals(object))) {
                 unlink(pointer);
-                size--;
                 return true;
             }
             pointer = pointer.next;
@@ -172,6 +116,59 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public boolean isEmpty() {
-        return head == null;
+        return size == 0;
+    }
+
+    private static class Node<T> {
+        private Node<T> prev;
+        private Node<T> next;
+        private T value;
+
+        public Node(Node<T> prev, Node<T> next, T value) {
+            this.prev = prev;
+            this.next = next;
+            this.value = value;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
+    }
+
+    private void unlink(Node<T> node) {
+        if (node.prev == null) {
+            head = node.next;
+        } else {
+            node.prev.next = node.next;
+        }
+        if (node.next == null) {
+            tail = node.prev;
+        } else {
+            node.next.prev = node.prev;
+        }
+        size--;
+    }
+
+    private Node<T> getNode(int index) {
+        int count = 0;
+        Node<T> pointer = head;
+        while (pointer != null) {
+            if (count == index) {
+                return pointer;
+            }
+            pointer = pointer.next;
+            count++;
+        }
+        return null;
+    }
+
+    private void testIndex(int index) {
+        if (index >= size() || index < 0) {
+            throw new IndexOutOfBoundsException("index: " + index + " outside size: " + size);
+        }
     }
 }
