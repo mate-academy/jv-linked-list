3bc0e0401231c9261fc8b1abee0bb1bb8ed769c6
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-02-21 22:46:51 +0200

    Node

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,6 +3,9 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    T value;
+    Node<T> prev;
+    Node<T> next;
     @Override
     public void add(T value) {
     }
@@ -43,5 +46,16 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public boolean isEmpty() {
         return false;
+    }
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+        private Node<E> prev;
+
+        private Node(Node<E> prev, E item, Node<E> next) {
+            this.item = item;
+            this.next = next;
+            this.prev = prev;
+        }
     }
 }
