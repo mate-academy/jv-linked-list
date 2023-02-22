39ab8fafe0d2c63300c6904184c6135f119e1021
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2023-02-22 15:50:03 +0200

    realize add & get methods

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,21 +3,53 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int length;
+
+
     @Override
     public void add(T value) {
+        if (isEmpty()) {
+            head = tail = new Node<T>(null, value, null);
+            //tail = head;
+        } else {
+            tail.next = new Node<T>(tail, value, null);
+            tail = tail.next;
+        }
+        length++;
     }
 
     @Override
     public void add(T value, int index) {
+        isIndexValidForAdd(index);
+        if (index == length) {
+            add(value);
+            return;
+        }
+        Node<T> nextNode = getNode(index);
+        Node<T> prevNextNode = nextNode.prev;
+        Node<T> newNode = new Node<>(prevNextNode, value, nextNode);
+        nextNode.prev = newNode;
+        if (prevNextNode != null) {
+            nextNode.next = newNode;
+        } else {
+            head = newNode;
+        }
+        length++;
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T listElement : list) {
+            add(listElement);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        isIndexValidForGet(index);
+        return getNode(index).item;
     }
 
     @Override
@@ -37,11 +69,68 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public int size() {
-        return 0;
+        return length;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return length == 0;
+    }
+
+    private void isIndexValidForGet(int index) {
+        if (index < 0 || index >= length) {
+            throw new IndexOutOfBoundsException("Index: " + index + " is out of scope");
+        }
+    }
+
+    private void isIndexValidForAdd(int index) {
+        if (index < 0 || index > length) {
+            throw new IndexOutOfBoundsException("Index: " + index + " is out of scope");
+        }
+    }
+
+    private Node<T> getNode(int index) {
+        isIndexValidForGet(index);
+        Node<T> current;
+        if ((length / 2) > index) {
+            current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+        } else {
+            current = tail;
+            for (int i = length; i > (index + 1); i--) {
+                current = current.prev;
+            }
+        }
+        return current;
+    }
+
+    private void unlink(Node<T> node) {
+        length--;
+        if (node.next == null) {
+            if (node.prev != null) {
+                node.prev.next = null;
+            }
+            tail = node.prev;
+        } else if (node.prev == null) {
+            node.next.prev = null;
+            head = node.next;
+        } else {
+            node.next.prev = node.prev;
+            node.prev.next = node.next;
+        }
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> prev;
+        private Node<E> next;
+
+        private Node(Node<E> prev, E item,  Node<E> next) {
+            this.prev = prev;
+            this.item = item;
+            this.next = next;
+        }
     }
 }
