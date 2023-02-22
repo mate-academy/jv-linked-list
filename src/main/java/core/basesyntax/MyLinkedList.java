7aa6246e16af617b7360e8c59a34d37b2e79f55c
Yehor-Cherkashyn <gooooodvin1995@gmail.com>
2023-02-22 09:59:16 +0200

    Replaced public methods

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -7,6 +7,84 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     private Node<T> tail;
     private int size;
 
+    @Override
+    public void add(T value) {
+        if (head == null) {
+            linkFirst(value);
+        } else {
+            linkLast(value);
+        }
+    }
+
+    @Override
+    public void add(T value, int index) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Index Out Of Bound");
+        }
+        if (index == size) {
+            linkLast(value);
+        } else {
+            linkBefore(value, searchByIndex(index));
+        }
+    }
+
+    @Override
+    public void addAll(List<T> list) {
+        for (T element : list) {
+            add(element);
+        }
+    }
+
+    @Override
+    public T get(int index) {
+        checkIndex(index);
+        return searchByIndex(index).value;
+    }
+
+    @Override
+    public T set(T value, int index) {
+        checkIndex(index);
+        Node<T> searchedNode = searchByIndex(index);
+        T oldVal = searchedNode.value;
+        searchedNode.value = value;
+        return oldVal;
+    }
+
+    @Override
+    public T remove(int index) {
+        checkIndex(index);
+        return unlink(searchByIndex(index));
+    }
+
+    @Override
+    public boolean remove(T object) {
+        for (Node<T> x = head; x != null; x = x.next) {
+            if (x.value == object) {
+                unlink(x);
+                return true;
+            }
+        }
+        if (object != null) {
+            for (Node<T> x = head; x != null; x = x.next) {
+                if (object.equals(x.value)) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
     private void linkFirst(T value) {
         final Node<T> h = head;
         final Node<T> newNode = new Node<>(null, value, h);
@@ -62,49 +140,6 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return search;
     }
 
-    @Override
-    public void add(T value) {
-        if (head == null) {
-            linkFirst(value);
-        } else {
-            linkLast(value);
-        }
-    }
-
-    @Override
-    public void add(T value, int index) {
-        if (index > size || index < 0) {
-            throw new IndexOutOfBoundsException("Index Out Of Bound");
-        }
-        if (index == size) {
-            linkLast(value);
-        } else {
-            linkBefore(value, searchByIndex(index));
-        }
-    }
-
-    @Override
-    public void addAll(List<T> list) {
-        for (T element : list) {
-            add(element);
-        }
-    }
-
-    @Override
-    public T get(int index) {
-        checkIndex(index);
-        return searchByIndex(index).value;
-    }
-
-    @Override
-    public T set(T value, int index) {
-        checkIndex(index);
-        Node<T> searchedNode = searchByIndex(index);
-        T oldVal = searchedNode.value;
-        searchedNode.value = value;
-        return oldVal;
-    }
-
     private T unlink(Node<T> unlinkElement) {
         final T unlinkValue = unlinkElement.value;
         final Node<T> nextFromUnlink = unlinkElement.next;
@@ -127,41 +162,6 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         unlinkElement.value = null;
         size--;
         return unlinkValue;
-    }
-
-    @Override
-    public T remove(int index) {
-        checkIndex(index);
-        return unlink(searchByIndex(index));
-    }
-
-    @Override
-    public boolean remove(T object) {
-        for (Node<T> x = head; x != null; x = x.next) {
-            if (x.value == object) {
-                unlink(x);
-                return true;
-            }
-        }
-        if (object != null) {
-            for (Node<T> x = head; x != null; x = x.next) {
-                if (object.equals(x.value)) {
-                    unlink(x);
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
     }
 
     private void checkIndex(int index) {
