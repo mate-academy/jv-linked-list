51df27e3949ac365768b7800fd81f4300b538872
kromchk <kromchyk@yahoo.com>
2023-02-23 13:45:58 +0200

    double-linked list relize

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,45 +3,141 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node<T> first;
+    private Node<T> last;
+    private int size;
+
     @Override
     public void add(T value) {
+        Node<T> newNode = new Node<>(value, null, null);
+        newNode.prev = last;
+        if (last != null) {
+            last.next = newNode;
+        }
+        if (first == null) {
+            first = newNode;
+        }
+        last = newNode;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index == size) {
+            add(value);
+        } else {
+            Node<T> newNode = new Node<>(value, null, null);
+            Node<T> tempPoint = getByIndex(index);
+            newNode.prev = tempPoint.prev;
+            newNode.next = tempPoint;
+            if (tempPoint.prev != null) {
+                tempPoint.prev.next = newNode;
+            }
+            tempPoint.prev = newNode;
+            if (newNode.prev == null) {
+                first = newNode;
+            }
+            size++;
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
+        if (list != null) {
+            for (T element : list) {
+                add(element);
+            }
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        Node<T> tempPoint = getByIndex(index);
+        return tempPoint != null ? tempPoint.data : null;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        T result = null;
+        Node<T> tempPoint = getByIndex(index);
+        if (tempPoint != null) {
+            result = tempPoint.data;
+            tempPoint.data = value;
+        }
+        return result;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        T result = null;
+        Node<T> tempPoint = getByIndex(index);
+        if (tempPoint != null) {
+            result = tempPoint.data;
+            if (tempPoint.next != null) {
+                tempPoint.next.prev = tempPoint.prev;
+            }
+            if (tempPoint.prev != null) {
+                tempPoint.prev.next = tempPoint.next;
+            }
+            if (tempPoint.prev == null) {
+                first = tempPoint.next;
+            }
+            if (tempPoint.next == null) {
+                last = tempPoint.prev;
+            }
+            size--;
+        }
+        return result;
     }
 
     @Override
     public boolean remove(T object) {
-        return false;
+        boolean result = false;
+        Node<T> tempPoint = first;
+        for (int i = 0; i < size; i++) {
+            if (tempPoint != null && (tempPoint.data == object || (tempPoint.data != null
+                    && tempPoint.data.equals(object)))) {
+                remove(i);
+                result = true;
+                break;
+            }
+            tempPoint = tempPoint.next;
+        }
+        return result;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private Node<T> getByIndex(int index) {
+        if (index < 0 || index > size - 1) {
+            throw new IndexOutOfBoundsException("Invalid index : " + index + " Out bounds of List");
+        }
+        Node<T> tempPoint = first;
+        if (tempPoint != null) {
+            for (int i = 0; i < index; i++) {
+                tempPoint = tempPoint.next;
+            }
+        }
+        return tempPoint;
+    }
+
+    private static class Node<T> {
+        private Node<T> next;
+        private Node<T> prev;
+        private T data;
+
+        public Node(T data, Node<T> prev, Node<T> next) {
+            this.prev = prev;
+            this.next = next;
+            this.data = data;
+        }
     }
 }
