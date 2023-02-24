dde36fecb345990fdd4d8056408888b5e7337d10
kromchk <kromchyk@yahoo.com>
2023-02-24 14:23:40 +0200

    double-linked list relize, attempt #4 what is it wrong again yet

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -67,36 +67,22 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public T remove(int index) {
         Node<T> tempPoint = getByIndex(index);
-        if (tempPoint.next != null) {
-            tempPoint.next.prev = tempPoint.prev;
-        }
-        if (tempPoint.prev != null) {
-            tempPoint.prev.next = tempPoint.next;
-        }
-        if (tempPoint.prev == null) {
-            first = tempPoint.next;
-        }
-        if (tempPoint.next == null) {
-            last = tempPoint.prev;
-        }
-        size--;
+        unlink(tempPoint);
         return tempPoint.data;
     }
 
     @Override
     public boolean remove(T object) {
-        boolean result = false;
         Node<T> tempPoint = first;
         for (int i = 0; i < size; i++) {
-            if (tempPoint != null && (tempPoint.data == object || (tempPoint.data != null
-                    && tempPoint.data.equals(object)))) {
-                exclude(tempPoint);
-                result = true;
-                break;
+            if (tempPoint.data == object || (tempPoint.data != null
+                    && tempPoint.data.equals(object))) {
+                unlink(tempPoint);
+                return true;
             }
             tempPoint = tempPoint.next;
         }
-        return result;
+        return false;
     }
 
     @Override
@@ -109,7 +95,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return size == 0;
     }
 
-    private void exclude(Node<T> node) {
+    private void unlink(Node<T> node) {
         if (node.prev != null) {
             node.prev.next = node.next;
         } else {
@@ -128,10 +114,14 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             throw new IndexOutOfBoundsException("Invalid index : " + index + " Out bounds of List");
         }
         Node<T> tempPoint = index < (size >> 1) ? first : last;
-        boolean direct = tempPoint == first;
-        for (int i = direct ? 0 : size - 1; direct ? i < index : i > index;
-                i = direct ? i + 1 : i - 1) {
-            tempPoint = direct ? tempPoint.next : tempPoint.prev;
+        if (tempPoint == first) {
+            for (int i = 0; i < index; i++) {
+                tempPoint = tempPoint.next;
+            }
+        } else {
+            for (int i = size - 1; i > index; i--) {
+                tempPoint = tempPoint.prev;
+            }
         }
         return tempPoint;
     }
@@ -146,17 +136,5 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             this.next = next;
             this.data = data;
         }
-    }
-
-    public static void main(String[] args) {
-        MyLinkedListInterface<String> myLinkedList = new MyLinkedList<>();
-        myLinkedList.add("FIRST_ITEM");
-        System.out.println(myLinkedList.size());
-        boolean firstRemove = myLinkedList.remove("FIRST_ITEM");
-        System.out.println(myLinkedList.size());
-        myLinkedList.add("SECOND_ITEM");
-        System.out.println(myLinkedList.size());
-        boolean secondRemove = myLinkedList.remove("FIRST_ITEM");
-        System.out.println(myLinkedList.size());
     }
 }
