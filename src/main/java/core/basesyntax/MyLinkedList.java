6c1aed2f0b69a74e11b95111a67e69d727ca525c
Vitalii Romanyuk <vitaliy37777@gmail.com>
2023-03-11 15:40:33 +0200

    did more readable code

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,12 +3,14 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
-    private int size;
     private Node<T> head;
     private Node<T> tail;
+    private int size;
 
     public MyLinkedList() {
         this.size = 0;
+        this.head = null;
+        this.tail = null;
     }
 
     @Override
@@ -18,7 +20,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public void add(T value, int index) {
-        int linkedListSize = this.size;
+        int linkedListSize = size;
         checkOnIndexOutOfBoundsException(index, false);
 
         if (linkedListSize == 0) {
@@ -66,8 +68,8 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public boolean remove(T object) {
         boolean isNode = false;
-        Node<T> node = this.head;
-        if (this.size != 0) {
+        Node<T> node = head;
+        if (size != 0) {
             do {
                 T value = node.value;
                 if ((value == null && object == null)
@@ -85,70 +87,70 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public int size() {
-        return this.size;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return this.size == 0;
+        return size == 0;
     }
 
     private void addHead(T value) {
-        Node<T> temp = this.head;
+        Node<T> temp = head;
         Node<T> node = new Node<>(null, value, temp);
-        this.head = node;
+        head = node;
         if (temp == null) {
-            this.tail = node;
+            tail = node;
         } else {
             temp.prev = node;
         }
-        this.size++;
+        size++;
     }
 
     private void addTail(T value) {
-        Node<T> temp = this.tail;
+        Node<T> temp = tail;
         Node<T> node = new Node<>(temp, value, null);
-        this.tail = node;
-        if (temp == null) {
-            this.head = node;
-        } else {
-            temp.next = node;
-        }
-        this.size++;
+        tail = node;
+        checkOnNull(temp, node);
+        size++;
     }
 
     private void addBetween(T value, Node<T> findNode) {
         Node<T> prev = findNode.prev;
         Node<T> newNode = new Node<>(prev, value, findNode);
         findNode.prev = newNode;
-        if (prev == null) {
-            this.head = newNode;
+        checkOnNull(prev, newNode);
+        size++;
+    }
+
+    private void checkOnNull(Node<T> checkNode, Node<T> useNode) {
+        if (checkNode == null) {
+            head = useNode;
         } else {
-            prev.next = newNode;
+            checkNode.next = useNode;
         }
-        this.size++;
     }
 
     private Node<T> findNode(int index) {
-        int sizeLinkedList = this.size;
+        int sizeLinkedList = size;
         if (sizeLinkedList == 0) {
-            return this.head;
+            return head;
         }
         if (index == sizeLinkedList) {
-            return this.tail;
+            return tail;
         }
 
         Node<T> needNode;
         int middle = sizeLinkedList / 2;
         if (index > middle) {
-            needNode = this.tail;
+            needNode = tail;
             while (sizeLinkedList - 1 != index) {
                 needNode = needNode.prev;
                 sizeLinkedList--;
             }
         } else {
             int checkIndex = 0;
-            needNode = this.head;
+            needNode = head;
             while (checkIndex != index) {
                 needNode = needNode.next;
                 checkIndex++;
@@ -161,31 +163,29 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         Node<T> prev = nodeForRemove.prev;
         Node<T> next = nodeForRemove.next;
         if (prev == null && next == null) {
-            this.head = null;
-            this.tail = null;
-            this.size = 0;
+            head = null;
+            tail = null;
+            size = 0;
         } else {
             if (prev == null) {
-                this.head = next;
-                next.prev = null;
+                head = next;
             } else if (next == null) {
-                this.tail = prev;
-                prev.next = null;
+                tail = prev;
             } else {
                 prev.next = next;
                 next.prev = prev;
             }
-            this.size--;
+            size--;
         }
     }
 
-    private void checkOnIndexOutOfBoundsException(int index, boolean isGet) {
+    private void checkOnIndexOutOfBoundsException(int index, boolean isGetIndex) {
         int newIndexForGet = index;
-        if (isGet) {
+        if (isGetIndex) {
             newIndexForGet++;
         }
 
-        if (newIndexForGet > this.size || index < 0) {
+        if (newIndexForGet > size || index < 0) {
             throw new IndexOutOfBoundsException("Index out of bounds: " + index);
         }
     }
