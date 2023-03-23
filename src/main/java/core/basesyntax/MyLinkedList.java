e4b599fe6f19de95f9ab90951adea2645fbc844a
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-03-23 03:09:26 +0200

    initial commit, paÑrtly implemented "add" method

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -1,14 +1,38 @@
 package core.basesyntax;
 
+import java.util.LinkedList;
 import java.util.List;
 
+
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    Node<T> head;
+    Node<T> tale;
+
     @Override
     public void add(T value) {
+        new Node(tale, value, null); // todo
     }
 
     @Override
     public void add(T value, int index) {
+        int indexCounter = 0;
+        if (index == 0) {
+            Node<T> initialHeadNode = head;
+            head = new Node<>(null, value, initialHeadNode);
+            initialHeadNode.prev = head;
+            return;
+        }
+        Node<T> tempNode = head.next;
+        while (indexCounter != index) {
+            indexCounter++;
+            if (indexCounter == index) {
+                Node<T> toAdd = new Node<>(tempNode.prev, value, tempNode);
+                tempNode.prev.next = toAdd;
+                tempNode.prev = toAdd;
+            }
+            tempNode = tempNode.next;
+
+        }
     }
 
     @Override
@@ -43,5 +67,17 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public boolean isEmpty() {
         return false;
+    }
+
+    private static class Node<T>{
+        Node<T> prev;
+        Node<T> next;
+        T body;
+
+        public Node(Node<T> prev, T body, Node<T> next) {
+            this.prev = prev;
+            this.next = next;
+            this.body = body;
+        }
     }
 }
