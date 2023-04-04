b08302b898bc66aabd9aa57a585f171223a33485
Artem <artempharm83@gmail.com>
2023-04-04 17:45:22 +0300

    create Node added get and set for Node class fields made an implementation for LinkedList

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -1,47 +1,134 @@
 package core.basesyntax;
 
 import java.util.List;
+import java.util.Objects;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
     @Override
     public void add(T value) {
+        Node<T> newNode = new Node<>(null, value, tail);
+        if (isEmpty()) {
+            head = newNode;
+        } else {
+            tail.setNextElement(newNode);
+        }
+        tail = newNode;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index == size) {
+            add(value);
+            return;
+        }
+        Node<T> nodeAtIndex = getNodeAtIndex(index);
+        Node<T> nodeBeforeIndex = nodeAtIndex.getPrevElement();
+        Node<T> newNode = new Node<>(nodeAtIndex, value, nodeBeforeIndex);
+        if (nodeBeforeIndex == null) {
+            head = newNode;
+        } else {
+            nodeBeforeIndex.setNextElement(newNode);
+        }
+        nodeAtIndex.setPrevElement(newNode);
+        size++;
+    }
+
+    private Node<T> getNodeAtIndex(int index) {
+        checkIndexException(index);
+        Node<T> temp;
+        if (index < size / 2) {
+            temp = head;
+            for (int i = 0; i < index; i++) {
+                temp = temp.getNextElement();
+            }
+        } else {
+            temp = tail;
+            for (int i = size - 1; i > index; i--) {
+                temp = temp.getPrevElement();
+            }
+        }
+        return temp;
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T element : list) {
+            add(element);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndexException(index);
+        Node<T> nodeAtIndex = getNodeAtIndex(index);
+        return nodeAtIndex.getItemIndex();
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        checkIndexException(index);
+        Node<T> temp = getNodeAtIndex(index);
+        T oldItem = temp.getItemIndex();
+        temp.setItem(value);
+        return oldItem;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndexException(index);
+        Node<T> deleteNode = getNodeAtIndex(index);
+        if (deleteNode == null) {
+            throw new IndexOutOfBoundsException("Index out of bounds: " + index);
+        }
+        Node<T> nextNode = deleteNode.getNextElement();
+        Node<T> prevNode = deleteNode.getPrevElement();
+        if (prevNode == null) {
+            head = nextNode;
+        } else {
+            prevNode.setNextElement(nextNode);
+        }
+        if (nextNode == null) {
+            tail = prevNode;
+        } else {
+            nextNode.setPrevElement(prevNode);
+        }
+        size--;
+        return deleteNode.getItemIndex();
     }
 
     @Override
     public boolean remove(T object) {
+        Node<T> temp = head;
+        int index = 0;
+        while (temp != null) {
+            if (Objects.equals(temp.getItemIndex(), object)) {
+                remove(index);
+                return true;
+            }
+            temp = temp.getNextElement();
+            index++;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return head == null;
+    }
+
+    private void checkIndexException(int index) {
+        if (index < 0 || index > size || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds: " + index);
+        }
     }
 }
