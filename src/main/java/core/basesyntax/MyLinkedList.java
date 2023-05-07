d8ce193c1f9256de576abedacd9e0af3122f9235
Mykola Panasiuk <mikola.panasyuk.01@gmail.com>
2023-05-07 15:24:21 +0300

    LinkedList has been implemented

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,45 +3,179 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private int size;
+    private Node<T> head;
+    private Node<T> tail;
+
     @Override
     public void add(T value) {
+        Node<T> node = new Node<>(tail, value);
+        if (isEmpty()) {
+            head = node;
+            tail = head;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        checkAddRange(index);
+        if (index == 0 && !isEmpty()) {
+            addFirst(value);
+        } else if (index == size) {
+            add(value);
+        } else {
+            insert(value, index);
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T t : list) {
+            add(t);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkRange(index);
+        Node<T> currentNode = getNode(index);
+        return currentNode.value;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        checkRange(index);
+        Node<T> node = getNode(index);
+        T oldValue = node.value;
+        node.value = value;
+        return oldValue;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkRange(index);
+        return unlink(getNode(index));
     }
 
     @Override
     public boolean remove(T object) {
+        for (int i = 0; i < size; i++) {
+            if (areEqualsValues(get(i), object)) {
+                unlink(getNode(i));
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void addFirst(T value) {
+        Node<T> insertNode = new Node<>(value, head);
+        head.prev = insertNode;
+        head = insertNode;
+        size++;
+    }
+
+    private void insert(T value, int index) {
+        Node<T> oldNode = getNode(index);
+        Node<T> insertNode = new Node<>(oldNode.prev, value, oldNode);
+        oldNode.prev.next = insertNode;
+        oldNode.prev = insertNode;
+        size++;
+    }
+
+    private T unlink(Node<T> removedNode) {
+        final T element = removedNode.value;
+        final Node<T> next = removedNode.next;
+        final Node<T> prev = removedNode.prev;
+
+        if (prev == null) {
+            head = next;
+        } else {
+            prev.next = next;
+            removedNode.prev = null;
+        }
+
+        if (next == null) {
+            tail = prev;
+        } else {
+            next.prev = prev;
+            removedNode.next = null;
+        }
+
+        removedNode.value = null;
+        size--;
+        return element;
+    }
+
+    private void checkAddRange(int index) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Illegal index: " + index);
+        }
+    }
+
+    private void checkRange(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Illegal index: " + index);
+        }
+    }
+
+    private Node<T> getNode(int index) {
+        return (index <= (size >> 1)) ? searchFromHead(index) : searchFromTail(index);
+    }
+
+    private Node<T> searchFromHead(int index) {
+        Node<T> currentNode = head;
+        for (int i = 0; i < index; ++i) {
+            currentNode = currentNode.next;
+        }
+        return currentNode;
+    }
+
+    private Node<T> searchFromTail(int index) {
+        Node<T> currentNode = tail;
+        for (int i = size - 1; i > index; --i) {
+            currentNode = currentNode.prev;
+        }
+        return currentNode;
+    }
+
+    private boolean areEqualsValues(T first, T second) {
+        return first == second || first != null && first.equals(second);
+    }
+
+    private static class Node<T> {
+        private Node<T> prev;
+        private T value;
+        private Node<T> next;
+
+        private Node(Node<T> prev, T value, Node<T> next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+        }
+
+        private Node(Node<T> prev, T value) {
+            this.prev = prev;
+            this.value = value;
+        }
+
+        private Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
     }
 }
