fa55035b4891320f604bb8818195d661d77d4ea7
Denis Balako <denisarius2000s@gmail.com>
2023-07-08 11:59:35 +0300

    fixed build

--- src/main/java/core/basesyntax/service/ObjectService.java
+++ src/main/java/core/basesyntax/service/ObjectService.java
@@ -2,6 +2,7 @@ package core.basesyntax.service;
 
 public class ObjectService {
     public static boolean equals(Object firstObject, Object secondObject) {
-        return firstObject == secondObject || firstObject != null && firstObject.equals(secondObject);
+        return firstObject == secondObject
+                || firstObject != null && firstObject.equals(secondObject);
     }
 }
