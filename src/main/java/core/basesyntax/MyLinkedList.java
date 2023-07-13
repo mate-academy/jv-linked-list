9d49c3fb1e6ef517d8e979ea5789ab6a892cce88
Oleg Stashkiv <stashkivolegg@gmail.com>
2023-07-13 12:33:44 +0300

    Fixed remarks about code reuse

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -18,8 +18,8 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public void add(T value, int index) {
-        if (index < 0 || index > size) {
-            throw new IndexOutOfBoundsException(getErrorMessage(index));
+        if (index != size) {
+            checkIndex(index);
         }
         if (index == size) {
             linkLast(value);
