577159535ea67aa0da7f3ec4e31f4fa62e4b78b9
Olexander Parkhomovskyi <sanekparkhomovsky@gmail.com>
2023-07-13 07:24:39 +0300

    constructor modified, added builder

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,54 +3,98 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private final BuilderNode<T> builderNode = new BuilderNode<T>();
     private Node<T> head;
     private Node<T> tail;
     private int size;
 
-    public static class Node<E> {
+    private static class Node<E> {
         private E item;
         private Node<E> next;
         private Node<E> prev;
 
-        public Node(E element) {
+        private Node(Node<E> prev, E element, Node<E> next) {
             this.item = element;
+            this.next = next;
+            this.prev = prev;
         }
     }
 
+    public static class BuilderNode<E> {
+        private E item;
+        private Node<E> next;
+        private Node<E> prev;
+
+        public BuilderNode<E> setItem(E item) {
+            this.item = item;
+            return this;
+        }
+
+        public BuilderNode<E> setNext(Node<E> next) {
+            this.next = next;
+            return this;
+        }
+
+        public BuilderNode<E> setPrev(Node<E> prev) {
+            this.prev = prev;
+            return this;
+        }
+
+        public Node<E> build() {
+            return new Node<>(prev, item, next);
+        }
+    }
+
+
     @Override
     public void add(T value) {
-        Node<T> newNode = new Node<>(value);
+        Node<T> newNode;
+        builderNode.setItem(value);
         if (size == 0) {
+            newNode = builderNode
+                    .setPrev(tail)
+                    .setNext(head)
+                    .build();
             head = tail = newNode;
         } else {
+            newNode = builderNode
+                    .setPrev(tail)
+                    .build();
             tail.next = newNode;
-            newNode.prev = tail;
             tail = newNode;
         }
         size++;
     }
+
 
     @Override
     public void add(T value, int index) {
         if (index < 0) {
             throw new IndexOutOfBoundsException("Negative index: " + index);
         }
-        Node<T> newNode = new Node<>(value);
+        Node<T> newNode;
+        builderNode.setItem(value);
         if (head == null) {
-            head = tail = newNode;
+            head = tail = builderNode.build();
         } else if (index == 0) {
-            newNode.next = head;
+            newNode = builderNode
+                    .setNext(head)
+                    .build();
             head.prev = newNode;
             head = newNode;
         } else if (index == size) {
+            newNode = builderNode
+                    .setPrev(tail)
+                    .build();
             tail.next = newNode;
-            newNode.prev = tail;
             tail = newNode;
         } else {
             checkIndex(index);
             Node<T> prev = getNodeByIndex(index - 1);
-            newNode.next = prev.next;
-            newNode.prev = prev;
+            newNode = builderNode
+                    .setNext(prev.next)
+                    .setPrev(prev)
+                    .build();
             prev.next.prev = newNode;
             prev.next = newNode;
         }
