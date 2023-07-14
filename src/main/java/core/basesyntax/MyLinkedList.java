c1711bea2337a80af8e267436d946df7b59bfea6
Vladiclevch <levchevlad@gmail.com>
2023-07-14 09:19:54 +0300

    implemented LinkedList task

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,45 +3,148 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+
+    static class Node<T> {
+        T element;
+        Node<T> next;
+
+        public Node(T element) {
+            this.element = element;
+        }
+    }
+
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    public static <T> List<T> of(T... elements) {
+        MyLinkedList<T> linkedList = new MyLinkedList<>();
+        for (T e : elements) {
+            linkedList.add(e);
+        }
+        return (List<T>) linkedList;
+    }
+
     @Override
     public void add(T value) {
+        Node<T> newNode = new Node<>(value);
+        if (size == 0) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        indexCheck(index);
+        Node<T> newNode = new Node<>(value);
+        if (head == null) {
+            head = tail = newNode;
+        } else if (index == 0) {
+            newNode.next = head;
+            head = newNode;
+        } else if (index == size) {
+            tail.next = newNode;
+            tail = newNode;
+        } else {
+            Node<T> previous = getNodeByIndex(index - 1);
+            newNode.next = previous.next;
+            previous.next = newNode;
+        }
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T element : list) {
+            add(element);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        indexCheck(index);
+        return getNodeByIndex(index).element;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        indexCheck(index);
+        Node<T> node = getNodeByIndex(index);
+        T oldValue = node.element;
+        node.element = value;
+        return oldValue;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Unable to set index: " + index);
+        }
+        T removedElement;
+        if (index == 0) {
+            removedElement = head.element;
+            head = head.next;
+            if (head == null) {
+                tail = null;
+            }
+        } else if (index == size - 1) {
+            Node<T> previous = getNodeByIndex(index - 1);
+            removedElement = tail.element;
+            previous.next = null;
+            tail = previous;
+        } else {
+            Node<T> previous = getNodeByIndex(index - 1);
+            Node<T> current = previous.next;
+            removedElement = current.element;
+            previous.next = current.next;
+        }
+        size--;
+        return removedElement;
     }
 
     @Override
     public boolean remove(T object) {
+        Node<T> current = head;
+        int index = 0;
+        do {
+            if (object == current.element || (current.element != null && current.element.equals(object))) {
+                remove(index);
+                return true;
+            }
+            index++;
+            current = current.next;
+        } while (current != null);
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return head == null;
+    }
+
+    private void indexCheck (int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Unable to set index: " + index);
+        }
+    }
+
+    private Node<T> getNodeByIndex (int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Unable to set index: " + index);
+        }
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current;
     }
 }
