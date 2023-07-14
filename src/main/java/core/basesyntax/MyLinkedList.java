2bdbd83ad06bdec278017daed1b0c3653de1cbb6
DaniilNepelev <kakashilvng@gmail.com>
2023-07-14 10:55:08 +0300

    done solution and checked mvn clean package

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -1,47 +1,179 @@
 package core.basesyntax;
 
 import java.util.List;
+import java.util.Objects;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private int size;
+    private Node<T> head;
+    private Node<T> tail;
+
     @Override
     public void add(T value) {
+        if (size == 0) {
+            Node<T> node = new Node<>(value);
+            node.prev = null;
+            node.next = null;
+            head = node;
+            tail = node;
+            size++;
+        } else {
+            addNode(value);
+        }
     }
 
     @Override
     public void add(T value, int index) {
+        findIndex(index);
+        if (index == size && index != 0) {
+            addNode(value);
+        } else if (index == 0) {
+            addFirst(value);
+        } else {
+            Node<T> newNode = new Node<>(value);
+            Node<T> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            newNode.prev = current;
+            current.next.prev = newNode;
+            current.next = newNode;
+            size++;
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T element : list) {
+            add(element);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        findIndexGet(index);
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.item;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        findIndexGet(index);
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        T oldValue = current.item;
+        current.item = value;
+        return oldValue;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        findIndexGet(index);
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        if (current == head) {
+            head = current.next;
+        } else {
+            current.prev.next = current.next;
+        }
+        if (current == tail) {
+            tail = current.prev;
+        } else {
+            current.next.prev = current.prev;
+        }
+        size--;
+        return current.item;
     }
 
     @Override
     public boolean remove(T object) {
+        Node<T> current = head;
+        while (current != null) {
+            if (Objects.equals(current.item, object)) {
+                if (current.prev != null) {
+                    current.prev.next = current.next;
+                } else {
+                    head = current.next;
+                }
+                if (current.next != null) {
+                    current.next.prev = current.prev;
+                } else {
+                    tail = current.prev;
+                }
+                size--;
+                return true;
+            }
+            current = current.next;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void addNode(T value) {
+        Node<T> node = new Node<>(value);
+        node.prev = tail;
+        node.next = null;
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+    private void findIndexGet(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+    }
+
+    private void findIndex(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+    }
+
+    private void addFirst(T value) {
+        if (size == 0) {
+            Node<T> node = new Node<>(value);
+            node.prev = null;
+            node.next = null;
+            head = node;
+            tail = node;
+            size++;
+        } else {
+            Node<T> node = new Node<>(value);
+            node.next = head;
+            node.prev = null;
+            head.prev = node;
+            head = node;
+            size++;
+        }
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+        private Node<E> prev;
+
+        public Node(E element) {
+            this.prev = null;
+            this.item = element;
+            this.next = null;
+        }
     }
 }
