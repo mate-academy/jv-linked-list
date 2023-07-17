9a44f720f4cc90e0b3aa8805c7bae2b17a2bd840
Iryna Mekh <iryna.mekh@gmail.com>
2023-07-17 15:14:36 +0300

    fixed mistakes after mentors review

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -45,13 +45,13 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public T get(int index) {
         checkIndex(index);
-        return getNode(index).value;
+        return getNodeByIndex(index).value;
     }
 
     @Override
     public T set(T value, int index) {
         checkIndex(index);
-        Node<T> current = getNode(index);
+        Node<T> current = getNodeByIndex(index);
         T prevValue = current.value;
         current.value = value;
         return prevValue;
@@ -60,7 +60,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public T remove(int index) {
         checkIndex(index);
-        Node<T> removed = getNode(index);
+        Node<T> removed = getNodeByIndex(index);
         unlink(removed);
         size--;
         return removed.value;
@@ -90,18 +90,6 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return size == 0;
     }
 
-    private class Node<T> {
-        private T value;
-        private Node<T> prev;
-        private Node<T> next;
-
-        public Node(Node<T> prev, T value, Node<T> next) {
-            this.prev = prev;
-            this.value = value;
-            this.next = next;
-        }
-    }
-
     private void checkIndex(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Invalid index: " + index);
@@ -125,19 +113,8 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         }
     }
 
-    private Node<T> getNode(int index) {
-        Node<T> current = head;
-        if (index < size / 2) {
-            for (int i = 0; i < index; i++) {
-                current = current.next;
-            }
-        } else {
-            current = tail;
-            for (int i = size - 1; i > index; i--) {
-                current = current.prev;
-            }
-        }
-        return current;
+    private Node<T> getNodeByIndex(int index) {
+        return (index < size / 2) ? searchFromHead(index) : searchFromTail(index);
     }
 
     private void addInHead(T value) {
@@ -150,12 +127,40 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     private void addInMiddle(T value, int index) {
         Node<T> newCurrent = new Node<>(null, value, null);
-        Node<T> newNext = getNode(index);
+        Node<T> newNext = getNodeByIndex(index);
         Node<T> previous = newNext.prev;
         newCurrent.next = newNext;
         newCurrent.prev = previous;
         newNext.prev = newCurrent;
         previous.next = newCurrent;
         size++;
+    }
+
+    private Node<T> searchFromHead(int index) {
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    private Node<T> searchFromTail(int index) {
+        Node<T> current = tail;
+        for (int i = size - 1; i > index; i--) {
+            current = current.prev;
+        }
+        return current;
+    }
+
+    private class Node<T> {
+        private T value;
+        private Node<T> prev;
+        private Node<T> next;
+
+        public Node(Node<T> prev, T value, Node<T> next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
