515336a41207cb6dd5621f0b6133c61e99c56d93
Artem Hlukhoman <giantking1231@gmail.com>
2023-07-26 18:16:34 +0300

    set method was overwritten

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -49,10 +49,10 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public T set(T value, int index) {
-        T valueRemoved = getElementOnIndex(index).value;
-        remove(index);
-        add(value, index);
-        return valueRemoved;
+        Node<T> node = getElementOnIndex(index);
+        T prevValue = node.value;
+        node.value = value;
+        return prevValue;
     }
 
     @Override
