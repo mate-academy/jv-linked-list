1c7ed5b1c5d0e7594e5e3a998fd26764aab7a852
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-08-12 13:37:05 +0300

    added changes to the class MyLinkedList

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -17,6 +17,30 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             this.data = data;
             this.next = next;
         }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node<T> getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node<T> prev) {
+            this.prev = prev;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
     }
 
     @Override
