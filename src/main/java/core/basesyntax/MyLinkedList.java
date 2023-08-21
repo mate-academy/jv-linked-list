0b2d23406bac823f69a3d2f05bb539a6d1bb71ba
Mykola Horianin <anyavic2379@gmail.com>
2023-08-21 13:53:18 +0300

    Made the necessary changes

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -91,7 +91,8 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     private void checkRangeForIndex(int index) {
         if (index != 0 && (index < 0 || index >= size)) {
-            throw new IndexOutOfBoundsException("The index is not valid");
+            throw new IndexOutOfBoundsException("The index " + index
+                    + " is not valid for size: " + size);
         }
     }
 
