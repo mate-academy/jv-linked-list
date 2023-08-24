8d240e75026113e4da02855e10526ae6b23d0503
Vyacheslav <torbin.official@gmail.com>
2023-08-24 19:01:32 +0300

    Completed implementation of MyLinkedList

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,45 +3,159 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private int size;
+    private Node<T> head;
+    private Node<T> tail;
+
     @Override
     public void add(T value) {
+        if (isEmpty()) {
+            addAsHead(value);
+        } else {
+            addAsTail(value);
+        }
     }
 
     @Override
     public void add(T value, int index) {
+        if (index == 0) {
+            addAsHead(value);
+        } else if (index == size) {
+            addAsTail(value);
+        } else {
+            Node<T> prev = findByIndex(index - 1);
+            Node<T> node = new Node<>(prev, value, prev.next);
+            prev.next.prev = node;
+            prev.next = node;
+            size++;
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T item : list) {
+            add(item);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        Node<T> node = findByIndex(index);
+        return node.value;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        checkIndex(index);
+        Node<T> node = findByIndex(index);
+        T oldValue = node.value;
+        node.value = value;
+        return oldValue;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        T oldValue;
+        if (index == 0) {
+            oldValue = head.value;
+            unlink(head);
+        } else if (index == size - 1) {
+            oldValue = tail.value;
+            unlink(tail);
+        } else {
+            Node<T> node = findByIndex(index);
+            oldValue = node.value;
+            unlink(node);
+        }
+        return oldValue;
     }
 
     @Override
     public boolean remove(T object) {
+        Node<T> node = head;
+        while (node != null) {
+            if (node.value == object || node.value != null && node.value.equals(object)) {
+                unlink(node);
+                return true;
+            }
+            node = node.next;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void addAsHead(T value) {
+        if (isEmpty()) {
+            head = new Node<>(null, value, null);
+            tail = head;
+        } else {
+            Node<T> node = new Node<>(null, value, head);
+            head.prev = node;
+            head = node;
+        }
+        size++;
+    }
+
+    private void addAsTail(T value) {
+        Node<T> node = new Node<>(tail, value, null);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Wrong index : " + index);
+        }
+    }
+
+    private void unlink(Node<T> node) {
+        if (node == head && node == tail) {
+            head = null;
+            tail = null;
+        } else if (node == head) {
+            head = head.next;
+            head.prev = null;
+        } else if (node == tail) {
+            tail = tail.prev;
+            tail.next = null;
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+        size--;
+    }
+
+    private Node<T> findByIndex(int index) {
+        checkIndex(index);
+        Node<T> node = head;
+        while (index-- != 0) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    private static class Node<T> {
+        private Node<T> next;
+        private Node<T> prev;
+        private T value;
+
+        Node(Node<T> prev, T element, Node<T> next) {
+            this.value = element;
+            this.next = next;
+            this.prev = prev;
+        }
+
     }
 }
