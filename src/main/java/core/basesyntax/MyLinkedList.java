b2141ea38494a4b1081d4c6fcc43d3552518f4e9
Volodymyr Hryhoryk <volodymyrhryhoryk@gmail.com>
2023-08-25 13:08:51 +0300

    modified access to Node class

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -7,7 +7,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     private Node<T> tail;
     private int size;
 
-    static class Node<T> {
+    private static class Node<T> {
         private Node<T> prev;
         private T element;
         private Node<T> next;
