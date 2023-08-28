d91ae34e9633c1db3d36cc320ff70e081b0c4962
Epikano <epikano@gmail.com>
2023-08-28 15:50:56 +0300

    Added jv-linked-list solution.

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,45 +3,194 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private int size;
+    private Node<T> first;
+    private Node<T> last;
+
     @Override
     public void add(T value) {
+        linkLast(value);
     }
 
     @Override
     public void add(T value, int index) {
+        checkPositionIndex(index);
+        if (index == size) {
+            linkLast(value);
+        } else {
+            linkBefore(value, node(index));
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
+        Object[] listAsArray = list.toArray();
+        int numNew = listAsArray.length;
+        if (numNew == 0) {
+            return;
+        }
+
+        for (Object object : listAsArray) {
+            T nodeElement = (T) object;
+            Node<T> newNode = new Node<>(last, nodeElement, null);
+            if (last == null) {
+                first = newNode;
+            } else {
+                last.next = newNode;
+            }
+            last = newNode;
+        }
+        size += numNew;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkElementIndex(index);
+        return node(index).item;
     }
 
     @Override
-    public T set(T value, int index) {
-        return null;
+    public T set(T item, int index) {
+        checkElementIndex(index);
+        Node<T> nodeValueByIndex = node(index);
+        T oldItem = nodeValueByIndex.item;
+        nodeValueByIndex.item = item;
+        return oldItem;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkElementIndex(index);
+        return unlink(node(index));
     }
 
     @Override
     public boolean remove(T object) {
+        if (object == null) {
+            for (Node<T> nodeValue = first; nodeValue != null; nodeValue = nodeValue.next) {
+                if (nodeValue.item == null) {
+                    unlink(nodeValue);
+                    return true;
+                }
+            }
+        } else {
+            for (Node<T> nodeValue = first; nodeValue != null; nodeValue = nodeValue.next) {
+                if (object.equals(nodeValue.item)) {
+                    unlink(nodeValue);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void linkLast(T value) {
+        final Node<T> last = this.last;
+        final Node<T> newNode = new Node<>(last, value, null);
+        this.last = newNode;
+        if (last == null) {
+            first = newNode;
+        } else {
+            last.next = newNode;
+        }
+        size++;
+    }
+
+    void linkBefore(T value, Node<T> succ) {
+        final Node<T> pred = succ.prev;
+        final Node<T> newNode = new Node<>(pred, value, succ);
+        succ.prev = newNode;
+        if (pred == null) {
+            first = newNode;
+        } else {
+            pred.next = newNode;
+        }
+        size++;
+    }
+
+    T unlink(Node<T> value) {
+        final T item = value.item;
+        final Node<T> next = value.next;
+        final Node<T> prev = value.prev;
+
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+            value.prev = null;
+        }
+
+        if (next == null) {
+            last = prev;
+        } else {
+            next.prev = prev;
+            value.next = null;
+        }
+
+        value.item = null;
+        size--;
+        return item;
+    }
+
+    private boolean isElementIndex(int index) {
+        return (index >= 0) && (index < size);
+    }
+
+    private boolean isPositionIndex(int index) {
+        return (index >= 0) && (index <= size);
+    }
+
+    private String outOfBoundsMsg(int index) {
+        return "Index: " + index + ", Size: " + size;
+    }
+
+    private void checkElementIndex(int index) {
+        if (!isElementIndex(index)) {
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+        }
+    }
+
+    private void checkPositionIndex(int index) {
+        if (!isPositionIndex(index)) {
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+        }
+    }
+
+    Node<T> node(int index) {
+        if (index < (size >> 1)) {
+            Node<T> nodeElement = first;
+            for (int i = 0; i < index; i++) {
+                nodeElement = nodeElement.next;
+            }
+            return nodeElement;
+        } else {
+            Node<T> nodeElement = last;
+            for (int i = size - 1; i > index; i--) {
+                nodeElement = nodeElement.prev;
+            }
+            return nodeElement;
+        }
+    }
+
+    private static class Node<T> {
+        private T item;
+        private Node<T> next;
+        private Node<T> prev;
+
+        Node(Node<T> prev, T element, Node<T> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
     }
 }
