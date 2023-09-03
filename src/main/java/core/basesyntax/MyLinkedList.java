dccab716c8c12ea5f0c08f693ba00e0d5b9c762d
Denis Gorda <dnsgorda@gmail.com>
2023-09-03 20:03:25 +0300

    move private after public

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -7,57 +7,6 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     private Node<T> last;
     private int size;
 
-    private static class Node<T> {
-        private T value;
-        private Node<T> prev;
-        private Node<T> next;
-
-        Node(T value, Node<T> prev, Node<T> next) {
-            this.value = value;
-            this.prev = prev;
-            this.next = next;
-        }
-    }
-
-    private boolean checkIndex(int index) {
-        if (index < 0 || index >= size) {
-            return false;
-        }
-        return true;
-    }
-
-    private Node<T> getNode(int index) {
-        if (index < size / 2) {
-            Node<T> current = first;
-            for (int i = 0; i < index; i++) {
-                current = current.next;
-            }
-            return current;
-        } else {
-            Node<T> current = last;
-            for (int i = size - 1; i > index; i--) {
-                current = current.prev;
-            }
-            return current;
-        }
-    }
-
-    private void unlink(Node node) {
-        if (last == first) {
-            last = first = null;
-        } else if (node == first) {
-            node.next.prev = null;
-            first = node.next;
-        } else if (node == last) {
-            node.prev.next = null;
-            last = node.prev;
-        } else {
-            node.next.prev = node.prev;
-            node.prev.next = node.next;
-        }
-        size--;
-    }
-
     @Override
     public void add(T value) {
         Node<T> newNode = new Node<>(value, null, null);
@@ -153,5 +102,56 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    private boolean checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            return false;
+        }
+        return true;
+    }
+
+    private Node<T> getNode(int index) {
+        if (index < size / 2) {
+            Node<T> current = first;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+            return current;
+        } else {
+            Node<T> current = last;
+            for (int i = size - 1; i > index; i--) {
+                current = current.prev;
+            }
+            return current;
+        }
+    }
+
+    private void unlink(Node node) {
+        if (last == first) {
+            last = first = null;
+        } else if (node == first) {
+            node.next.prev = null;
+            first = node.next;
+        } else if (node == last) {
+            node.prev.next = null;
+            last = node.prev;
+        } else {
+            node.next.prev = node.prev;
+            node.prev.next = node.next;
+        }
+        size--;
+    }
+
+    private static class Node<T> {
+        private T value;
+        private Node<T> prev;
+        private Node<T> next;
+
+        Node(T value, Node<T> prev, Node<T> next) {
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
+        }
     }
 }
