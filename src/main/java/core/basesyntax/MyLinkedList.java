cd5c9207efd682ee077452f72aff86e2fd1c0d52
Lukian Muzyka <lukianmuzyka@gmail.com>
2023-09-19 11:09:53 +0300

    changed for loop

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -93,8 +93,8 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public void addAll(List<T> list) {
-        for (int i = 0; i < list.size(); i++) {
-            add(list.get(i));
+        for (T t : list) {
+            add(t);
         }
     }
 
