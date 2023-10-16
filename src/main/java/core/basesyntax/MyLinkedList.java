89a6a5a44f56fba0e23295e606df75f528e3994a
Anton Dudar <dudar.anton4uk@gmail.com>
2023-10-16 13:48:30 +0300

    added iteration from both tail&head, renamed first > head; last > tail; moved Node class to the end of the file

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -4,32 +4,20 @@ import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     private static final String INVALID_INDEX = "The provided index is invalid: ";
-    private Node first;
-    private Node last;
+    private Node head;
+    private Node tail;
     private int size;
-
-    private static class Node<E> {
-        private E item;
-        private Node<E> next;
-        private Node<E> prev;
-
-        Node(Node<E> prev, E element, Node<E> next) {
-            this.item = element;
-            this.next = next;
-            this.prev = prev;
-        }
-    }
 
     @Override
     public void add(T value) {
         Node<T> newNode = new Node<>(null, value, null);
         if (size == 0) {
-            first = newNode;
+            head = newNode;
         } else {
-            last.next = newNode;
-            newNode.prev = last;
+            tail.next = newNode;
+            newNode.prev = tail;
         }
-        last = newNode;
+        tail = newNode;
         size++;
     }
 
@@ -40,9 +28,9 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         if (size == 0 || index == size) {
             add(value);
         } else if (index == 0) {
-            newNode.next = first;
-            first.prev = newNode;
-            first = newNode;
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
             size++;
         } else {
             Node<T> preAddingNode = getNodeByIndex(index - 1);
@@ -114,9 +102,17 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     private Node<T> getNodeByIndex(int index) {
         validateIndexForGetAndRemove(index);
-        Node<T> current = first;
-        for (int i = 0; i < index; i++) {
-            current = current.next;
+        Node<T> current;
+        if (size / 2 > index) {
+            current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+        } else {
+            current = tail;
+            for (int i = size; i > index + 1; i--) {
+                current = current.prev;
+            }
         }
         return current;
     }
@@ -126,13 +122,13 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         Node<T> next = x.next;
         Node<T> prev = x.prev;
         if (prev == null) {
-            first = next;
+            head = next;
         } else {
             prev.next = next;
             x.prev = null;
         }
         if (next == null) {
-            last = prev;
+            tail = prev;
         } else {
             next.prev = prev;
             x.next = null;
@@ -143,7 +139,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     }
 
     private int getIndexByObject(T object) {
-        Node<T> current = first;
+        Node<T> current = head;
         int index = 0;
         while (current != null) {
             if ((current.item == object) || (current.item != null && current.item.equals(object))) {
@@ -153,5 +149,17 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             index++;
         }
         return -1;
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+        private Node<E> prev;
+
+        private Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
     }
 }
