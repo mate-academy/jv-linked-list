b161643a34ae6848848df671626bb2c19ca7722e
Dmytro Potapov <potapovds1993@gmail.com>
2023-10-16 21:19:29 +0300

    created class Node

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,6 +3,28 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node head;
+    private Node tail;
+    private int size = 0;
+
+    public MyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    private class Node {
+        Node prev;
+        T value;
+        Node next;
+
+        public Node(Node prev, T value, Node next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+        }
+
+    }
     @Override
     public void add(T value) {
     }
