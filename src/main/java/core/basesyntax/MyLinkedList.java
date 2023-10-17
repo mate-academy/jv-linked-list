0e9b0940296d51a1dd4b177f2ff0ca8452517e11
Dmytro Potapov <potapovds1993@gmail.com>
2023-10-17 18:30:36 +0300

    hw-solution-jv-linked-list

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private static final String BOUNDS_EXCEPTION = "This index is not correct: ";
     private Node head;
     private Node tail;
     private int size = 0;
@@ -14,9 +15,9 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     }
 
     private class Node {
-        Node prev;
-        T value;
-        Node next;
+        private Node prev;
+        private T value;
+        private Node next;
 
         public Node(Node prev, T value, Node next) {
             this.prev = prev;
@@ -25,45 +26,138 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         }
 
     }
+
     @Override
     public void add(T value) {
+        Node addNode = new Node(tail, value, null);
+        tail = (tail == null) ? (head = addNode) : (tail.next = addNode);
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException(BOUNDS_EXCEPTION + index);
+        }
+        if (index == size) {
+            add(value);
+            return;
+        }
+        Node currentNode = getNodeByIndex(index);
+        Node addNode = new Node(currentNode.prev, value, currentNode);
+        if (currentNode.prev == null) {
+            head = addNode;
+        } else {
+            currentNode.prev.next = addNode;
+        }
+        currentNode.prev = addNode;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T value : list) {
+            add(value);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkException(index);
+        Node currentNode = getNodeByIndex(index);
+        return currentNode.value;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        checkException(index);
+        Node current = getNodeByIndex(index);
+        T oldValue = current.value;
+        current.value = value;
+        return oldValue;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkException(index);
+        Node removeNode = getNodeByIndex(index);
+        unlink(removeNode);
+        return removeNode.value;
     }
 
     @Override
     public boolean remove(T object) {
+        if (object == null) {
+            return removeNull();
+        }
+        Node currentNode = head;
+        while (currentNode != null) {
+            if (object.equals(currentNode.value)) {
+                unlink(currentNode);
+                return true;
+            }
+            currentNode = currentNode.next;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void unlink(Node node) {
+        Node prevNode = node.prev;
+        Node nextNode = node.next;
+        head = (prevNode == null) ? nextNode : head;
+        tail = (nextNode == null) ? prevNode : tail;
+        if (prevNode != null) {
+            prevNode.next = nextNode;
+            node.prev = null;
+        }
+        if (nextNode != null) {
+            nextNode.prev = prevNode;
+            node.next = null;
+        }
+        size--;
+    }
+
+    private Node getNodeByIndex(int index) {
+        Node currentNode;
+        if (index < (size / 2)) {
+            currentNode = head;
+            for (int i = 0; i < index; i++) {
+                currentNode = currentNode.next;
+            }
+        } else {
+            currentNode = tail;
+            for (int i = size - 1; i > index; i--) {
+                currentNode = currentNode.prev;
+            }
+        }
+        return currentNode;
+    }
+
+    private boolean removeNull() {
+        Node currentNode = head;
+        while (currentNode != null) {
+            if (currentNode.value == null) {
+                unlink(currentNode);
+                return true;
+            }
+            currentNode = currentNode.next;
+        }
         return false;
+    }
+
+    private void checkException(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(BOUNDS_EXCEPTION + index);
+        }
     }
 }
