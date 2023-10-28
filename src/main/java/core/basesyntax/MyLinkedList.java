2ee37f0a23a05d8a8985eed480242e30d8d713c6
danX <danildryzov@gmail.com>
2023-10-28 13:56:38 +0200

    added tail and simplified code

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     private static final String iOutOfBounds = "Index is out of bounds";
     private Node<T> head;
+    private Node<T> tail;
     private int size;
 
     public MyLinkedList() {
@@ -22,11 +23,14 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         }
 
         if (index == 0) {
-            Node<T> newNode = new Node<>(null, value, head);
-            if (head != null) {
-                head.prev = newNode;
+            head = new Node<>(null, value, head);
+            if (size == 0) {
+                tail = head;
             }
-            head = newNode;
+        } else if (index == size) {
+            Node<T> newNode = new Node<>(tail, value, null);
+            tail.next = newNode;
+            tail = newNode;
         } else {
             Node<T> previous = getNode(index - 1);
             Node<T> newNode = new Node<>(previous, value, previous.next);
@@ -47,72 +51,24 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public T get(int index) {
-        Node<T> node = getNode(index);
-        return node.value;
-    }
-
-    @Override
-    public T set(T value, int index) {
-        Node<T> node = getNode(index);
-        T oldValue = node.value;
-        node.value = value;
-        return oldValue;
-    }
-
-    @Override
-    public T remove(int index) {
-        Node<T> removedNode = getNode(index);
-        if (index == 0) {
-            head = removedNode.next;
-        } else {
-            removedNode.prev.next = removedNode.next;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(iOutOfBounds);
         }
 
-        if (removedNode.next != null) {
-            removedNode.next.prev = removedNode.prev;
-        }
+        Node<T> current;
 
-        size--;
-        return removedNode.value;
-    }
-
-    @Override
-    public boolean remove(T object) {
-        Node<T> current = head;
-        while (current != null) {
-            if (object == null) {
-                if (current.value == null) {
-                    return removeObject(current);
-                }
-            } else if (object.equals(current.value)) {
-                return removeObject(current);
+        if (index < size / 2) {
+            current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
             }
-            current = current.next;
-        }
-        return false;
-    }
-
-    private boolean removeObject(Node<T> current) {
-        if (current.prev == null) {
-            head = current.next;
         } else {
-            current.prev.next = current.next;
+            current = tail;
+            for (int i = size - 1; i > index; i--) {
+                current = current.prev;
+            }
         }
-        if (current.next != null) {
-            current.next.prev = current.prev;
-        }
-        size--;
-        return true;
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
+        return current.value;
     }
 
     private Node<T> getNode(int index) {
@@ -125,6 +81,67 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             current = current.next;
         }
         return current;
+    }
+
+    @Override
+    public T set(T value, int index) {
+        Node<T> node = getNode(index);
+        T oldValue = node.value;
+        node.value = value;
+        return oldValue;
+    }
+
+    @Override
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(iOutOfBounds);
+        }
+
+        Node<T> removedNode = getNode(index);
+        updateReferences(removedNode);
+        return removedNode.value;
+    }
+
+    @Override
+    public boolean remove(T object) {
+        Node<T> current = head;
+        while (current != null) {
+            if (object == null) {
+                if (current.value == null) {
+                    updateReferences(current);
+                    return true;
+                }
+            } else if (object.equals(current.value)) {
+                updateReferences(current);
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    private void updateReferences(Node<T> node) {
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        } else {
+            head = node.next;
+        }
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        } else {
+            tail = node.prev;
+        }
+        size--;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
     }
 
     static class Node<T> {
