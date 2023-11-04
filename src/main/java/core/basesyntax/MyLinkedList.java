ea5c75b8739135bb735672e2d2f8c459d73dad39
Dmytro Sokolovskyi <dmytro.sokolovskyi@icloud.com>
2023-11-04 09:09:34 -0400

    added changes after review

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -7,18 +7,6 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     private Node<T> last;
     private int size;
 
-    private static class Node<T> {
-        private T item;
-        private Node<T> next;
-        private Node<T> prev;
-
-        private Node(Node<T> prev, T element, Node<T> next) {
-            this.item = element;
-            this.next = next;
-            this.prev = prev;
-        }
-    }
-
     private void linkFirst(T element) {
         Node<T> prevFirst = first;
         Node<T> newNode = new Node<>(null, element, prevFirst);
@@ -29,98 +17,6 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             prevFirst.prev = newNode;
         }
         size++;
-    }
-
-    private void linkBefore(T element, Node<T> exist) {
-        Node<T> prevExist = exist.prev;
-        Node<T> newNode = new Node<>(prevExist, element, exist);
-        exist.prev = newNode;
-        if (prevExist == null) {
-            first = newNode;
-        } else {
-            prevExist.next = newNode;
-        }
-        size++;
-    }
-
-    private void linkLast(T element) {
-        Node<T> prevLast = last;
-        Node<T> newNode = new Node<>(prevLast, element, null);
-        last = newNode;
-        if (prevLast == null) {
-            first = newNode;
-        } else {
-            prevLast.next = newNode;
-        }
-        size++;
-    }
-
-    private Node<T> node(int index) {
-        checkIndex(index);
-        if (index < (size >> 1)) {
-            Node<T> current = first;
-            for (int i = 0; i < index; i++) {
-                current = current.next;
-            }
-            return current;
-        } else {
-            Node<T> current = last;
-            for (int i = size - 1; i > index; i--) {
-                current = current.prev;
-            }
-            return current;
-        }
-    }
-
-    private void deleteFirst() {
-        Node<T> oldFirst = first;
-        Node<T> newFirst = first.next;
-        oldFirst.item = null;
-        oldFirst.next = null;
-        first = newFirst;
-        if (newFirst == null) {
-            last = null;
-        } else {
-            newFirst.prev = null;
-        }
-        size--;
-    }
-
-    private void deleteLast() {
-        Node<T> oldLast = last;
-        Node<T> newLast = last.prev;
-        oldLast.item = null;
-        oldLast.prev = null;
-        last = newLast;
-        if (newLast == null) {
-            first = null;
-        } else {
-            newLast.next = null;
-        }
-        size--;
-    }
-
-    private void deleteNode(Node<T> node) {
-        Node<T> next = node.next;
-        Node<T> prev = node.prev;
-        next.prev = prev;
-        prev.next = next;
-        node.item = null;
-        node.next = null;
-        node.prev = null;
-        size--;
-    }
-
-    private void checkPositionIndex(int index) {
-        if (index < 0 || index > size) {
-            throw new IndexOutOfBoundsException("incorrect position index: " + index);
-        }
-    }
-
-    private void checkIndex(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("invalid index: " + index);
-        }
     }
 
     @Override
@@ -233,5 +129,109 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public boolean isEmpty() {
         return size() == 0;
+    }
+
+    private void linkBefore(T element, Node<T> exist) {
+        Node<T> prevExist = exist.prev;
+        Node<T> newNode = new Node<>(prevExist, element, exist);
+        exist.prev = newNode;
+        if (prevExist == null) {
+            first = newNode;
+        } else {
+            prevExist.next = newNode;
+        }
+        size++;
+    }
+
+    private void linkLast(T element) {
+        Node<T> prevLast = last;
+        Node<T> newNode = new Node<>(prevLast, element, null);
+        last = newNode;
+        if (prevLast == null) {
+            first = newNode;
+        } else {
+            prevLast.next = newNode;
+        }
+        size++;
+    }
+
+    private Node<T> node(int index) {
+        checkIndex(index);
+        if (index < (size >> 1)) {
+            Node<T> current = first;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+            return current;
+        } else {
+            Node<T> current = last;
+            for (int i = size - 1; i > index; i--) {
+                current = current.prev;
+            }
+            return current;
+        }
+    }
+
+    private void deleteFirst() {
+        Node<T> oldFirst = first;
+        Node<T> newFirst = first.next;
+        oldFirst.item = null;
+        oldFirst.next = null;
+        first = newFirst;
+        if (newFirst == null) {
+            last = null;
+        } else {
+            newFirst.prev = null;
+        }
+        size--;
+    }
+
+    private void deleteLast() {
+        Node<T> oldLast = last;
+        Node<T> newLast = last.prev;
+        oldLast.item = null;
+        oldLast.prev = null;
+        last = newLast;
+        if (newLast == null) {
+            first = null;
+        } else {
+            newLast.next = null;
+        }
+        size--;
+    }
+
+    private void deleteNode(Node<T> node) {
+        Node<T> next = node.next;
+        Node<T> prev = node.prev;
+        next.prev = prev;
+        prev.next = next;
+        node.item = null;
+        node.next = null;
+        node.prev = null;
+        size--;
+    }
+
+    private void checkPositionIndex(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("incorrect position index: " + index);
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("invalid index: " + index);
+        }
+    }
+
+    private static class Node<T> {
+        private T item;
+        private Node<T> next;
+        private Node<T> prev;
+
+        private Node(Node<T> prev, T element, Node<T> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
     }
 }
