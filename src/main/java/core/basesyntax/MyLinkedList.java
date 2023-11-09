67d78aa394485409750a46b83d55638a7b1346f8
gulini32 <gulini32@gmail.com>
2023-11-09 14:27:14 +0200

    LinkList

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -13,58 +13,56 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         if (index == size) {
             add(value);
         } else {
-            indexExist(index);
             Node<T> newNode = new Node<>(value);
-            if (index == 0) {
-                newNode.next = head;
+            Node<T> currentNode = getNodeByIndex(index);
+            if (currentNode == head) {
                 head = newNode;
             } else {
-                Node<T> prevNode = getNodeByIndex(index - 1);
-                newNode.next = prevNode.next;
-                prevNode.next = newNode;
+                currentNode.prev.next = newNode;
+                newNode.prev = currentNode.prev;
             }
+            newNode.next = currentNode;
+            currentNode.prev = newNode;
             size++;
         }
     }
 
     @Override
     public void add(T value) {
-        if (size == 0) {
-            head = new Node<>(value);
-            tail = head;
+        Node<T> newNode = new Node<>(value);
+        if (head == null) {
+            head = newNode;
         } else {
-            Node<T> newNode = new Node<>(tail, value);
             tail.next = newNode;
-            tail = newNode;
+            newNode.prev = tail;
         }
+        tail = newNode;
         size++;
     }
 
     @Override
     public T get(int index) {
-        Node<T> currentNode = getNodeByIndex(index);
-        return currentNode.value;
+        return getNodeByIndex(index).value;
     }
 
     @Override
     public void addAll(List<T> list) {
-        for (T t : list) {
-            add(t);
+        for (T element : list) {
+            add(element);
         }
     }
 
     @Override
     public T set(T value, int index) {
-        Node<T> currentNode = nodeGetByIndex(index);
-        T old = currentNode.value;
+        Node<T> currentNode = getNodeByIndex(index);
+        T oldValue = currentNode.value;
         currentNode.value = value;
-        return old;
+        return oldValue;
     }
 
     @Override
     public T remove(int index) {
-
-        Node<T> currentNode = nodeGetByIndex(index);
+        Node<T> currentNode = getNodeByIndex(index);
         unlink(currentNode);
         return currentNode.value;
     }
@@ -84,13 +82,12 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public int size() {
-
         return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return size == 0;
+        return head == null && tail == null;
     }
 
     private void indexExist(int index) {
@@ -108,27 +105,15 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return currentNode;
     }
 
-    private Node<T> nodeGetByIndex(int index) {
-        indexExist(index);
-        Node<T> currentNode = head;
-        for (int i = 0; i < size; i++) {
-            if (i == index) {
-                break;
-            }
-            currentNode = currentNode.next;
-        }
-        return currentNode;
-    }
-
     private void unlink(Node<T> currentNode) {
-        if (currentNode.prev == null) {
+        if (currentNode == head) {
             head = currentNode.next;
-        }
-        if (currentNode.next == null) {
-            tail = currentNode.prev;
-        }
-        if (currentNode.prev != null && currentNode.next != null) {
+        } else {
             currentNode.prev.next = currentNode.next;
+        }
+        if (currentNode == tail) {
+            tail = currentNode.prev;
+        } else {
             currentNode.next.prev = currentNode.prev;
         }
         size--;
@@ -139,12 +124,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         private Node<T> next;
         private Node<T> prev;
 
-        public Node(T value) {
-            this.value = value;
-        }
-
-        public Node(Node<T> prev, T value) {
-            this.prev = prev;
+        private Node(T value) {
             this.value = value;
         }
     }
