4297eceef52fc51bceece4fa14ad9f4b11c36d28
Bohdan <bohdan.g.54@gmail.com>
2023-11-12 17:28:53 +0200

    backupping

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -1,111 +1,11 @@
 package core.basesyntax;
 
 import java.util.List;
-import java.util.Objects;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
-    private Node head;
-    private Node tail;
+    private Node<T> head;
+    private Node<T> tail;
     private int size;
-
-    private void validateIndexExclusive(int index) {
-        if (index > size || index < 0) {
-            throw new IndexOutOfBoundsException("Wrong index provided");
-        }
-    }
-
-    private void validateIndex(int index) {
-        if (index >= size || index < 0) {
-            throw new IndexOutOfBoundsException("Wrong index provided");
-        }
-    }
-
-    private Node getNode(int index) {
-        validateIndex(index);
-        if (index == 0) {
-            return head;
-        } else if (index == size - 1) {
-            return tail;
-        }
-        Node currentNode;
-        int steps = stepsToTake(index);
-        currentNode = steps > 0 ? head : tail;
-
-        // Should I reverse if and for order ?
-        // It will add to readability, but it will execute a bit longer
-        if (steps > 0) {
-            for (int i = 0; i < steps; i++) {
-                currentNode = currentNode.next;
-            }
-        } else {
-            // Should change steps sign to plus
-            for (int i = 0; i < -steps; i++) {
-                currentNode = currentNode.prev;
-            }
-        }
-        return currentNode;
-    }
-
-    private boolean equals(T a, T b) {
-        return Objects.equals(a, b);
-    }
-
-    private int getIndexOfNode(T value) {
-        for (int i = 0; i < size; i++) {
-            if (equals(value, getNode(i).data)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    private int stepsToTake(int index) {
-        if (index <= size / 2) {
-            return index;
-        } else {
-            return -(size - 1 - index);
-        }
-    }
-
-    private void insertBefore(Node nodeToInsertAfter, Node newNode) {
-        newNode.prev = nodeToInsertAfter.prev;
-        newNode.next = nodeToInsertAfter;
-        nodeToInsertAfter.prev.next = newNode;
-        nodeToInsertAfter.prev = newNode;
-    }
-
-    public void printLinkedList() {
-        Node currentNode = head;
-
-        while (currentNode != null) {
-            System.out.println(currentNode.data);
-            currentNode = currentNode.next;
-        }
-    }
-
-    public void printReversedLinkedList() {
-        Node currentNode = tail;
-
-        while (currentNode != null) {
-            System.out.println(currentNode.data);
-            currentNode = currentNode.prev;
-        }
-    }
-
-    private Node unlink(Node node) {
-        size--;
-        if (node == tail) {
-            tail = node.prev;
-            return node;
-        }
-        if (node == head) {
-            head = node.next;
-            return node;
-        }
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-        return node;
-    }
 
     @Override
     public void add(T value) {
@@ -115,7 +15,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public void add(T value, int index) {
         validateIndexExclusive(index);
-        Node newNode = new Node(value);
+        Node<T> newNode = new Node<>(value, null, null);
         if (isEmpty()) {
             head = newNode;
             tail = newNode;
@@ -128,14 +28,10 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
             tail.next = newNode;
             tail = newNode;
         } else {
-            Node currentNode = getNode(index);
+            Node<T> currentNode = getNode(index);
             insertBefore(currentNode, newNode);
         }
         size++;
-        System.out.println("------------");
-        printLinkedList();
-        System.out.println("------------");
-        printReversedLinkedList();
     }
 
     @Override
@@ -152,7 +48,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public T set(T value, int index) {
-        Node nodeToUpdate = getNode(index);
+        Node<T> nodeToUpdate = getNode(index);
         T oldValue = nodeToUpdate.data;
         nodeToUpdate.data = value;
         return oldValue;
@@ -160,7 +56,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public T remove(int index) {
-        Node nodeToRemove = getNode(index);
+        Node<T> nodeToRemove = getNode(index);
         return unlink(nodeToRemove).data;
     }
 
@@ -184,13 +80,115 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return size == 0;
     }
 
-    class Node {
-        private T data;
-        private Node next;
-        private Node prev;
+    private void validateIndexExclusive(int index) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Wrong index provided");
+        }
+    }
 
-        private Node(T data) {
+    private void validateIndex(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Wrong index provided");
+        }
+    }
+
+    private Node<T> getNode(int index) {
+        validateIndex(index);
+        if (index == 0) {
+            return head;
+        } else if (index == size - 1) {
+            return tail;
+        }
+        Node<T> currentNode;
+        int steps = stepsToTake(index);
+        currentNode = steps > 0 ? head : tail;
+
+        // Should I reverse if and for order ?
+        // It will add to readability, but it will execute a bit longer
+        if (steps > 0) {
+            for (int i = 0; i < steps; i++) {
+                currentNode = currentNode.next;
+            }
+        } else {
+            // Should change steps sign to plus
+            for (int i = 0; i < -steps; i++) {
+                currentNode = currentNode.prev;
+            }
+        }
+        return currentNode;
+    }
+
+    private boolean equals(T a, T b) {
+        return (a == b) || (a != null && a.equals(b));
+    }
+
+    private int getIndexOfNode(T value) {
+        for (int i = 0; i < size; i++) {
+            if (equals(value, getNode(i).data)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Using true to start from head, false to start from tail
+    private int sideOfListToStart(int index) {
+        if (index <= size / 2) {
+            return index;
+        } else {
+            return -(size - 1 - index);
+        }
+    }
+
+    private void insertBefore(Node<T> nodeToInsertAfter, Node<T> newNode) {
+        newNode.prev = nodeToInsertAfter.prev;
+        newNode.next = nodeToInsertAfter;
+        nodeToInsertAfter.prev.next = newNode;
+        nodeToInsertAfter.prev = newNode;
+    }
+
+    private void printLinkedList() {
+        Node<T> currentNode = head;
+
+        while (currentNode != null) {
+            System.out.println(currentNode.data);
+            currentNode = currentNode.next;
+        }
+    }
+
+    private void printReversedLinkedList() {
+        Node<T> currentNode = tail;
+
+        while (currentNode != null) {
+            System.out.println(currentNode.data);
+            currentNode = currentNode.prev;
+        }
+    }
+
+    private Node<T> unlink(Node<T> node) {
+        size--;
+        if (node == tail) {
+            tail = node.prev;
+            return node;
+        }
+        if (node == head) {
+            head = node.next;
+            return node;
+        }
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        return node;
+    }
+
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
+        private Node<E> prev;
+
+        private Node(E data, Node<E> next, Node<E> prev) {
             this.data = data;
+            this.next = next;
+            this.prev = prev;
         }
     }
 }
