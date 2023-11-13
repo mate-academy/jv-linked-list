97870957e4301fd134c09e927113e97a14e1c3ef
Bohdan Chupika <boroda4436@users.noreply.github.com>
2023-11-13 13:06:39 +0200

    remove encoding config from checkstyle plugin

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,45 +3,184 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    public Node first;
+    int size;
+
+    private class Node<T> {
+        T element;
+        Node<T> next;
+
+        public Node(T element) {
+            this.element = element;
+        }
+
+        public Node(T element, Node next) {
+            this.element = element;
+            this.next = next;
+        }
+    }
+
     @Override
     public void add(T value) {
+        if (first == null) {
+            first = new Node(value);
+            size = 1;
+            return;
+        }
+        Node current = first;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = new Node(value);
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        checkIndex(index);
+        if (index == 0) {
+            addFirst(value);
+            return;
+        }
+        Node current = first;
+        int counter = 0;
+        while (counter < index - 1) {
+            if (current.next == null) {
+                throw new IndexOutOfBoundsException();
+            }
+            current = current.next;
+            ++counter;
+        }
+        current.next = new Node(value, current.next);
+        size++;
+    }
+
+    public void addFirst(T value) {
+        if (first == null) {
+            first = new Node(value);
+            size++;
+            return;
+        }
+        first = new Node(value, first);
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
+        Node current = first;
+        while (current.next != null) {
+            current = current.next;
+        }
+        for (int i = 0; i < list.size() - 1; ++i) {
+            current.next = new Node(list.get(i));
+            current = current.next;
+            size++;
+        }
+        size++;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIndex(index);
+        Node<T> current = first;
+        int i = 0;
+        do {
+            if (i == index) {
+                return (T) current.element;
+            }
+            current = current.next;
+            i++;
+        } while (index < size);
+        throw new IndexOutOfBoundsException();
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        checkIndex(index);
+        Node current = first;
+        int counter = 0;
+        while (counter < index) {
+            if (current.next == null) {
+                throw new IndexOutOfBoundsException();
+            }
+            current = current.next;
+            ++counter;
+        }
+        T old = (T) current.element;
+        current.element = value;
+        return old;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkIndex(index);
+        if (index == 0 && first != null) {
+            T value = (T) first.element;
+            first = first.next;
+            size--;
+            return value;
+        }
+
+        Node<T> previous = first;
+        int counter = 0;
+        while (counter < index - 1) {
+            previous = previous.next;
+            if (previous == null || previous.next == null) {
+                throw new IndexOutOfBoundsException();
+            }
+            counter++;
+        }
+
+        T value = (T) previous.next.element;
+        previous.next = previous.next.next;
+        size--;
+        return value;
     }
 
     @Override
     public boolean remove(T object) {
+        if (first == null) {
+            return false;
+        }
+
+        if (areEqual(first.element, object)) {
+            first = first.next;
+            size--;
+            return true;
+        }
+
+        Node<T> previous = first;
+        while (previous.next != null) {
+            if (areEqual(previous.next.element, object)) {
+                previous.next = previous.next.next;
+                size--;
+                return true;
+            }
+            previous = previous.next;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return first == null;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index > size + 1) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private boolean areEqual(Object object1, Object object2) {
+        if (object1 == null || object2 == null) {
+            return object1 == object2;
+        }
+        return object1.equals(object2);
     }
 }
