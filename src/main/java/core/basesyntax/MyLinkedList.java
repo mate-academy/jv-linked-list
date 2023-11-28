f6f6a3aefc400e00eb8a26333a3ade56d20b9ad7
Nonzeq <bgdn.kobylchak@gmail.com>
2023-11-28 22:18:34 +0200

    refactored indexChed method, moved indexCheck method to the findNode method and rename local variable in linkToTail method

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -7,47 +7,18 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     private Node<T> tail;
     private int size;
 
-    private static class Node<T> {
-        private Node<T> prev;
-        private T value;
-        private Node<T> next;
-
-        public Node(T value) {
-            this.value = value;
-        }
-
-        public Node(Node<T> prev, T value, Node<T> next) {
-            this.prev = prev;
-            this.value = value;
-            this.next = next;
-        }
-    }
-
-    public T getHeadValue() {
-        return head.value;
-    }
-
-    public T getTailValue() {
-        return tail.value;
-    }
-
     @Override
     public void add(T value) {
-        insertNodeToTail(value);
+        linkNodeToTail(value);
     }
 
     @Override
     public void add(T value, int index) {
-        validateIndex(index, 1);
         if (index == size) {
-            insertNodeToTail(value);
-            return;
+            linkNodeToTail(value);
+        } else {
+            linkNode(findNode(index), value);
         }
-        if (index == 0) {
-            insertNodeToHead(value);
-            return;
-        }
-        insertNode(findNode(index), value);
     }
 
     @Override
@@ -59,42 +30,21 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public T get(int index) {
-        validateIndex(index, 0);
-        if (index == 0) {
-            return getHeadValue();
-        }
-        if (index == size - 1) {
-            return getTailValue();
-        }
         return findNode(index).value;
     }
 
     @Override
     public T set(T value, int index) {
-        validateIndex(index, 0);
-        final T val;
-        if (index == 0) {
-            val = head.value;
-            head.value = value;
-            return val;
-        }
-        if (index == size - 1) {
-            val = tail.value;
-            tail.value = value;
-            return val;
-        }
+        final T deletedValue;
         Node<T> node = findNode(index);
-        val = node.value;
+        deletedValue = node.value;
         node.value = value;
-        return val;
+        return deletedValue;
     }
 
     @Override
     public T remove(int index) {
-        validateIndex(index, 0);
-        final T val;
-        val = removeNode(findNode(index));
-        return val;
+        return unLink(findNode(index));
     }
 
     @Override
@@ -112,30 +62,55 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return size == 0;
     }
 
+    private static class Node<T> {
+        private Node<T> prev;
+        private T value;
+        private Node<T> next;
+
+        public Node(T value) {
+            this.value = value;
+        }
+
+        public Node(Node<T> prev, T value, Node<T> next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
     private Node<T> findNode(int index) {
+        checkValidPositionForIndex(index);
         return index > size >> 1 ? iterFromTail(index) : iterFromHead(index);
     }
 
-    private void insertNodeToTail(T value) {
-        Node<T> t = tail;
-        Node<T> newNode = new Node<>(tail, value, null);
+    private void linkNodeToTail(T value) {
+        Node<T> previousTail = tail;
+        Node<T> newNode = new Node<>(previousTail, value, null);
         tail = newNode;
-        if (t == null) {
+        if (previousTail == null) {
             head = newNode;
         } else {
-            t.next = newNode;
+            previousTail.next = newNode;
         }
         size++;
     }
 
-    private void insertNodeToHead(T value) {
-        head.prev = new Node<>(value);
-        head.prev.next = head;
-        head = head.prev;
+    private void linkNode(Node<T> node, T value) {
+        Node<T> newNode = new Node<>(value);
+        if (node.prev == null) {
+            head.prev = newNode;
+            newNode.next = head;
+            head = newNode;
+        } else {
+            node.prev.next = newNode;
+            node.prev = newNode;
+            newNode.prev = node.prev;
+            newNode.next = node;
+        }
         size++;
     }
 
-    private T removeNode(Node<T> node) {
+    private T unLink(Node<T> node) {
         final T value = node.value;
         if (node.prev != null) {
             node.prev.next = node.next;
@@ -155,19 +130,12 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         Node<T> node = head;
         for (int i = 0; i < size; i++) {
             if (obj == node.value || (node.value != null && node.value.equals(obj))) {
-                removeNode(node);
+                unLink(node);
                 return true;
             }
             node = node.next;
         }
         return false;
-    }
-
-    private void insertNode(Node<T> node, T value) {
-        node = node.prev;
-        node.next = new Node<>(node, value, node.next);
-        node.next.next.prev = node.next;
-        size++;
     }
 
     private Node<T> iterFromHead(int index) {
@@ -190,8 +158,8 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return node;
     }
 
-    private void validateIndex(int index, int shift) {
-        if (index < 0 || index >= size + shift) {
+    private void checkValidPositionForIndex(int index) {
+        if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("index: " + index + " is invalid index");
         }
     }
