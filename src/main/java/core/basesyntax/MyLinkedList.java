b2d732e4ef51a38a9e5e6472b04946517c55d13b
voody <tarasyashchuk089@gmail.com>
2024-01-25 14:51:16 +0200

    Applied suggested changes

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -7,17 +7,12 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     private Node<T> tail;
     private int size;
 
-    public MyLinkedList() {
-        this.head = null;
-        this.tail = null;
-    }
-
     private static class Node<T> {
         private T item;
         private Node<T> next;
         private Node<T> prev;
 
-        public Node(Node<T> next, T item, Node<T> prev) {
+        public Node(Node<T> prev, T item, Node<T> next) {
             this.item = item;
             this.next = next;
             this.prev = prev;
@@ -26,7 +21,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public void add(T value) {
-        Node<T> newNode = new Node<>(null, value, tail);
+        Node<T> newNode = new Node<>(tail, value, null);
         if (tail == null) {
             head = newNode;
         } else {
@@ -50,10 +45,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
                 tail = newNode;
             }
         } else {
-            Node<T> current = head;
-            for (int i = 0; i < index - 1; ++i) {
-                current = current.next;
-            }
+            Node<T> current = getNodeByIndex(index - 1);
             newNode.next = current.next;
             newNode.prev = current;
             if (current.next != null) {
@@ -68,17 +60,8 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         size++;
     }
 
-    private void checkIndexForAdd(int index) {
-        if (index < 0 || index > size) {
-            throw new IndexOutOfBoundsException("Theres no index " + index);
-        }
-    }
-
     @Override
     public void addAll(List<T> list) {
-        if (list == null || list.isEmpty()) {
-            return;
-        }
         for (T t : list) {
             add(t);
         }
@@ -87,20 +70,14 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     @Override
     public T get(int index) {
         checkIndex(index);
-        Node<T> current = head;
-        for (int i = 0; i < index; ++i) {
-            current = current.next;
-        }
+        Node<T> current = getNodeByIndex(index);
         return current.item;
     }
 
     @Override
     public T set(T value, int index) {
         checkIndex(index);
-        Node<T> current = head;
-        for (int i = 0; i < index; ++i) {
-            current = current.next;
-        }
+        Node<T> current = getNodeByIndex(index);
         T changedValue = current.item;
         current.item = value;
         return changedValue;
@@ -130,6 +107,24 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         return false;
     }
 
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    private Node<T> getNodeByIndex(int index) {
+        Node<T> current = head;
+        for (int i = 0; i < index; ++i) {
+            current = current.next;
+        }
+        return current;
+    }
+
     private void unlink(Node<T> node) {
         if (node.prev == null) {
             head = node.next;
@@ -145,19 +140,16 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         size--;
     }
 
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Theres no index " + index);
+        }
+    }
+
     private void checkIndex(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Theres no index " + index);
         }
     }
 
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return head == null;
-    }
 }
