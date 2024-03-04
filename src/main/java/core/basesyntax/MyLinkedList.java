619c29991515c7a7a245a59d4f739a19b87b13fe
Bohdan <kbs20043131@gmail.com>
2024-03-04 20:01:12 +0200

    jv-linked-list complete

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,45 +3,158 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private int size;
+    private Node<T> head;
+    private Node<T> tail;
+
+    private static class Node<T> {
+        private Node<T> prev;
+        private T item;
+        private Node<T> next;
+
+        Node(Node<T> prev, T item, Node<T> next) {
+            this.prev = prev;
+            this.item = item;
+            this.next = next;
+        }
+    }
+
     @Override
     public void add(T value) {
+        Node<T> newNode = new Node<>(tail, value, null);
+        if (tail == null) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+        tail = newNode;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index == size) {
+            add(value);
+            return;
+        }
+        Node<T> findNode = find(index);
+        Node<T> newNode;
+        if (findNode.equals(head)) {
+            newNode = new Node<>(null, value, head);
+            head.prev = newNode;
+            head = newNode;
+        } else {
+            newNode = new Node<>(findNode.prev, value, findNode);
+            newNode.next = findNode;
+            newNode.prev = findNode.prev;
+            findNode.prev.next = newNode;
+            findNode.prev = newNode;
+        }
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T node : list) {
+            add(node);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        Node<T> findNode = find(index);
+        return findNode.item;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        Node<T> findNode = find(index);
+        T deletedValue = findNode.item;
+        findNode.item = value;
+        return deletedValue;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        Node<T> findNode = find(index);
+        if (findNode.equals(head) && findNode.equals(tail)) {
+            head = tail = null;
+        } else if (findNode.equals(head)) {
+            head = head.next;
+        } else if (findNode.equals(tail)) {
+            tail = tail.prev;
+        } else {
+            findNode.next.prev = findNode.prev;
+            findNode.prev.next = findNode.next;
+        }
+        T deletedValue = findNode.item;
+        findNode.item = null;
+        size--;
+        return deletedValue;
     }
 
     @Override
     public boolean remove(T object) {
+        Node<T> findNode = head;
+        for (int i = 0; i < size; i++) {
+            if (object != null && object.equals(findNode.item) || object == findNode.item) {
+                remove(i);
+                return true;
+            }
+            findNode = findNode.next;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    public void unlink(Node node) {
+
+        if (node.prev == null) {
+            head = node;
+        } else {
+            node.prev.next = node.next;
+        }
+        if (node.next == null) {
+            tail = node;
+        } else {
+            node.next.prev = node.prev;
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index >= size) {
+            throw new IndexOutOfBoundsException("Specified index is invalid: "
+                    + index + " >= " + size);
+        } else {
+            if (index < 0) {
+                throw new IndexOutOfBoundsException("Specified index is invalid: "
+                        + index + " < 0");
+            }
+        }
+    }
+
+    private Node<T> find(int index) {
+        checkIndex(index);
+        Node<T> findNode = head;
+        if (index <= size / 2) {
+            for (int i = 0; i < index; i++) {
+                findNode = findNode.next;
+            }
+        } else {
+            findNode = tail;
+            for (int i = size - 1; i > index; i--) {
+                findNode = findNode.prev;
+            }
+        }
+        return findNode;
     }
 }
