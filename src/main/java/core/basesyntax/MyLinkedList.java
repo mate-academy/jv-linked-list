e1f99a22c7f0ae0ffb532b2b7a011bcf93da2f8e
Artem Klishch <artemklishch@gmail.com>
2024-03-07 01:19:14 +0200

    feat: should add linked list methods
    BREAKING CHANGES:
    
    *Made logic to implement linked list methods
    
    Task: https://mate.academy/learn/java-core/java-linked-list?section=practice&videoId=606&testTaskSlug=java_linked_list&taskSection=task-description

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -1,47 +1,211 @@
 package core.basesyntax;
 
 import java.util.List;
+import java.util.Objects;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    public MyLinkedList() {
+        this.size = 0;
+    }
+
+    public static class Node<T> {
+        private T value;
+        private Node<T> prev;
+        private Node<T> next;
+
+        public Node(Node<T> prev, T value, Node<T> next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
     @Override
     public void add(T value) {
+        if (size == 0) {
+            addFirstElement(value);
+        } else {
+            addNodeToTheEnd(value);
+        }
     }
 
     @Override
     public void add(T value, int index) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException(
+                    "Impossible to add element on the pointed index"
+            );
+        }
+        if (size == 0) {
+            addFirstElement(value);
+        } else if (index == size) {
+            addNodeToTheEnd(value);
+        } else if (index == 0) {
+            Node<T> prevHead = head;
+            Node<T> newHead = new Node<>(null, value, prevHead);
+            prevHead.prev = newHead;
+            this.head = newHead;
+            size++;
+        } else {
+            Node<T> nodeOnIndex = findNodeByIndex(index);
+            Node<T> prevNode = nodeOnIndex.prev;
+            Node<T> newNode = new Node<>(nodeOnIndex.prev, value, nodeOnIndex);
+            nodeOnIndex.prev = newNode;
+            prevNode.next = newNode;
+            size++;
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
+        for (T value : list) {
+            addNodeToTheEnd(value);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        return findNodeByIndex(index).value;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        if (index > size - 1 || index < 0) {
+            throw new IndexOutOfBoundsException("Impossible to set value");
+        }
+        Node<T> node = findNodeByIndex(index);
+        T oldValue = node.value;
+        node.value = value;
+        return oldValue;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index > size - 1 || index < 0) {
+            throw new IndexOutOfBoundsException(
+                    "Impossible to remove element on the pointed index"
+            );
+        }
+        Node<T> removingNode;
+        if (index == 0 && size == 1) {
+            removingNode = head;
+            this.head = null;
+        } else if (index == 0) {
+            removingNode = head;
+            Node<T> newHead = head.next;
+            newHead.prev = null;
+            this.head.next = null;
+            this.head = newHead;
+        } else if (index == size - 1) {
+            removingNode = tail;
+            Node<T> newTail = tail.prev;
+            newTail.next = null;
+            this.tail.prev = null;
+            this.tail = newTail;
+        } else {
+            removingNode = findNodeByIndex(index);
+            Node<T> prevNode = removingNode.prev;
+            Node<T> nextNode = removingNode.next;
+            prevNode.next = nextNode;
+            nextNode.prev = prevNode;
+        }
+        unlink(removingNode);
+        size--;
+        return removingNode.value;
     }
 
     @Override
     public boolean remove(T object) {
-        return false;
+        if (size == 0) {
+            throw new IndexOutOfBoundsException(
+                    "Impossible to remove element"
+            );
+        }
+        T removingNodeValue = null;
+        boolean isNullRemoved = false;
+        Node<T> searchingNode = head;
+        for (int i = 0; i < size; i++) {
+            T value = searchingNode.value;
+            if (Objects.equals(value, object)) {
+                removingNodeValue = searchingNode.value;
+                isNullRemoved = searchingNode.value == null;
+                remove(i);
+                unlink(searchingNode);
+                break;
+            }
+            searchingNode = searchingNode.next;
+        }
+        return removingNodeValue != null || isNullRemoved;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
+    }
+
+    @Override
+    public String toString() {
+        return "MyLinkedList{"
+                + "head=" + head.value + ", tail=" + tail.value
+                + ", size=" + size + '}';
+    }
+
+    private void addFirstElement(T value) {
+        this.head = new Node<>(null, value, null);
+        this.tail = head;
+        size++;
+    }
+
+    private void addNodeToTheEnd(T value) {
+        Node<T> prevLastNode = this.tail;
+        this.tail = new Node<>(prevLastNode, value, null);
+        prevLastNode.next = this.tail;
+        size++;
+    }
+
+    private Node<T> findNodeByIndex(int index) {
+        if (index > size - 1 || size == 0 || index < 0) {
+            throw new IndexOutOfBoundsException(
+                    "No element on the pointed index"
+            );
+        }
+        if (index == 0) {
+            return head;
+        }
+        if (index == size - 1) {
+            return tail;
+        }
+        boolean isCloserToHead = (size / 2) > index;
+        Node<T> searchingElement;
+        if (isCloserToHead) {
+            searchingElement = head;
+            int loopIndex = 0;
+            while (loopIndex < index) {
+                searchingElement = searchingElement.next;
+                loopIndex++;
+            }
+        } else {
+            searchingElement = tail;
+            int loopIndex = size - 1;
+            while (loopIndex > index) {
+                searchingElement = searchingElement.prev;
+                loopIndex--;
+            }
+        }
+        return searchingElement;
+    }
+
+    private void unlink(Node<T> node) {
+        node.next = null;
+        node.prev = null;
     }
 }
