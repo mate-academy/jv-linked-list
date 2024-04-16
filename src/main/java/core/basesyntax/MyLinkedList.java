e57e6d74e092986609ba0267f07b4fc605febf60
Vadym <Servicepost.v@gmail.com>
2024-04-16 18:45:48 +0200

    Created MyLinkedList. Added new methods to split a logic and get better structure.

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,45 +3,194 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+
+    private int size;
+    private Node<T> node;
+    private Node<T> head;
+    private Node<T> tail;
+
     @Override
     public void add(T value) {
+        if (head == null) {
+            addHeadNode(value);
+        } else {
+            addNextNode(value);
+        }
     }
 
     @Override
     public void add(T value, int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayIndexOutOfBoundsException("Index is not valid: " + index);
+        }
+        if (index == 0) {
+            if (head == null) {
+                addHeadNode(value);
+            } else {
+                node = new Node<>(null, value, head);
+                head.prev = node;
+                head = node;
+                size++;
+            }
+        } else if (size == index) {
+            addNextNode(value);
+        } else {
+            addNodeInside(index, value);
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
+        if (size == 0) {
+            addHeadNode(list.get(0));
+            for (int i = 1; i < list.size(); i++) {
+                addNextNode(list.get(i));
+            }
+        } else {
+            for (T t : list) {
+                addNextNode(t);
+            }
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkElementIndex(index);
+        return getNodeOnIndex(index).item;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        checkElementIndex(index);
+        Node<T> nodeOnIndex = getNodeOnIndex(index);
+        T changedValue = nodeOnIndex.item;
+        nodeOnIndex.item = value;
+        return changedValue;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        checkElementIndex(index);
+        Node<T> removedNode = getNodeOnIndex(index);
+        return unLink(removedNode);
     }
 
     @Override
     public boolean remove(T object) {
+        if (object == null) {
+            for (Node<T> i = head; i != null; i = i.next) {
+                if (i.item == null) {
+                    unLink(i);
+                    return true;
+                }
+            }
+        } else {
+            for (Node<T> i = head; i != null; i = i.next) {
+                if (object.equals(i.item)) {
+                    unLink(i);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private static class Node<T> {
+        private T item;
+        private Node<T> next;
+        private Node<T> prev;
+
+        public Node(Node<T> prev, T element, Node<T> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    private void checkElementIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayIndexOutOfBoundsException("Index is not valid: " + index);
+        }
+    }
+
+    private Node<T> getNodeOnIndex(int index) {
+        Node<T> nodeOnIndex;
+        if (index < (size >> 1)) {
+            nodeOnIndex = head;
+            for (int i = 0; i < index; i++) {
+                nodeOnIndex = nodeOnIndex.next;
+            }
+        } else {
+            nodeOnIndex = tail;
+            for (int i = size - 1; i > index; i--) {
+                nodeOnIndex = nodeOnIndex.prev;
+            }
+        }
+        return nodeOnIndex;
+    }
+
+    private void addHeadNode(T value) {
+        node = new Node<>(null, value, null);
+        head = node;
+        tail = node;
+        size++;
+    }
+
+    private void addNextNode(T value) {
+        if (head.equals(tail)) {
+            node = new Node<>(node.prev, value, node.next);
+            node.next = null;
+            node.prev = head;
+            head.next = node;
+            head.prev = null;
+            tail = node;
+            size++;
+        } else {
+            node = new Node<>(node.prev, value, node.next);
+            node.prev = tail;
+            tail.next = node;
+            node.next = null;
+            tail = node;
+            size++;
+        }
+    }
+
+    private void addNodeInside(int index, T value) {
+        Node<T> nextNode = getNodeOnIndex(index);
+        node = new Node<>(nextNode.prev, value, nextNode);
+        nextNode.prev = node;
+        node.prev.next = node;
+        size++;
+    }
+
+    private T unLink(Node<T> removedNode) {
+        T removedValue = removedNode.item;
+        if (head.equals(tail)) {
+            size--;
+            return (T) (removedNode = null);
+        } else if (removedNode.prev == null) {
+            head = head.next;
+            head.prev = null;
+            size--;
+        } else if (removedNode == tail) {
+            tail = tail.prev;
+            tail.next = null;
+            size--;
+        } else {
+            removedNode.prev.next = removedNode.next;
+            removedNode.next.prev = removedNode.prev;
+            size--;
+        }
+        return removedValue;
     }
 }
