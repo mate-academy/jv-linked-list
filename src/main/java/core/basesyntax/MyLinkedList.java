286f45b189ba9444dd81d87f21f1347945c1ac4e
Ілля Лазарев <108271589+bagouser123@users.noreply.github.com>
2024-06-10 23:10:26 +0300

    Linked list Task

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -1,47 +1,151 @@
 package core.basesyntax;
-
 import java.util.List;
+import java.util.Objects;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private static class Node<T> {
+        T item;
+        Node<T> next;
+        Node<T> prev;
+
+        Node(T item) {
+            this.item = item;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
     @Override
     public void add(T value) {
+        Node<T> newNode = new Node<>(value);
+        if (size == 0) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index < 0 || index > size ) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            Node<T> newNode = new Node<>(value);
+            if (head == null) {
+            head = tail = newNode;
+            } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+                add(value);
+                return;
+            }
+            size++;
+        }
     }
-
     @Override
     public void addAll(List<T> list) {
+        for (T value : list) {
+            add(value);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.item;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        T list = current.item;
+        current.item = value;
+        return list;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> current = head;
+        if (index == 0) {
+            head = head.next;
+            if (head != null) {
+                head.prev = null;
+            } else {
+                tail = null;
+            }
+        } else if (index == size - 1) {
+            current = tail;
+            tail = tail.prev;
+            tail.next = null;
+        } else {
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+        }
+        size--;
+        return current.item;
     }
 
     @Override
     public boolean remove(T object) {
+        Node<T> newNode = head;
+        while(newNode != null) {
+            if (Objects.equals(object, newNode.item)) {
+                if (newNode == head) {
+                    newNode = newNode.next;
+                    if (head != null) {
+                        head.prev = null;
+                    } else {
+                        tail = null;
+                    }
+                } else if (newNode == tail) {
+                    tail = tail.prev;
+                    tail.next = null;
+                } else {
+                    newNode.next.prev = newNode.prev;
+                    newNode.prev.next = newNode.next;
+                }
+                size--;
+                return true;
+            }
+            newNode = newNode.next;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+    return size == 0;
     }
 }
