c97e33ac8b7046fbd582120a9b29edff32259ef2
JaverJaver <mopasaj205@arpizol.com>
2024-06-19 01:33:45 +0300

    first

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,12 +3,28 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    public MyLinkedList() {
+
+    }
+
     @Override
     public void add(T value) {
+        boolean isFirst = false;
+        Node node = null;
+        if (!isFirst) {
+            node = new Node(null, value, null);
+            this.head = node;
+            this.tail = node;
+        }
     }
 
     @Override
     public void add(T value, int index) {
+
     }
 
     @Override
@@ -37,11 +53,23 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private class Node<E> {
+        E element;
+        Node<E> prev;
+        Node<E> next;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
+            this.prev = prev;
+        }
     }
 }
