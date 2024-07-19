f0ed43d3021b4817938172d904095f6281a316ac
Nastia Pyshak <nastapisak618@gmail.com>
2024-07-19 07:28:55 +0200

    Line 97 removed

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -94,7 +94,6 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     }
 
     private Node<T> findNodeByIndex(int index) {
-        checkIndex(index);
         Node<T> current;
         if (index < (size / 2)) {
             current = head;
