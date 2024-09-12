f7d9042060a8b42db17118701232ee3b19442ee0
StoneBlood <brkalix@gmail.com>
2024-09-12 17:01:25 +0300

    make class Node private and static

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -148,7 +148,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         }
     }
 
-    public class Node<T> {
+    private static class Node<T> {
         private T value;
         private Node<T> prev;
         private Node<T> next;
