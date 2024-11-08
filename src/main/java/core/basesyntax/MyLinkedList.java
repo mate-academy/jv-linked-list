38bf603e209c9d84eebead7e7ac735234cf357a4
Olha Oleshchuk <olha.oleshchuk@gmail.com>
2024-11-08 10:59:48 +0200

    first try

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -8,6 +8,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     private Node<T> head;
     private Node<T> tail;
     private int size;
+    //
 
     public class Node<T> {
         private T value;
