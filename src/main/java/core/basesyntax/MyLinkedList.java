5098b2effdf756c5d04da6b04dff3ceb38fb3873
Teslenko Daniil <burgalhashem@gmail.com>
2024-12-11 01:17:12 +0200

    added requested changes

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -9,7 +9,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public void add(T value) {
-        Node<T> newNode = new Node<>(value);
+        Node<T> newNode = new Node<>(null, value, null);
         if (head == null) {
             head = tail = newNode;
         } else {
@@ -22,40 +22,39 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public void add(T value, int index) {
-        if (size != index) {
-            checkIndex(index);
+        if (index != size()) {
+            checkIndexForAddMethod(index);
         }
-        Node<T> newNode = new Node<>(value);
-        size++;
-        if (this.isEmpty() && index == 0) {
+        Node<T> newNode = new Node<>(null, value, null);
+        if (isEmpty() && index == 0) {
             head = tail = newNode;
-            return;
-        }
-        if (index == 0) {
+        } else if (index == 0) {
             newNode.right = head;
-            if (head != null) {
-                head.left = newNode;
-            }
+            head.left = newNode;
             head = newNode;
-            return;
-        }
-        if (index == size - 1) {
+        } else if (index == size() - 1) {
+            newNode.left = tail.left;
+            newNode.right = tail;
+            tail.left.right = newNode;
+            tail.left = newNode;
+        } else if (index == size()) {
             tail.right = newNode;
             newNode.left = tail;
-            tail = newNode;
-            return;
+            tail = tail.right;
+        } else {
+            Node<T> tempNode = findByIndex(index);
+            tempNode.left.right = newNode;
+            newNode.right = tempNode;
+            newNode.left = tempNode.left;
+            tempNode.left = newNode;
         }
-        Node<T> tempNode = findByIndex(index);
-        tempNode.left.right = newNode;
-        newNode.right = tempNode;
-        newNode.left = tempNode.left;
-        tempNode.left = newNode;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
         for (T element : list) {
-            this.add(element);
+            add(element);
         }
     }
 
@@ -80,20 +79,20 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         checkIndex(index);
         Node<T> temp = findByIndex(index);
         if (temp == head) {
-            if (this.size > 1) {
+            if (size > 1) {
                 head = temp.right;
-                temp.right = null;
                 head.left = null;
             } else {
-                head = null;
-                tail = null;
+                head = tail = null;
             }
         } else if (temp == tail) {
+            tail = tail.left;
             temp.left.right = null;
         } else {
             temp.left.right = temp.right;
             temp.right.left = temp.left;
         }
+        unlink(temp);
         size--;
         return temp.data;
     }
@@ -104,7 +103,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         for (int i = 0; i < size; i++) {
             if ((temp.data != null && temp.data.equals(object))
                     || (temp.data == null && null == object)) {
-                this.remove(i);
+                remove(i);
                 return true;
             }
             temp = temp.right;
@@ -119,21 +118,39 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public boolean isEmpty() {
-        return head == null;
+        return size == 0;
+    }
+
+    private void checkIndexForAddMethod(int index) {
+        if (index < 0 || (isEmpty() && index > 0) || index > size) {
+            throw new IndexOutOfBoundsException("Element not found in the list");
+        }
     }
 
     private void checkIndex(int index) {
-        if (index < 0 || index > size - 1 || (this.isEmpty() && index > 0)) {
+        if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Element not found in the list");
         }
     }
 
     private Node<T> findByIndex(int index) {
-        Node<T> node = head;
-        for (int i = 0; i < index; i++) {
-            node = node.right;
+        Node<T> node;
+        if (index <= (size() >> 1)) {
+            node = head;
+            for (int i = 0; i < index; i++) {
+                node = node.right;
+            }
+        } else {
+            node = tail;
+            for (int i = size() - 1; i > index; i--) {
+                node = node.left;
+            }
         }
         return node;
+    }
+
+    private void unlink(Node<T> node) {
+        node.left = node.right = null;
     }
 
     private static class Node<T> {
@@ -141,9 +158,10 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         private Node<T> left;
         private Node<T> right;
 
-        public Node(T data) {
-            this.data = data;
-            left = right = null;
+        public Node(Node<T> left, T item, Node<T> right) {
+            this.left = left;
+            this.data = item;
+            this.right = right;
         }
     }
 }
