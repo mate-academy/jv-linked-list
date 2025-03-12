d94845adea8f20d00579a8686569ecaaab1087a1
Mizuo <mizuo.personal@gmail.com>
2025-03-12 19:46:49 +0100

    minor improvements

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -129,9 +129,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
     }
 
     private Node<T> findNodeByIndex(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException();
-        }
+        checkIndex(index);
         Node<T> current;
         if (index < size / 2) {
             current = head.next;
