35d6e89c2423c523d0fc60245c8eaba4129808c4
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-05-29 16:23:39 +0200

    Moved Node<T> to the end of MyLinkedList.java as per review comments

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -50,106 +50,106 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
         }
 
 
-    @Override
-    public void addAll(List<T> list) {
-        for (T value : list) {
-            add(value);
-        }
-    }
-
-    @Override
-    public T get(int index) {
-        validateIndex(index, false);
-        return findNodeByIndex(index).value;
-    }
-
-    @Override
-    public T set(T value, int index) {
-        validateIndex(index, false);
-        Node<T> node = findNodeByIndex(index);
-        T oldValue = node.value;
-        node.value = value;
-        return oldValue;
-    }
-
-    @Override
-    public T remove(int index) {
-        validateIndex(index, false);
-        return unlink(findNodeByIndex(index));
-    }
-
-    @Override
-    public boolean remove(T object) {
-        Node<T> current = head;
-        while (current != null) {
-            if (current.value == object
-                    || (current.value != null && current.value.equals(object))) {
-                unlink(current);
-                return true;
+        @Override
+        public void addAll (List < T > list) {
+            for (T value : list) {
+                add(value);
             }
-            current = current.next;
         }
-        return false;
-    }
 
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    private void validateIndex(int index, boolean isAddOperation) {
-        boolean isInvalidIndex = index < 0 || index > size || (!isAddOperation && index == size);
-
-        if (isInvalidIndex) {
-            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        @Override
+        public T get ( int index){
+            validateIndex(index, false);
+            return findNodeByIndex(index).value;
         }
-    }
 
-    private Node<T> findNodeByIndex(int index) {
-        Node<T> current;
-        if (index < size / 2) {
-            current = head;
-            for (int i = 0; i < index; i++) {
+        @Override
+        public T set (T value,int index){
+            validateIndex(index, false);
+            Node<T> node = findNodeByIndex(index);
+            T oldValue = node.value;
+            node.value = value;
+            return oldValue;
+        }
+
+        @Override
+        public T remove ( int index){
+            validateIndex(index, false);
+            return unlink(findNodeByIndex(index));
+        }
+
+        @Override
+        public boolean remove (T object){
+            Node<T> current = head;
+            while (current != null) {
+                if (current.value == object
+                        || (current.value != null && current.value.equals(object))) {
+                    unlink(current);
+                    return true;
+                }
                 current = current.next;
             }
-        } else {
-            current = tail;
-            for (int i = size - 1; i > index; i--) {
-                current = current.prev;
+            return false;
+        }
+
+        @Override
+        public int size () {
+            return size;
+        }
+
+        @Override
+        public boolean isEmpty () {
+            return size == 0;
+        }
+
+        private void validateIndex ( int index, boolean isAddOperation){
+            boolean isInvalidIndex = index < 0 || index > size || (!isAddOperation && index == size);
+
+            if (isInvalidIndex) {
+                throw new IndexOutOfBoundsException("Invalid index: " + index);
             }
         }
-        return current;
-    }
 
-    private T unlink(Node<T> node) {
-        if (node.prev != null) {
-            node.prev.next = node.next;
-        } else {
-            head = node.next;
+        private Node<T> findNodeByIndex ( int index){
+            Node<T> current;
+            if (index < size / 2) {
+                current = head;
+                for (int i = 0; i < index; i++) {
+                    current = current.next;
+                }
+            } else {
+                current = tail;
+                for (int i = size - 1; i > index; i--) {
+                    current = current.prev;
+                }
+            }
+            return current;
         }
-        if (node.next != null) {
-            node.next.prev = node.prev;
-        } else {
-            tail = node.prev;
+
+        private T unlink (Node < T > node) {
+            if (node.prev != null) {
+                node.prev.next = node.next;
+            } else {
+                head = node.next;
+            }
+            if (node.next != null) {
+                node.next.prev = node.prev;
+            } else {
+                tail = node.prev;
+            }
+            size--;
+
+            return node.value;
         }
-        size--;
 
-        return node.value;
-    }
-
-    Node<T> nextNode = findNodeByIndex(index);
-    Node<T> newNode = new Node<>(value, nextNode.prev, nextNode);
+        Node<T> nextNode = findNodeByIndex(index);
+        Node<T> newNode = new Node<>(value, nextNode.prev, nextNode);
         if (nextNode.prev != null) {
-        nextNode.prev.next = newNode;
-    } else {
-        head = newNode;
+            nextNode.prev.next = newNode;
+        } else {
+            head = newNode;
+        }
+        nextNode.prev = newNode;
+        size++;
     }
-    nextNode.prev = newNode;
-    size++;
-}
 }
